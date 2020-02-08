6585bb43450ca51f6ac6d74655c1f8554e9748dc
Krisztian Szucher <krisztian@mindboard.io>
2020-02-08 13:19:45 +0100

    some more es6 compatibility stuff

--- client/map/MapDivVisualize.js
+++ client/map/MapDivVisualize.js
@@ -1,106 +1,100 @@
-import {mapMem}                                             from "./Map";
-import {genHash, getLatexString, copy, getBgc}              from "../src/Utils";
-import {hasCell}                                            from "../node/Node";
+import {mapMem} from "./Map";
+import {genHash, getLatexString, copy, getBgc} from "../src/Utils";
+import {hasCell} from "../node/Node";
 
 class MapDivVisualize {
     start() {
-        let cm =                                            mapMem.data.s[0];
+        let cm = mapMem.data.s[0];
         this.iterate(cm);
     }
 
     iterate(cm) {
-        if (cm.type === 'struct') {
-            if (hasCell(cm)) {
+        if (cm.type === 'struct' && ! hasCell(cm)) {
+            let divStyle = {
+                left: cm.nodeStartX + 'px',
+                top: cm.nodeStartY - cm.selfH / 2 + 'px',
+                width: cm.selfW - mapMem.padding - 2 + 'px',
+                height: cm.selfH - mapMem.padding - 2 + 'px',
+                paddingLeft: mapMem.padding - 2 + 'px',
+                paddingTop: mapMem.padding - 2 + 'px',
+                position: 'absolute',
+                border: cm.selected ? '1px solid black' : '1px solid' + getBgc(),
+                borderRadius: 8 + 'px',
+                fontSize: cm.sTextFontSize + 'px',
+                fontFamily: 'Roboto',
+                textDecoration: cm.linkType !== "" ? "underline" : "",
+                cursor: 'default',
+                color: cm.sTextColor,
+                backgroundColor: cm.ellipseFill ? cm.ellipseFillColor : getBgc(),
+            };
 
+            if (mapMem.density === 'small' && cm.contentType === 'text') {
+                divStyle.width = parseInt(divStyle.width, 10) - 3 + 'px';
+                divStyle.paddingLeft = parseInt(divStyle.paddingLeft, 10) + 3 + 'px';
             }
-            else {
-                let divStyle = {
-                    left:                                   cm.nodeStartX +                                 'px',
-                    top:                                    cm.nodeStartY - cm.selfH/2  +                   'px',
-                    width :                                 cm.selfW - mapMem.padding - 2 +                 'px',
-                    height :                                cm.selfH - mapMem.padding - 2 +                 'px',
-                    paddingLeft :                           mapMem.padding - 2 +                            'px',
-                    paddingTop :                            mapMem.padding - 2 +                            'px',
-                    position:                               'absolute',
-                    border:             cm.selected?        '1px solid black' : '1px solid' + getBgc(),
-                    borderRadius:                           8 +                                             'px',
-                    fontSize:                               cm.sTextFontSize +                              'px',
-                    fontFamily:                             'Roboto',
-                    textDecoration:     cm.linkType !== ""? "underline" : "",
-                    cursor:                                 'default',
-                    color:                                  cm.sTextColor,
-                    backgroundColor:    cm.ellipseFill?     cm.ellipseFillColor : getBgc(),
-                };
 
-                if (mapMem.density === 'small' && cm.contentType === 'text') {
-                    divStyle.width =                        parseInt(divStyle.width, 10)       - 3 + 'px';
-                    divStyle.paddingLeft =                  parseInt(divStyle.paddingLeft, 10) + 3 + 'px';
+            let div;
+            if (cm.isDivAssigned === 0) {
+                cm.isDivAssigned = 1;
+
+                cm.divId = 'div' + genHash(8);
+                mapMem.divData[cm.divId] = {divStyle: {}};
+
+                div = document.createElement('div');
+                div.id = cm.divId;
+                div.contentEditable = false;
+                div.spellcheck = false;
+
+                div.appendChild(document.createTextNode(''));
+                document.getElementById('mapDiv').appendChild(div);
+
+                for (let i = 0; i < Object.keys(divStyle).length; i++) {
+                    let styleName = Object.keys(divStyle)[i];
+                    if (divStyle[styleName] !== mapMem.divData[cm.divId].divStyle[styleName]) {
+                        div.style[styleName] = divStyle[styleName];
+                    }
                 }
+            } else {
+                div = document.getElementById(cm.divId);
 
-                let div;
-                if (cm.isDivAssigned === 0) {
-                    cm.isDivAssigned =                      1;
-
-                    cm.divId = 'div' + genHash(8);
-                    mapMem.divData[cm.divId] =              {divStyle:{}};
-
-                    div =                                   document.createElement('div');
-                    div.id =                                cm.divId;
-                    div.contentEditable =                   false;
-                    div.spellcheck =                        false;
-
-                    div.appendChild(document.createTextNode(''));
-                    document.getElementById('mapDiv').appendChild(div);
-
-                    for (let i = 0; i < Object.keys(divStyle).length; i++) {
-                        let styleName = Object.keys(divStyle)[i];
+                for (let i = 0; i < Object.keys(divStyle).length; i++) {
+                    let styleName = Object.keys(divStyle)[i];
+                    if (styleName !== 'left' && styleName !== 'top') {
                         if (divStyle[styleName] !== mapMem.divData[cm.divId].divStyle[styleName]) {
-                            div.style[styleName] =          divStyle[styleName];
+                            div.style[styleName] = divStyle[styleName];
                         }
                     }
                 }
-                else {
-                    div = document.getElementById(cm.divId);
 
-                    for (let i = 0; i < Object.keys(divStyle).length; i++) {
-                        let styleName = Object.keys(divStyle)[i];
-                        if (styleName !== 'left' && styleName !== 'top') {
-                            if (divStyle[styleName] !== mapMem.divData[cm.divId].divStyle[styleName]) {
-                                div.style[styleName] =      divStyle[styleName];
-                            }
-                        }
-                    }
+                let leftDelta = parseInt(div.style.left, 10) - parseInt(divStyle.left);
+                let topDelta = parseInt(div.style.top, 10) - parseInt(divStyle.top);
 
-                    let leftDelta =                         parseInt(div.style.left, 10) -     parseInt(divStyle.left);
-                    let topDelta =                          parseInt(div.style.top, 10) -      parseInt(divStyle.top);
+                if (leftDelta !== 0 || topDelta !== 0) {
+                    div.style.transform = "translate(" + leftDelta + ',' + topDelta + ")";
+                    div.style.transition = '0.5s ease-out';
 
-                    if (leftDelta !== 0 || topDelta !== 0) {
-                        div.style.transform =               "translate(" + leftDelta + ',' + topDelta + ")";
-                        div.style.transition =              '0.5s ease-out';
-
-                        div.style.left =                    divStyle.left;
-                        div.style.top =                     divStyle.top;
-                    } else {
-                        div.style.transform =               '';
-                        div.style.transition =              '';
-                    }
+                    div.style.left = divStyle.left;
+                    div.style.top = divStyle.top;
+                } else {
+                    div.style.transform = '';
+                    div.style.transition = '';
                 }
+            }
 
-                mapMem.divData[cm.divId].divStyle =         copy(divStyle);
+            mapMem.divData[cm.divId].divStyle = copy(divStyle);
 
-                if (cm.isContentAssigned === 0) {
-                    cm.isContentAssigned = 1;
+            if (cm.isContentAssigned === 0) {
+                cm.isContentAssigned = 1;
 
-                    if (cm.contentType === 'text') {
-                        div.innerHTML =                     cm.content;
-                    }
-                    else if (cm.contentType === 'image') {
-                        div.innerHTML =                     '<img src="' + 'http://localhost:8082/file/' + cm.content + '">';
-                    }
-                    else if (cm.contentType === 'equation') {
-                        // div.innerHTML = katex.renderToString(getLatexString(cm.content), {throwOnError: false});
-                        katex.render(getLatexString(cm.content), div, {throwOnError: false});
-                    }
+                if (cm.contentType === 'text') {
+                    div.innerHTML = cm.content;
+                } else if (cm.contentType === 'image') {
+                    div.innerHTML = '<img src="' + 'http://localhost:8082/file/' + cm.content + '">';
+                } else if (cm.contentType === 'equation') {
+                    // div.innerHTML = katex.renderToString(getLatexString(cm.content), {throwOnError: false});
+                    katex.render(getLatexString(cm.content), div, {throwOnError: false});
+                } else {
+
                 }
             }
         }
--- client/node/NodeInsert.js
+++ client/node/NodeInsert.js
@@ -1,6 +1,6 @@
-import {clearStructSelection}                           from "./NodeSelect";
-import {mapref}                                         from "../map/Map";
-import {getDefaultNode}                                 from "./Node";
+import {mapref} from "../map/Map";
+import {getDefaultNode} from "./Node";
+import {clearStructSelection} from "./NodeSelect";
 
 export function structInsert(lm, direction) {
     clearStructSelection();
@@ -10,29 +10,29 @@ export function structInsert(lm, direction) {
             'selected': 1,
             'taskStatus': parentRef.taskStatus
         }))
-    }
-    if (direction === 'down') {
+    } else if (direction === 'down') {
         parentRef.s.splice(lm.index + 1, 0, getDefaultNode({
             'selected': 1,
             'taskStatus': parentRef.taskStatus
         }));
-    }
-    else if (direction === 'right') {
+    } else if (direction === 'right') {
         lm.s.splice(lm.s.length, 0, getDefaultNode({
             'selected': 1,
             'taskStatus': lm.taskStatus
         }));
+    } else {
+
     }
 }
 
 export function cellInsert (lastPath, direction) {
-    let lm =       mapref(lastPath);
-    let parentRef =     mapref(lm.parentPath);
+    let lm = mapref(lastPath);
+    let parentRef = mapref(lm.parentPath);
 
-    let currRow =       lm.index[0];
-    let currCol =       lm.index[1];
-    let rowLen =        parentRef.c.length;
-    let colLen =        parentRef.c[0].length;
+    let currRow = lm.index[0];
+    let currCol = lm.index[1];
+    let rowLen = parentRef.c.length;
+    let colLen = parentRef.c[0].length;
 
     if (direction === 'ArrowDown') {
         let newRow = new Array(colLen);
@@ -40,22 +40,22 @@ export function cellInsert (lastPath, direction) {
             newRow[i] = getDefaultNode({s:[getDefaultNode()]});
         }
         parentRef.c.splice(currRow + 1, 0, newRow);
-    }
-    else if (direction === 'ArrowRight') {
+    } else if (direction === 'ArrowRight') {
         for (let i = 0; i < rowLen; i++) {
             parentRef.c[i].splice(currCol + 1, 0, getDefaultNode({s:[getDefaultNode()]}));
         }
-    }
-    else if (direction === 'ArrowUp') {
+    } else if (direction === 'ArrowUp') {
         let newRow = new Array(colLen);
         for (let i = 0; i < colLen; i++) {
             newRow[i] = getDefaultNode({s:[getDefaultNode()]});
         }
         parentRef.c.splice(currRow, 0, newRow);
-    }
-    else if (direction === 'ArrowLeft') {
+    } else if (direction === 'ArrowLeft') {
         for (let i = 0; i < rowLen; i++) {
             parentRef.c[i].splice(currCol, 0, getDefaultNode({s:[getDefaultNode()]}));
         }
+    }
+    else {
+
     }
 }
