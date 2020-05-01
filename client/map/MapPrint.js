d845fe6e4ecf52f1fe70badc19497952601a535e
Krisztian Szucher <krisztian@mindboard.io>
2020-05-01 13:17:43 +0200

    mapprint improved

--- client/map/MapPrint.js
+++ client/map/MapPrint.js
@@ -1,12 +1,13 @@
 export const mapPrint = {
     start: (cml) => {
         mapPrint.str = '';
+        mapPrint.entryLength = cml.path.length;
         mapPrint.iterate(cml);
         console.log(mapPrint.str)
     },
 
     iterate: (cm) => {
-        mapPrint.str += ('\n' + '  '.repeat(cm.path.length) + cm.content);
+        mapPrint.str += ('  '.repeat(cm.path.length - mapPrint.entryLength) + cm.content + '\n');
 
         let sCount = Object.keys(cm.s).length;
         for (let i = 0; i < sCount; i++) {
