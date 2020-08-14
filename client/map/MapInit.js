59646fa9c241b601516d4300caa47cf8deb210e8
Krisztian Szucher <krisztian@mindboard.io>
2020-08-15 00:15:15 +0200

    but they are necessary...

--- client/map/MapInit.js
+++ client/map/MapInit.js
@@ -1,5 +1,6 @@
 import {mapMem} from "./Map";
 import {props} from "../node/Node"
+import {copy, shallowCopy} from "../core/Utils"
 
 export const mapInit = {
     start: () => {
@@ -10,18 +11,20 @@ export const mapInit = {
     iterate: (cm) => {
         Object.keys(props.saveOptional).map(currProperty => {
             if (!cm.hasOwnProperty(currProperty)) {
-                cm[currProperty] = props.saveOptional[currProperty];
+                cm[currProperty] = ['s', 'c', 'path'].includes(currProperty)?
+                    copy(props.saveOptional[currProperty]) :
+                    shallowCopy(props.saveOptional[currProperty])
             }
         });
 
         Object.keys(props.saveNever.initOnce).map(currProperty => {
             if (!cm.hasOwnProperty(currProperty)) {
-                cm[currProperty] = props.saveNever.initOnce[currProperty];
+                cm[currProperty] = shallowCopy(props.saveNever.initOnce[currProperty])
             }
         });
 
         Object.keys(props.saveNever.initAlways).map(currProperty => {
-            cm[currProperty] = props.saveNever.initAlways[currProperty];
+            cm[currProperty] = shallowCopy(props.saveNever.initAlways[currProperty])
         });
 
         let rowCount = Object.keys(cm.c).length;
