2ad1574bc214d9d35f22dea2c9c13f2f87f9bff9
Krisztian Szucher <krisztian@mindboard.io>
2020-08-23 23:20:58 +0200

    preventing deletion of cell struct

--- client/node/NodeDelete.js
+++ client/node/NodeDelete.js
@@ -2,41 +2,44 @@ import {mapref} from "../map/Map";
 import {checkSelection} from "./NodeSelect";
 
 export function structDeleteReselect(sc) {
-    // calculate jumpback
     let lm = sc.lm;
     let lastParentRef = mapref(lm.parentPath);
-    let lastParentRefChildCntr = lastParentRef.s.length;
-    let lastParentRefDelChildCntr = 0;
-    for (let i = lm.index; i > - 1; i--) {
-        if (lastParentRef.s[i].selected > 0) {
-            lastParentRefDelChildCntr++;
-        }
-    }
 
-    // delete
-    for (let i = sc.structSelectedPathList.length - 1; i > -1; i--) {
-        let currRef = mapref(sc.structSelectedPathList[i]);
-        if (currRef.isRoot === 0) {
-            let currParentRef = mapref(currRef.parentPath);
-            currParentRef.taskStatus = currRef.taskStatus;
-            currParentRef.taskStatusInherited = 0;
-            currParentRef.s.splice(currRef.index, 1);
+    if (lastParentRef.type !== 'cell') {
+        // calculate jumpback
+        let lastParentRefChildCntr = lastParentRef.s.length;
+        let lastParentRefDelChildCntr = 0;
+        for (let i = lm.index; i > -1; i--) {
+            if (lastParentRef.s[i].selected > 0) {
+                lastParentRefDelChildCntr++;
+            }
         }
-    }
 
-    // reselect on jumpback
-    if (lm.isRoot === 0) {
-        if (lastParentRefChildCntr === lastParentRefDelChildCntr) {
-            lastParentRef.selected = 1;
-        } else {
-            if (lm.index === 0) {
-                if (lastParentRef.s.length > 0) {
-                    lastParentRef.s[0].selected = 1;
-                } else {
-                    lastParentRef.selected = 1;
-                }
+        // delete
+        for (let i = sc.structSelectedPathList.length - 1; i > -1; i--) {
+            let currRef = mapref(sc.structSelectedPathList[i]);
+            if (currRef.isRoot === 0) {
+                let currParentRef = mapref(currRef.parentPath);
+                currParentRef.taskStatus = currRef.taskStatus;
+                currParentRef.taskStatusInherited = 0;
+                currParentRef.s.splice(currRef.index, 1);
+            }
+        }
+
+        // reselect on jumpback
+        if (lm.isRoot === 0) {
+            if (lastParentRefChildCntr === lastParentRefDelChildCntr) {
+                lastParentRef.selected = 1;
             } else {
-                lastParentRef.s[lm.index - lastParentRefDelChildCntr].selected = 1;
+                if (lm.index === 0) {
+                    if (lastParentRef.s.length > 0) {
+                        lastParentRef.s[0].selected = 1;
+                    } else {
+                        lastParentRef.selected = 1;
+                    }
+                } else {
+                    lastParentRef.s[lm.index - lastParentRefDelChildCntr].selected = 1;
+                }
             }
         }
     }
