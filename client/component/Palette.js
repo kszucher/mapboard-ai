00a86c728485935b4a06192931d40c8cad6b99c2
Krisztian Szucher <krisztian@mindboard.io>
2021-02-17 23:59:34 +0100

    palette right down

--- client/component/Palette.js
+++ client/component/Palette.js
@@ -16,22 +16,29 @@ import {checkPop, push, redraw} from "../map/Map";
 //     ['#5b0f00', '#660000', '#783f04', '#7f6000', '#274e13', '#0c343d', '#1c4587', '#073763', '#20124d', '#4c1130']
 // ];
 
+// const colorList = [
+//     ['#ffffff', '#cccccc', '#000000'],
+//     ['#e6b8af', '#cc4125', '#85200c'],
+//     ['#f4cccc', '#e06666', '#990000'],
+//     ['#fce5cd', '#f6b26b', '#b45f06'],
+//     ['#fff2cc', '#ffd966', '#bf9000'],
+//     ['#d9ead3', '#93c47d', '#38761d'],
+//     ['#d0e0e3', '#76a5af', '#134f5c'],
+//     ['#c9daf8', '#6d9eeb', '#1155cc'],
+//     ['#cfe2f3', '#6fa8dc', '#0b5394'],
+//     ['#d9d2e9', '#8e7cc3', '#351c75'],
+//     ['#ead1dc', '#c27ba0', '#741b47'],
+//     ['#d4ebfe', '#f6e5d4', '#d4f6d4'],
+//     ['#990000', '#000099', '#fbfafc'],
+//     ['#d5802a', '#1c8e1c', '#8e1c8e'],
+//     ['#999999', '#bbbbbb', '#dddddd'],
+// ];
+
 const colorList = [
-    ['#ffffff', '#cccccc', '#000000'],
-    ['#e6b8af', '#cc4125', '#85200c'],
-    ['#f4cccc', '#e06666', '#990000'],
-    ['#fce5cd', '#f6b26b', '#b45f06'],
-    ['#fff2cc', '#ffd966', '#bf9000'],
-    ['#d9ead3', '#93c47d', '#38761d'],
-    ['#d0e0e3', '#76a5af', '#134f5c'],
-    ['#c9daf8', '#6d9eeb', '#1155cc'],
-    ['#cfe2f3', '#6fa8dc', '#0b5394'],
-    ['#d9d2e9', '#8e7cc3', '#351c75'],
-    ['#ead1dc', '#c27ba0', '#741b47'],
-    ['#d4ebfe', '#f6e5d4', '#d4f6d4'],
-    ['#990000', '#000099', '#fbfafc'],
-    ['#d5802a', '#1c8e1c', '#8e1c8e'],
-    ['#999999', '#bbbbbb', '#dddddd'],
+    ['#000000', '#434343', '#666666', '#999999', '#b7b7b7', '#cccccc', '#d9d9d9', '#efefef', '#f3f3f3', '#ffffff', '#d4ebfe', '#f6e5d4', '#d4f6d4'],
+    ['#e6b8af', '#f4cccc', '#fce5cd', '#fff2cc', '#d9ead3', '#d0e0e3', '#c9daf8', '#cfe2f3', '#d9d2e9', '#ead1dc', '#990000', '#000099', '#fbfafc'],
+    ['#cc4125', '#e06666', '#f6b26b', '#ffd966', '#93c47d', '#76a5af', '#6d9eeb', '#6fa8dc', '#8e7cc3', '#c27ba0', '#d5802a', '#1c8e1c', '#8e1c8e'],
+    ['#85200c', '#990000', '#b45f06', '#bf9000', '#38761d', '#134f5c', '#1155cc', '#0b5394', '#351c75', '#741b47', '#999999', '#bbbbbb', '#dddddd'],
 ];
 
 export function Palette () {
@@ -99,13 +106,13 @@ export function Palette () {
         checkPop();
 
         // instead of setSel
-        // dispatch to state-nek, which will refresh color, which will call useEffects, which well setSel and
+        // dispatch to state, which will refresh color, which will call useEffects, which well setSel and
         // put the nodeDispatch into the mapComponent
     };
 
     return (
         <div id = 'palette'>
-            <svg viewBox='0 0 140 520'>
+            <svg viewBox='0 0 460 180'>
                 {colorList.map((iEl, i) => (
                     iEl.map((jEl, j) => (
                         <circle
--- client/component/Workspace.js
+++ client/component/Workspace.js
@@ -170,6 +170,7 @@ export function Workspace() {
                 <Breadcrumbs/>
                 <Preferences/>
                 <Toolbar/>
+                <Palette/>
             </MuiThemeProvider>
         </div>
     )
