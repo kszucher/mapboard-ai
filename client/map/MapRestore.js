2b3d759fda623c64a2ffe6625028276de34f122f
Krisztian Szucher <krisztian@mindboard.io>
2021-03-20 23:52:21 +0100

    preventing nonselection event

--- client/map/MapRestore.js
+++ client/map/MapRestore.js
@@ -1,11 +1,21 @@
 import {getDefaultNode} from "../node/Node";
 
+let selectionFound = 0;
+
 export const mapRestore = {
     start: (r) => {
         mapRestore.iterate(r);
+        if (!selectionFound) {
+            r.selected = 1;
+            console.log('MAP RESTORED AFTER NO SELECTION')
+        }
     },
 
     iterate: (cm) => {
+        if (cm.selected) {
+            selectionFound = 1;
+        }
+
         if (cm.type === 'cell' && !cm.s.length) {
             cm.s.push(getDefaultNode());
         }
