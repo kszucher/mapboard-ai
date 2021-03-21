2eddb7f01ad7704ec3f331f4a8305f32d7229774
Krisztian Szucher <krisztian@mindboard.io>
2021-03-21 14:42:05 +0100

    conditional mapChangeProp

--- client/map/MapChangeProp.js
+++ client/map/MapChangeProp.js
@@ -1,12 +1,18 @@
 export const mapChangeProp = {
-    start: (cm, prop, val) => {
-        mapChangeProp.iterate(cm, prop, val);
+    start: (cm, prop, val, cond) => {
+        mapChangeProp.iterate(cm, prop, val, cond);
     },
 
-    iterate: (cm, prop, val) => {
-        cm[prop] = val;
-        cm.d.map(i => mapChangeProp.iterate(i, prop, val));
-        cm.s.map(i => mapChangeProp.iterate(i, prop, val));
-        cm.c.map(i => i.map(j => mapChangeProp.iterate(j, prop, val)));
+    iterate: (cm, prop, val, cond) => {
+        if (cond === 's') {
+            if (cm.type === 'struct' && !cm.hasCell) {
+                cm[prop] = val;
+            }
+        } else {
+            cm[prop] = val;
+        }
+        cm.d.map(i => mapChangeProp.iterate(i, prop, val, cond));
+        cm.s.map(i => mapChangeProp.iterate(i, prop, val, cond));
+        cm.c.map(i => i.map(j => mapChangeProp.iterate(j, prop, val, cond)));
     }
 };
