3933c9bd5363af66f674ff6bf52225980c23cae8
Krisztian Szucher <krisztian@mindboard.io>
2021-04-10 22:00:33 +0200

    25 50 75

--- client/component/Palette.js
+++ client/component/Palette.js
@@ -5,10 +5,10 @@ import {nodeDispatch} from "../core/NodeFlow";
 import {checkPop, push, redraw} from "../core/MapFlow";
 
 const colorList = [
-    ['#000000', '#434343', '#666666', '#999999', '#b7b7b7', '#cccccc', '#d9d9d9', '#efefef', '#f3f3f3', '#ffffff', '#d4ebfe', '#f6e5d4', '#d4f6d4'],
-    ['#e6b8af', '#f4cccc', '#fce5cd', '#fff2cc', '#d9ead3', '#d0e0e3', '#c9daf8', '#cfe2f3', '#d9d2e9', '#ead1dc', '#990000', '#000099', '#fbfafc'],
-    ['#cc4125', '#e06666', '#f6b26b', '#ffd966', '#93c47d', '#76a5af', '#6d9eeb', '#6fa8dc', '#8e7cc3', '#c27ba0', '#d5802a', '#1c8e1c', '#8e1c8e'],
-    ['#85200c', '#990000', '#b45f06', '#bf9000', '#38761d', '#134f5c', '#1155cc', '#0b5394', '#351c75', '#741b47', '#999999', '#bbbbbb', '#dddddd'],
+    // ['#000000', '#434343', '#666666', '#999999', '#b7b7b7', '#cccccc', '#d9d9d9', '#efefef', '#f3f3f3', '#ffffff', '#d4ebfe', '#f6e5d4', '#d4f6d4'],
+    // ['#e6b8af', '#f4cccc', '#fce5cd', '#fff2cc', '#d9ead3', '#d0e0e3', '#c9daf8', '#cfe2f3', '#d9d2e9', '#ead1dc', '#990000', '#000099', '#fbfafc'],
+    ['#cc4125', '#e06666', '#f6b26b', '#ffd966', '#93c47d', '#76a5af', '#6d9eeb', '#6fa8dc', '#8e7cc3', '#c27ba0',/* '#d5802a', '#1c8e1c', '#8e1c8e'*/],
+    // ['#85200c', '#990000', '#b45f06', '#bf9000', '#38761d', '#134f5c', '#1155cc', '#0b5394', '#351c75', '#741b47', '#999999', '#bbbbbb', '#dddddd'],
 ];
 
 export function Palette () {
@@ -52,11 +52,11 @@ export function Palette () {
 
     return (
         <div id = 'palette'>
-            <svg viewBox='0 0 460 180'>
+            <svg viewBox='0 0 350 48'>
                 {colorList.map((iEl, i) => (iEl.map((jEl, j) => (
                     <circle
-                        cx={40 + j * 32}
-                        cy={40 + i * 32}
+                        cx={32 + j * 32}
+                        cy={24 + i * 32}
                         r={12}
                         key={'key' + i * 10 + j}
                         fill={jEl}
