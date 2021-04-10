2f18c8ae91b799ca3a425d3de017f0820da28228
Krisztian Szucher <krisztian@mindboard.io>
2021-04-10 13:51:52 +0200

    editor layout change

---
+++ client/component-styled/StyledButtonGroup.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+}));
+
+export default function StyledButtonGroup(arg) {
+
+    const name = arg.input[0];
+    const currValue = arg.input[1];
+    const action =  arg.input[2];
+    const valueList = arg.input[3];
+
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+                {valueList.map((name, index) =>
+                    <Button onClick={e=>action(valueList[index])} key={index}>{name}</Button>
+                )}
+            </ButtonGroup>
+        </div>
+    );
+}
--- client/component/Preferences.js
+++ client/component/Preferences.js
@@ -1,27 +1,27 @@
 import React, {useContext} from 'react';
 import {Context} from "../core/Store";
-import {StyledSelect} from "../component-styled/StyledSelect";
+import StyledButtonGroup from "../component-styled/StyledButtonGroup";
 import '../component-css/Preferences.css'
 
 export function Preferences () {
     const [state, dispatch] = useContext(Context);
     const {density, alignment, fontSize, lineWidth, lineType, colorMode} = state;
 
-    const setDensity =        e => dispatch({type: 'SET_DENSITY',     payload: e.target.value});
-    const setAlignment =      e => dispatch({type: 'SET_ALIGNMENT',   payload: e.target.value});
-    const setFontSize =       e => dispatch({type: 'SET_FONT_SIZE',   payload: e.target.value});
-    const setLineWidth =      e => dispatch({type: 'SET_LINE_WIDTH',  payload: e.target.value});
-    const setLineType =       e => dispatch({type: 'SET_LINE_TYPE',   payload: e.target.value});
-    const setColorMode =      e => dispatch({type: 'SET_COLOR_MODE',  payload: e.target.value});
+    const setDensity =        e => dispatch({type: 'SET_DENSITY',     payload: e});
+    const setAlignment =      e => dispatch({type: 'SET_ALIGNMENT',   payload: e});
+    const setFontSize =       e => dispatch({type: 'SET_FONT_SIZE',   payload: e});
+    const setLineWidth =      e => dispatch({type: 'SET_LINE_WIDTH',  payload: e});
+    const setLineType =       e => dispatch({type: 'SET_LINE_TYPE',   payload: e});
+    const setColorMode =      e => dispatch({type: 'SET_COLOR_MODE',  payload: e});
 
     return (
         <div id = 'preferences'>
-            <StyledSelect input = {['Map Density',      density,    setDensity,     ['small', 'large']]}/>
-            <StyledSelect input = {['Map Alignment',    alignment,  setAlignment,   ['adaptive', 'symmetrical']]}/>
-            <StyledSelect input = {['Font Size',        fontSize,   setFontSize,    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']]}/>
-            <StyledSelect input = {['Line Width',       lineWidth,  setLineWidth,   ['p1', 'p2', 'p3']]}/>
-            <StyledSelect input = {['Line Type',        lineType,   setLineType,    ['bezier', 'bezierCircle', 'edge']]}/>
-            <StyledSelect input = {['Color Mode',       colorMode,  setColorMode,   ['text', 'border', 'highlight', 'line', 'cellFrame']]}/>
+            <StyledButtonGroup input = {['Map Density',      density,    setDensity,     ['small', 'large']]}/>
+            <StyledButtonGroup input = {['Map Alignment',    alignment,  setAlignment,   ['adaptive', 'symmetrical']]}/>
+            <StyledButtonGroup input = {['Font Size',        fontSize,   setFontSize,    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']]}/>
+            <StyledButtonGroup input = {['Line Width',       lineWidth,  setLineWidth,   ['p1', 'p2', 'p3']]}/>
+            <StyledButtonGroup input = {['Line Type',        lineType,   setLineType,    ['bezier', 'bezierCircle', 'edge']]}/>
+            <StyledButtonGroup input = {['Color Mode',       colorMode,  setColorMode,   ['text', 'border', 'highlight', 'line', 'cellFrame']]}/>
         </div>
     );
 }
