86f675c966f8fc0e302cf669a4869b50e3cfd423
Krisztian Szucher <krisztian@mindboard.io>
2021-04-26 22:46:28 +0200

    reord

--- client/map/MapSvgVisualize.js
+++ client/map/MapSvgVisualize.js
@@ -41,58 +41,6 @@ export const mapSvgVisualize = {
                 fill: '#fbfafc',
             };
         }
-        // selectionRect
-        if (cm.selectionRect.length) {
-            svgElementData[4].selectionRect = {
-                type: 'rect',
-                x: cm.selectionRect[0],
-                y: cm.selectionRect[1],
-                width: cm.selectionRect[2],
-                height: cm.selectionRect[3],
-                rx: 8,
-                ry: 8,
-                fill: '#5f0a87',
-                fillOpacity: 0.05,
-                strokeWidth: 2,
-                preventTransition: 1,
-            };
-        }
-        // moveLine, moveRect
-        if (cm.moveData.length) {
-            let x1, y1, c1x, c1y, c2x, c2y, x2, y2;
-            let deltaX = cm.moveData[2] - cm.moveData[0];
-            let deltaY = cm.moveData[3] - cm.moveData[1];
-            // the elegant solution would be the inheritance of the target line type
-            x1 = cm.moveData[0];
-            y1 = cm.moveData[1];
-            c1x = cm.moveData[0] + deltaX / 4;
-            c1y = cm.moveData[1];
-            c2x = cm.moveData[0] + deltaX / 4;
-            c2y = cm.moveData[1] + deltaY;
-            x2 = cm.moveData[2];
-            y2 = cm.moveData[3];
-            svgElementData[4].moveLine = {
-                type: 'path',
-                path: `M${x1},${y1} C${c1x},${c1y} ${c2x},${c2y} ${x2},${y2}`,
-                stroke: '#5f0a87',
-                strokeWidth: 1,
-                preventTransition: 1,
-            }
-            svgElementData[4].moveRect = {
-                type: 'rect',
-                x: cm.moveData[2] - 10,
-                y: cm.moveData[3] - 10,
-                width: 20,
-                height: 20,
-                rx: 8,
-                ry: 8,
-                fill: '#fbfafc',
-                fillOpacity: 1,
-                stroke: '#5f0a87',
-                strokeWidth: 5,
-                preventTransition: 1,
-            };
-        }
         // connectionLine
         if (!cm.isRoot && !cm.isRootChild && cm.parentType !== 'cell' && (
             cm.type === 'struct' && !cm.hasCell ||
@@ -282,6 +230,58 @@ export const mapSvgVisualize = {
                     fill: fill,
                 };
             }
+        }
+        // moveLine, moveRect
+        if (cm.moveData.length) {
+            let x1, y1, c1x, c1y, c2x, c2y, x2, y2;
+            let deltaX = cm.moveData[2] - cm.moveData[0];
+            let deltaY = cm.moveData[3] - cm.moveData[1];
+            // the elegant solution would be the inheritance of the target line type
+            x1 = cm.moveData[0];
+            y1 = cm.moveData[1];
+            c1x = cm.moveData[0] + deltaX / 4;
+            c1y = cm.moveData[1];
+            c2x = cm.moveData[0] + deltaX / 4;
+            c2y = cm.moveData[1] + deltaY;
+            x2 = cm.moveData[2];
+            y2 = cm.moveData[3];
+            svgElementData[4].moveLine = {
+                type: 'path',
+                path: `M${x1},${y1} C${c1x},${c1y} ${c2x},${c2y} ${x2},${y2}`,
+                stroke: '#5f0a87',
+                strokeWidth: 1,
+                preventTransition: 1,
+            }
+            svgElementData[4].moveRect = {
+                type: 'rect',
+                x: cm.moveData[2] - 10,
+                y: cm.moveData[3] - 10,
+                width: 20,
+                height: 20,
+                rx: 8,
+                ry: 8,
+                fill: '#fbfafc',
+                fillOpacity: 1,
+                stroke: '#5f0a87',
+                strokeWidth: 5,
+                preventTransition: 1,
+            };
+        }
+        // selectionRect
+        if (cm.selectionRect.length) {
+            svgElementData[4].selectionRect = {
+                type: 'rect',
+                x: cm.selectionRect[0],
+                y: cm.selectionRect[1],
+                width: cm.selectionRect[2],
+                height: cm.selectionRect[3],
+                rx: 8,
+                ry: 8,
+                fill: '#5f0a87',
+                fillOpacity: 0.05,
+                strokeWidth: 2,
+                preventTransition: 1,
+            };
         }
         let svgGroupList = [];
         if (!mapSvgData.hasOwnProperty(cm.svgId) ||
