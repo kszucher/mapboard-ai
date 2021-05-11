4e2509a388019fbb2d113a4aafb27e3efab83629
Krisztian Szucher <krisztian@mindboard.io>
2021-05-11 23:37:37 +0200

    less verbose

--- client/core/NodeFlow.js
+++ client/core/NodeFlow.js
@@ -385,14 +385,8 @@ function nodeReducer(action, payload) {
             let field = {
                 line: 'lineColor',
                 text: 'sTextColor',
-                fill: lm.selection === 's'
-                    ? 'ellipseNodeFillColor'
-                    : 'ellipseBranchFillColor',
-                border: lm.hasCell
-                    ? 'cBorderColor'
-                    : lm.selection === 's'
-                        ? 'ellipseNodeBorderColor'
-                        :'ellipseBranchBorderColor'
+                fill: lm.selection === 's' ? 'ellipseNodeFillColor' : 'ellipseBranchFillColor',
+                border: lm.hasCell ? 'cBorderColor' : lm.selection === 's' ? 'ellipseNodeBorderColor' :'ellipseBranchBorderColor'
             }[payload.formatMode]
             for (let i = 0; i < sc.structSelectedPathList.length; i++) {
                 let cm = mapref(sc.structSelectedPathList[i]);
