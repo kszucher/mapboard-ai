4244fa4e1091655d9f6c38cd4a0c1c31bb20d754
Krisztian Szucher <krisztian@mindboard.io>
2021-05-16 14:10:38 +0200

    tabs simplified

--- client/component/Tabs.js
+++ client/component/Tabs.js
@@ -1,49 +1,12 @@
-import React, {useContext, useState} from 'react';
-import PropTypes from 'prop-types';
+import React, {useContext} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import {Context} from "../core/Store";
-
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`vertical-tabpanel-${index}`}
-            aria-labelledby={`vertical-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box p={3}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-};
-
-// function a11yProps(index) {
-//     return {
-//         id: `vertical-tab-${index}`,
-//         'aria-controls': `vertical-tabpanel-${index}`,
-//     };
-// }
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-        // backgroundColor: theme.palette.background.paper,
         display: 'flex-start',
     },
     tabs: {
@@ -63,15 +26,11 @@ export default function VerticalTabs() {
     const [state, dispatch] = useContext(Context);
     const {mapSelected, mapNameList} = state;
 
-    const handleChange = (e, value) =>  {
-        dispatch({type: 'OPEN_MAP', payload: {source: 'TAB', value}});
-    };
+    const handleChange = (e, value) =>  {dispatch({type: 'OPEN_MAP', payload: {source: 'TAB', value}})};
 
     return (
         <div style={{
             position: 'fixed',
-            // top: '50%',
-            // transform: 'translate(0, -50%)',
             top: 48*2,
             width: 216,
             backgroundColor: '#fbfafc',
