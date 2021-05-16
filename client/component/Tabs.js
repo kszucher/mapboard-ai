8e15f3a6920ae109ea87c7df0bfe8bf0062d2c6d
Krisztian Szucher <krisztian@mindboard.io>
2021-05-16 16:50:06 +0200

    styled tabs

--- client/component/Tabs.js
+++ client/component/Tabs.js
@@ -4,6 +4,29 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import {Context} from "../core/Store";
 
+export default function VerticalTabs() {
+    const [state, dispatch] = useContext(Context);
+    const {mapSelected, mapNameList} = state;
+    const handleChange = (e, value) =>  {dispatch({type: 'OPEN_MAP', payload: {source: 'TAB', value}})};
+
+    return (
+        <div style={{
+            position: 'fixed',
+            top: 48*2,
+            width: 216,
+            backgroundColor: '#fbfafc',
+            borderTopRightRadius: '16px',
+            borderBottomRightRadius: '16px',
+            borderWidth: '1px',
+            borderStyle: 'solid',
+            borderColor: '#dddddd',
+            borderLeft: 0
+        }}>
+            <StyledTabs valueList={mapNameList} value={mapSelected} onChange={handleChange}/>
+        </div>
+    );
+}
+
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
@@ -21,45 +44,28 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function VerticalTabs() {
+const StyledTabs = (arg) => {
     const classes = useStyles();
-    const [state, dispatch] = useContext(Context);
-    const {mapSelected, mapNameList} = state;
-
-    const handleChange = (e, value) =>  {dispatch({type: 'OPEN_MAP', payload: {source: 'TAB', value}})};
-
+    const {valueList, value, onChange} = arg;
     return (
-        <div style={{
-            position: 'fixed',
-            top: 48*2,
-            width: 216,
-            backgroundColor: '#fbfafc',
-            borderTopRightRadius: '16px',
-            borderBottomRightRadius: '16px',
-            borderWidth: '1px',
-            borderStyle: 'solid',
-            borderColor: '#dddddd',
-            borderLeft: 0
-        }}>
-            <div className={classes.root}>
-                <Tabs
-                    classes={{
-                        indicator: classes.indicator
-                    }}
-                    orientation="vertical"
-                    variant="scrollable"
-                    aria-label="Vertical tabs example"
-                    className={classes.tabs}
-                    value={mapSelected}
-                    onChange={handleChange}
-                    indicatorColor="primary">
-                    {mapNameList.map((name, index) => (
-                        <Tab
-                            label={name}
-                            key={index}/>
-                    ))}>
-                </Tabs>
-            </div>
+        <div className={classes.root}>
+            <Tabs
+                classes={{
+                    indicator: classes.indicator
+                }}
+                orientation="vertical"
+                variant="scrollable"
+                aria-label="Vertical tabs example"
+                className={classes.tabs}
+                value={value}
+                onChange={onChange}
+                indicatorColor="primary">
+                {valueList.map((name, index) => (
+                    <Tab
+                        label={name}
+                        key={index}/>
+                ))}>
+            </Tabs>
         </div>
-    );
+    )
 }
