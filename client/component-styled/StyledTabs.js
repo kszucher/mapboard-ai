fb09a57776059c8f77698ac1388f75d96d5536d5
Krisztian Szucher <krisztian@mindboard.io>
2021-05-16 17:36:52 +0200

    conditional styling where it is appropriate

--- client/component-styled/StyledTabs.js
+++ client/component-styled/StyledTabs.js
@@ -5,22 +5,31 @@ import React from "react";
 
 export default function StyledTabs (arg) {
     const {valueList, value, onChange, orientation, component} = arg;
-    const classes = makeStyles((theme) => ({
-        root: {
-            flexGrow: 1,
-            display: 'flex-start',
-        },
-        tabs: {
-            borderRight: `0px solid ${theme.palette.divider}`,
-        },
-        indicator: {
-            left: "0px",
-            width: "8px",
-            borderTopRightRadius: "16px",
-            borderBottomRightRadius: "16px",
-            backgroundImage: "linear-gradient(180deg, #a4508b 0%, #5f0a87 74%)",
-        },
-    }))()
+    const getStyle = (theme) => {
+        if (component === 'mapSelector') {
+            return {
+                root: {
+                    flexGrow: 1,
+                    display: 'flex-start',
+                },
+                tabs: {
+                    borderRight: `0px solid ${theme.palette.divider}`,
+                },
+                indicator: {
+                    left: "0px",
+                    width: "8px",
+                    borderTopRightRadius: "16px",
+                    borderBottomRightRadius: "16px",
+                    backgroundImage: "linear-gradient(180deg, #a4508b 0%, #5f0a87 74%)",
+                },
+            }
+        } else if (component === 'sign') {
+            return {
+                
+            }
+        }
+    }
+    const classes = makeStyles((theme) => (getStyle(theme)))()
     return (
         <div className={classes.root}>
             <Tabs
