a53a73a2af9cf3fc7f00f4c666d391472cd6f37d
Krisztian Szucher <krisztian@mindboard.io>
2021-05-16 14:13:31 +0200

    breadcrumbs simplified

--- client/component/Breadcrumbs.js
+++ client/component/Breadcrumbs.js
@@ -1,28 +1,14 @@
 import React, {useContext} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import {Context} from "../core/Store";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > * + *': {
-            marginTop: theme.spacing(2),
-        },
-    },
-}));
-
 export default function WorkspaceBreadcrumbs() {
-    const classes = useStyles();
-
     const [state, dispatch] = useContext(Context);
     const {breadcrumbsHistory} = state;
 
-    const handleClick = index => event => {
-        event.preventDefault();
-        dispatch({type: 'OPEN_MAP', payload: {source: 'BREADCRUMBS', index}});
-    };
+    const handleClick = index => event => {dispatch({type: 'OPEN_MAP', payload: {source: 'BREADCRUMBS', index}})};
 
     return (
         <div style={{
@@ -42,19 +28,17 @@ export default function WorkspaceBreadcrumbs() {
             borderColor: '#9040b8',
             borderTop: 0,
         }}>
-            <div className={classes.root}>
-                <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
-                    {breadcrumbsHistory.map((currElement, index) => (
-                        <Link
-                            color="inherit"
-                            href="/"
-                            onClick={handleClick(index)}
-                            key={index}>
-                            {currElement.mapName}
-                        </Link>
-                    ))}>
-                </Breadcrumbs>
-            </div>
+            <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+                {breadcrumbsHistory.map((currElement, index) => (
+                    <Link
+                        color="inherit"
+                        href="/"
+                        onClick={handleClick(index)}
+                        key={index}>
+                        {currElement.mapName}
+                    </Link>
+                ))}>
+            </Breadcrumbs>
         </div>
     );
 }
