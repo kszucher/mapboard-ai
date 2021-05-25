de63c51a37ad66f501ebc24585a984c8f09a07e6
Krisztian Szucher <krisztian@mindboard.io>
2021-05-25 21:16:59 +0200

    slack invite included

--- server/js/msServerApp.js
+++ server/js/msServerApp.js
@@ -125,9 +125,10 @@ async function sendResponse(c2s) {
                                 <p>Hello ${userName}!</p>
                                 <p>Welcome to MindBoard!<br>You can complete your registration using the following code:</p>
                                 <p>${confirmationCode}</p>
+                                <p>You can also join the conversation, propose features and get product news here:<br>
+                                <a href="MindBoard Slack">https://join.slack.com/t/mindboardio/shared_invite/zt-qunqabbo-fE_2dnrU7GPuEiDsAy6L~A</a></p>
                                 <p>Cheers,<br>Krisztian from MindBoard</p>
                             `
-                        // TODO send out slack invite https://join.slack.com/t/mindboardio/shared_invite/zt-qunqabbo-fE_2dnrU7GPuEiDsAy6L~A
                     });
                     currUser = await collectionUsers.insertOne({
                         email: userEmail,
