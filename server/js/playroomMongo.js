cdd4df8fa53385f6ac6ace05a0b9970629f21f23
Krisztian Szucher <krisztian@mindboard.io>
2021-06-02 22:49:01 +0200

    playroomMongo changed to be able to solve specific commands

--- server/js/playroomMongo.js
+++ server/js/playroomMongo.js
@@ -2,111 +2,115 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://mindboard-server:3%21q.FkpzkJPTM-Q@cluster0-sg0ny.mongodb.net/test?retryWrites=true&w=majority";
 const ObjectId = require('mongodb').ObjectId;
 
-async function mongoFunction() {
-    const client = new MongoClient(uri, { useNewUrlParser: true });
+async function filterStage (collectionMaps, params) {
+    let filter = [];
+    switch (params.filterMode) {
+        case 'all': {
+            await collectionMaps.find({}).forEach( doc => {
+                filter.push({
+                    _id : doc._id,
+                });
+            });
+            break;
+        }
+        case 'filtered': {
+            let dataElemField = 'linkType';
+            await collectionMaps.aggregate(
+                [
+                    {
+                        $project: {
+                            data: {
+                                $filter: {
+                                    input: "$data",
+                                    as: "dataElem",
+                                    cond: {
+                                        [`$${params.cond}`]: [
+                                            "$$dataElem." + dataElemField,
+                                            `${params.condVal}`
+                                        ]}
+                                }
+                            }
+                        }
+                    },
+                    {
+                        $unwind: {
+                            path: "$data"
+                        }
+                    }
+                ]
+            ).forEach( doc => {
+                filter.push({
+                    _id : doc._id,
+                    path: doc.data['path'],
+                    value : doc.data[dataElemField]
+                });
+            });
+        }
+    }
+    return filter;
+}
 
+async function updateStage (params) {
+    switch (updateStage) {
+        case 'setMapProp': {
+            for (let i = 0; i < filter.length; i++) {
+                await collectionMaps.updateOne(
+                    {_id: filter[i]._id},
+                    {$set: {"MAPROPNAME" : "MAPROPVALUE"}}
+                );
+            }
+            break;
+        }
+        case 'unsetMapProp': {
+            for (let i = 0; i < filter.length; i++) {
+                await collectionMaps.updateOne(
+                    {_id: filter[i]._id},
+                    {$unset: {"MAPROPNAME" : ""}}
+                );
+            }
+            // note: unable to use aggregation unset from mongodb v4.2, as mongodb free tier only supports mongodb 4.0
+            break;
+        }
+        case 'setNodeProp': {
+            for (let i = 0; i < filter.length; i++) {
+                await collectionMaps.updateOne(
+                    { _id: filter[i]._id },
+                    { $set: { "data.$[elem].NODEPROPNAME" : "NODEPROPVALUE" } },
+                    { "arrayFilters": [{ "elem.path": filter[i].path }], "multi": true }
+                )
+            }
+            break;
+        }
+        case 'unsetNodeProp': {
+            for (let i = 0; i < filter.length; i++) {
+                await collectionMaps.updateOne(
+                    { _id: filter[i]._id },
+                    { $unset: { "data.$[elem].NODEPROPNAME" : "" } },
+                    { "arrayFilters": [{ "elem.path": filter[i].path }], "multi": true }
+                )
+            }
+            break;
+        }
+    }
+}
+
+async function mongoFunction(cmd) {
+    const client = new MongoClient(uri, { useNewUrlParser: true });
     try {
         await client.connect();
-
         const collectionMaps =      client.db("app").collection("maps");
         const collectionUsers =     client.db("app").collection("users");
 
-        let filter = [];
-
-        // FIND STAGE
-        filterOp = '';
-        switch (filterOp) {
-            case 'allMap': {
-                await collectionMaps.find({}).forEach( doc => {
-                    filter.push({
-                        _id : doc._id,
-                    });
-                });
-                break;
-            }
-            case 'existingNodeProp': {
-                let dataElemField = 'NODPROPNAME';
-                await collectionMaps.aggregate(
-                    [
-                        {
-                            $project: {
-                                data: {
-                                    $filter: {
-                                        input: "$data",
-                                        as: "dataElem",
-                                        cond: {
-                                            $ifNull: [
-                                                "$$dataElem." + dataElemField,
-                                                false
-                                            ]}
-                                    }
-                                }
-                            }
-                        },
-                        {
-                            $unwind: {
-                                path: "$data"
-                            }
-                        }
-                    ]
-                ).forEach( doc => {
-                    filter.push({
-                        _id : doc._id,
-                        path: doc.data['path'],
-                        value : doc.data[dataElemField]
-                    });
-                });
-            }
-        }
-
-        // console.log(filter);
-        console.log(filter.length);
-
-        // MODIFY STAGE
-        modifyOp = '';
-        switch (modifyOp) {
-            case 'setMapProp': {
-                for (let i = 0; i < filter.length; i++) {
-                    await collectionMaps.updateOne(
-                        {_id: filter[i]._id},
-                        {$set: {"MAPROPNAME" : "MAPROPVALUE"}}
-                    );
-                }
-                break;
-            }
-            case 'unsetMapProp': {
-                for (let i = 0; i < filter.length; i++) {
-                    await collectionMaps.updateOne(
-                        {_id: filter[i]._id},
-                        {$unset: {"MAPROPNAME" : ""}}
-                        );
-                }
-                // note: unable to use aggregation unset from mongodb v4.2, as mongodb free tier only supports mongodb 4.0
-                break;
-            }
-            case 'setNodeProp': {
-                for (let i = 0; i < filter.length; i++) {
-                    await collectionMaps.updateOne(
-                        { _id: filter[i]._id },
-                        { $set: { "data.$[elem].NODEPROPNAME" : "NODEPROPVALUE" } },
-                        { "arrayFilters": [{ "elem.path": filter[i].path }], "multi": true }
-                    )
-                }
-                break;
-            }
-            case 'unsetNodeProp': {
-                for (let i = 0; i < filter.length; i++) {
-                    await collectionMaps.updateOne(
-                        { _id: filter[i]._id },
-                        { $unset: { "data.$[elem].NODEPROPNAME" : "" } },
-                        { "arrayFilters": [{ "elem.path": filter[i].path }], "multi": true }
-                    )
-                }
+        switch (cmd) {
+            case 'findDeleteUnusedMaps': {
+                let maps = await filterStage(collectionMaps, {filterMode: 'all'});
+                console.log(maps.length);
+                let mapsThatHaveInternalLink = await filterStage(collectionMaps, {filterMode: 'filtered', cond: 'eq', condVal: 'internal'})
+                console.log(mapsThatHaveInternalLink.length);
                 break;
             }
         }
     }
-
     catch (err) {
         console.log('error');
         console.log(err.stack);
@@ -114,4 +118,4 @@ async function mongoFunction() {
     client.close();
 }
 
-mongoFunction();
+mongoFunction('findDeleteUnusedMaps');
