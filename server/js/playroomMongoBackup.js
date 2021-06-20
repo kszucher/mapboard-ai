5a7739d584fb14fffd99887fe8a6e2ebc84a4686
Krisztian Szucher <krisztian@mindboard.io>
2021-06-20 17:07:39 +0200

    mongo backup operations done

--- server/js/playroomMongoBackup.js
+++ server/js/playroomMongoBackup.js
@@ -1,68 +1,44 @@
 const spawn = require('await-spawn')
 
-const main = async () => {
-    try {
-        const bl = await spawn('ls', ['-al'])
-        console.log(bl.toString())
-    } catch (e) {
-        console.log(e.stderr.toString())
+const mongoTransfer = async (mode) => {
+    switch (mode) {
+        case 'prod2dev': {
+            await mongoProcess('mongodump', {source: 'app_prod'});
+            await mongoProcess('mongorestore', {source:'app_prod', target:'app_dev'});
+            break;
+        }
+        case 'dev2prod': {
+            await mongoProcess('mongodump', {source: 'app_dev'});
+            await mongoProcess('mongorestore', {source:'app_dev', target:'app_prod'});
+        }
     }
 }
 
-const mongoProcess = async (cmd, db, path) => {
-    console.log(`process ${cmd} started`);
+const mongoProcess = async (mongoCmd, endPoints) => {
+    let baseUri = 'mongodb+srv://mindboard-server:3%21q.FkpzkJPTM-Q@cluster0-sg0ny.mongodb.net'
+    let basePath = 'C:/Users/Kryss/Dropbox/mindboard/mongobackup';
+    const mongoParams = [
+        `--uri=${baseUri}/${mongoCmd === 'mongodump' ? endPoints.source : endPoints.target}`,
+        `--archive=${basePath}/${endPoints.source}`,
+        '--gzip'
+    ]
+    if (mongoCmd === 'mongorestore') {
+        mongoParams.push(`--nsFrom=${endPoints.source}.*`);
+        mongoParams.push(`--nsTo=${endPoints.target}.*`);
+    }
+    console.log(mongoParams)
+    await spawnProcess(mongoCmd, mongoParams);
+}
 
+const spawnProcess = async (mongoCmd, mongoParams) => {
+    console.log(`process ${mongoCmd} started`);
     try {
-        const bl = await spawn(cmd, [
-            `--uri=mongodb+srv://mindboard-server:3%21q.FkpzkJPTM-Q@cluster0-sg0ny.mongodb.net/${db}`,
-            `--archive=${path}`,
-            '--gzip',
-        ])
+        const bl = await spawn(mongoCmd, mongoParams)
         // console.log(bl.toString())
     } catch (e) {
         console.log(e.stderr.toString())
     }
-    console.log(`process ${cmd} fininshed`);
-
-
-    // const child = await spawn(cmd, [
-    //     `--uri=mongodb+srv://mindboard-server:3%21q.FkpzkJPTM-Q@cluster0-sg0ny.mongodb.net/${db}`,
-    //     `--archive=${path}`,
-    //     '--gzip',
-    // ]);
-    //
-    // child.stdout.on('data', (data) => {
-    //     console.log('stdout:n', data);
-    // });
-    // child.stderr.on('data', (data) => {
-    //     console.log('stderr:n', Buffer.from(data).toString());
-    // });
-    // child.on('error', (error) => {
-    //     console.log('error:n', error);
-    // });
-    // child.on('exit', (code, signal) => {
-    //     if (code) {
-    //         console.log('Process exit with code:', code);
-    //     } else if (signal) {
-    //         console.log('Process killed with signal:', signal);
-    //     } else
-    //         console.log(`process ${cmd} completed`);
-    // });
-}
-
-async function mongoTransfer(mode) {
-    let basePath = 'C:/Users/Kryss/Dropbox/mindboard/mongobackup';
-    switch (mode) {
-        case 'prod2dev': {
-            await mongoProcess('mongodump', 'app_prod', basePath + '/app_prod');
-            await mongoProcess('mongorestore', 'app_dev', basePath + '/app_prod');
-            break;
-        }
-        case 'dev2prod': {
-            await mongoProcess('mongodump', 'app_dev', basePath + '/app_dev');
-            await mongoProcess('mongorestore', 'app_prod', basePath + '/app_dev');
-        }
-    }
+    console.log(`process ${mongoCmd} fininshed`);
 }
 
 mongoTransfer('prod2dev');
