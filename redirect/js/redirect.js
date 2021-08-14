a3903f20ecea7362a770d96bd0bb133ed7c90614
Krisztian Szucher <krisztian@mindboard.io>
2021-08-14 16:10:13 +0200

    slight changes

--- redirect/js/redirect.js
+++ redirect/js/redirect.js
@@ -3,12 +3,11 @@ const app = express();
 const port = process.env.PORT || 8082;
 
 app.get("/", (req, res) => {
-    res.status(200).type("html").send("<h1>Hello World!<h1>");
-});
+    res.redirect(301, "https://mapboard.io");});
 
-app.get("/google", (req, res) => {
-    res.redirect(301, "https://mapboard.io");
-});
+// app.get("/google", (req, res) => {
+//     res.redirect(301, "https://mapboard.io");
+// });
 
 app.use(function (req, res, next) {
     res.status(404).send("<h1>Sorry nothing found!<h1>");
