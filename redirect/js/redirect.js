19ed6e384e648534024a127128ccca23dab32b44
Krisztian Szucher <krisztian@mindboard.io>
2021-08-14 16:08:28 +0200

    different redirect js

--- redirect/js/redirect.js
+++ redirect/js/redirect.js
@@ -1,6 +1,19 @@
-var express = require('express')
-var app = express();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8082;
 
-app.use("/",function (req,res) {
-    res.redirect("https:/mapboard.io/");
+app.get("/", (req, res) => {
+    res.status(200).type("html").send("<h1>Hello World!<h1>");
+});
+
+app.get("/google", (req, res) => {
+    res.redirect(301, "https://mapboard.io");
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send("<h1>Sorry nothing found!<h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`);
 });
