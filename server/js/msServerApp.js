9063ad513b0e066b3a50abc3535cc006abaee518
Krisztian Szucher <krisztian@mindboard.io>
2021-08-17 22:41:24 +0200

    much better check for saving last map

--- server/js/msServerApp.js
+++ server/js/msServerApp.js
@@ -173,8 +173,9 @@ async function sendResponse(c2s) {
                 } else if (currUser.activationStatus === 'awaitingConfirmation') {
                     s2c = {cmd: 'signInFailIncompleteRegistration'}
                 } else {
-                    if (['openMapFromTab', 'openMapFromMap', 'openMapFromBreadcrumbs',
-                        'createMapInMap', 'createMapInTab', 'saveMap', 'importFrame', 'openFrame', 'duplicateFrame'].includes(c2s.serverCmd)) {
+                    if (c2s.serverPayload.hasOwnProperty('mapIdOut') &&
+                        c2s.serverPayload.hasOwnProperty('mapSourceOut') &&
+                        c2s.serverPayload.hasOwnProperty('mapStorageOut')) {
                         const {mapIdOut, mapSourceOut, mapStorageOut} = c2s.serverPayload;
                         if (mapSourceOut === 'data') {
                             await collectionMaps.updateOne({_id: ObjectId(mapIdOut)}, {$set: {data: mapStorageOut}});
