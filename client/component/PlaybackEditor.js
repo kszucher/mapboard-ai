24243a53bd1bb853f302452dccac0e2ab0a2441d
Krisztian Szucher <krisztian@mindboard.io>
2021-08-20 12:03:06 +0200

    stepper instead of slider

--- client/component/PlaybackEditor.js
+++ client/component/PlaybackEditor.js
@@ -1,15 +1,28 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {Context} from "../core/Store";
 import StyledButton from "../component-styled/StyledButton";
-import {List, ListItem, ListItemText, Slider} from "@material-ui/core";
+import {List, ListItem, ListItemText, MobileStepper, Slider, useTheme} from "@material-ui/core";
 import StyledButtonGroup from "../component-styled/StyledButtonGroup";
+import Button from "@material-ui/core/Button";
+import {KeyboardArrowLeft, KeyboardArrowRight} from "@material-ui/icons";
 
 export function PlaybackEditor () {
+    const theme = useTheme();
+
     const [state, dispatch] = useContext(Context)
     const {serverResponse, serverResponseCntr} = state
     const [frameLen, setFrameLen] = useState(0)
     const [frameSelection, setFrameSelection] = useState([])
-
+    const prevFrame = _ => {
+        let frameSelectedOut = frameSelection[0];
+        let frameSelected = frameSelectedOut > 0 ? frameSelectedOut - 1 : 0;
+        dispatch({type: 'OPEN_FRAME', payload: {frameSelected}})
+    }
+    const nextFrame = _ => {
+        let frameSelectedOut = frameSelection[0];
+        let frameSelected = frameSelectedOut === frameSelectedOut.length ? frameSelectedOut : frameSelectedOut + 1;
+        dispatch({type: 'OPEN_FRAME', payload: {frameSelected}})
+    }
     const importFrame = _ => {
         dispatch({type: 'IMPORT_FRAME'})
     }
@@ -66,17 +79,44 @@ export function PlaybackEditor () {
             borderBottom: 0
         }}>
             {frameLen > 0 &&
-            < Slider
-                defaultValue={1}
-                aria-labelledby="discrete-slider"
-                valueLabelDisplay="on"
-                step={1}
-                marks
-                min={1}
-                max={frameLen}
-                onChangeCommitted={(event, value) => openFrame(value - 1)}
+            // < Slider
+            //     defaultValue={1}
+            //     aria-labelledby="discrete-slider"
+            //     valueLabelDisplay="on"
+            //     step={1}
+            //     marks
+            //     min={1}
+            //     max={frameLen}
+            //     onChangeCommitted={(event, value) => openFrame(value - 1)}
+            // />
+
+
+            <MobileStepper
+                variant="dots"
+                steps={frameLen}
+                position="static"
+                activeStep={frameSelection[0]}
+                // className={classes.root}
+                nextButton={
+                    <Button size="small" onClick={nextFrame} disabled={frameSelection[0] === frameLen - 1}>
+                        Next Frame
+                        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                    </Button>
+                }
+                backButton={
+                    <Button size="small" onClick={prevFrame} disabled={frameSelection[0] === 0}>
+                        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                        Previous Frame
+                    </Button>
+                }
             />
+
+
             }
+
+
+
+
             <StyledButtonGroup action={importFrame} value={''} valueList={['import frame']}/>
             <StyledButtonGroup disabled={!frameSelection.length || !frameLen} action={deleteFrame} value={''} valueList={['delete frame']}/>
             <StyledButtonGroup disabled={!frameSelection.length || !frameLen} action={duplicateFrame} value={''} valueList={['duplicate frame']}/>
