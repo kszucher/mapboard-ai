08107352268dd88b08abb168a2f271482db91d94
Krisztian Szucher <krisztian@mindboard.io>
2021-08-20 12:27:47 +0200

    compression

--- client/component/PlaybackEditor.js
+++ client/component/PlaybackEditor.js
@@ -1,50 +1,24 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {Context} from "../core/Store";
 import StyledButton from "../component-styled/StyledButton";
-import {List, ListItem, ListItemText, MobileStepper, Slider, useTheme} from "@material-ui/core";
+import {MobileStepper} from "@material-ui/core";
 import StyledButtonGroup from "../component-styled/StyledButtonGroup";
 import Button from "@material-ui/core/Button";
 import {KeyboardArrowLeft, KeyboardArrowRight} from "@material-ui/icons";
 
 export function PlaybackEditor () {
-    const theme = useTheme();
-
     const [state, dispatch] = useContext(Context)
     const {serverResponse, serverResponseCntr} = state
     const [frameLen, setFrameLen] = useState(0)
     const [frameSelection, setFrameSelection] = useState([])
-    const prevFrame = _ => {
-        let frameSelectedOut = frameSelection[0];
-        let frameSelected = frameSelectedOut > 0 ? frameSelectedOut - 1 : 0;
-        dispatch({type: 'OPEN_FRAME', payload: {frameSelected}})
-    }
-    const nextFrame = _ => {
-        let frameSelectedOut = frameSelection[0];
-        let frameSelected = frameSelectedOut === frameSelectedOut.length ? frameSelectedOut : frameSelectedOut + 1;
-        dispatch({type: 'OPEN_FRAME', payload: {frameSelected}})
-    }
-    const importFrame = _ => {
-        dispatch({type: 'IMPORT_FRAME'})
-    }
-    const openFrame = (idx) => {
-        let frameSelected = idx;
-        dispatch({type: 'OPEN_FRAME', payload: {frameSelected}})
-    }
-    const deleteFrame = _=> {
-        if (frameLen > 0) {
-            let frameSelectedOut = frameSelection[0];
-            let frameSelected = frameSelectedOut > 0 ? frameSelectedOut - 1 : 0;
-            dispatch({type: 'DELETE_FRAME', payload: {frameSelectedOut, frameSelected}})
-        }
-    }
-    const duplicateFrame = _=> {
-        let frameSelectedOut = frameSelection[0];
-        let frameSelected = frameSelectedOut + 1;
-        dispatch({type: 'DUPLICATE_FRAME', payload: {frameSelectedOut, frameSelected}})
-    }
-    const closePlaybackEditor = _ => {
-        dispatch({type: 'CLOSE_PLAYBACK_EDITOR'})
-    }
+
+    const prevFrame = _ =>           dispatch({type: 'OPEN_FRAME', payload: {frameSelected: frameSelection[0] - 1}})
+    const nextFrame = _ =>           dispatch({type: 'OPEN_FRAME', payload: {frameSelected: frameSelection[0] + 1}})
+    const importFrame = _ =>         dispatch({type: 'IMPORT_FRAME'})
+    const openFrame = idx => {       dispatch({type: 'OPEN_FRAME', payload: {frameSelected: idx}})}
+    const deleteFrame = _=>          dispatch({type: 'DELETE_FRAME', payload: {frameSelectedOut: frameSelection[0], frameSelected: frameSelection[0] > 0 ? frameSelection[0] - 1 : 0}})
+    const duplicateFrame = _=>       dispatch({type: 'DUPLICATE_FRAME', payload: {frameSelectedOut: frameSelection[0], frameSelected: frameSelection[0] + 1}})
+    const closePlaybackEditor = _ => dispatch({type: 'CLOSE_PLAYBACK_EDITOR'})
 
     useEffect(() => {
         if (serverResponse.payload?.hasOwnProperty('frameLen')) {
@@ -55,68 +29,31 @@ export function PlaybackEditor () {
         }
     }, [serverResponseCntr])
 
-    const xWidth = 192;
-
     return (
         <div style={{
-            position: 'fixed',
-            left: '50%',
-            transform: 'translate(-50%)',
-            bottom: 0,
-            width: 1366 - 2*20,
-            display: 'flex',
-            alignItems: 'center',
-            flexWrap: 'wrap',
-            gap: 16,
-            padding: 20,
-
-            backgroundColor: '#fbfafc',
-            borderTopLeftRadius: '16px',
-            borderTopRightRadius: '16px',
-            borderWidth: '2px',
-            borderStyle: 'solid',
-            borderColor: '#9040b8',
-            borderBottom: 0
+            position: 'fixed', left: '50%', transform: 'translate(-50%)', bottom: 0, width: 1366 - 2*20, display: 'flex', alignItems: 'center', flexWrap: 'wrap', gap: 16, padding: 20,
+            backgroundColor: '#fbfafc', borderTopLeftRadius: '16px', borderTopRightRadius: '16px', borderWidth: '2px', borderStyle: 'solid', borderColor: '#9040b8', borderBottom: 0
         }}>
             {frameLen > 0 &&
-            // < Slider
-            //     defaultValue={1}
-            //     aria-labelledby="discrete-slider"
-            //     valueLabelDisplay="on"
-            //     step={1}
-            //     marks
-            //     min={1}
-            //     max={frameLen}
-            //     onChangeCommitted={(event, value) => openFrame(value - 1)}
-            // />
-
-
             <MobileStepper
                 variant="dots"
                 steps={frameLen}
                 position="static"
                 activeStep={frameSelection[0]}
-                // className={classes.root}
                 nextButton={
                     <Button size="small" onClick={nextFrame} disabled={frameSelection[0] === frameLen - 1}>
                         Next Frame
-                        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                        <KeyboardArrowRight />
                     </Button>
                 }
                 backButton={
                     <Button size="small" onClick={prevFrame} disabled={frameSelection[0] === 0}>
-                        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                        <KeyboardArrowLeft />
                         Previous Frame
                     </Button>
                 }
             />
-
-
             }
-
-
-
-
             <StyledButtonGroup action={importFrame} value={''} valueList={['import frame']}/>
             <StyledButtonGroup disabled={!frameSelection.length || !frameLen} action={deleteFrame} value={''} valueList={['delete frame']}/>
             <StyledButtonGroup disabled={!frameSelection.length || !frameLen} action={duplicateFrame} value={''} valueList={['duplicate frame']}/>
