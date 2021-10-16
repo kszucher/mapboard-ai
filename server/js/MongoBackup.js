fee6cae0d4e12941a3d446567e3ec5d699e62f01
Krisztian Szucher <krisztian@mindboard.io>
2021-10-16 16:44:49 +0200

    reord

--- server/js/MongoBackup.js
+++ server/js/MongoBackup.js
@@ -1,5 +1,33 @@
 const spawn = require('await-spawn')
 
+const spawnProcess = async (mongoCmd, mongoParams) => {
+    console.log(`process ${mongoCmd} started`);
+    try {
+        const bl = await spawn(mongoCmd, mongoParams)
+        // console.log(bl.toString())
+    } catch (e) {
+        console.log(e.stderr.toString())
+    }
+    console.log(`process ${mongoCmd} finished`);
+}
+
+const mongoProcess = async (mongoCmd, endPoints, date) => {
+    let baseUri = 'mongodb+srv://mindboard-server:3%21q.FkpzkJPTM-Q@cluster0-sg0ny.mongodb.net'
+    let basePath = 'C:/Users/Kryss/Dropbox/mindboard/mongobackup';
+    const mongoParams = [
+        `--uri=${baseUri}/${mongoCmd === 'mongodump' ? endPoints.source : endPoints.target}`,
+        `--archive=${basePath}/${endPoints.source}_${date}`,
+        '--gzip'
+    ]
+    if (mongoCmd === 'mongorestore') {
+        mongoParams.push(`--nsFrom=${endPoints.source}.*`);
+        mongoParams.push(`--nsTo=${endPoints.target}.*`);
+        mongoParams.push(`--drop`);
+    }
+    console.log(mongoParams)
+    await spawnProcess(mongoCmd, mongoParams);
+}
+
 const mongoTransfer = async (mode) => {
     var date = + new Date();
     switch (mode) {
@@ -22,34 +50,6 @@ const mongoTransfer = async (mode) => {
             break;
         }
     }
-}
-
-const mongoProcess = async (mongoCmd, endPoints, date) => {
-    let baseUri = 'mongodb+srv://mindboard-server:3%21q.FkpzkJPTM-Q@cluster0-sg0ny.mongodb.net'
-    let basePath = 'C:/Users/Kryss/Dropbox/mindboard/mongobackup';
-    const mongoParams = [
-        `--uri=${baseUri}/${mongoCmd === 'mongodump' ? endPoints.source : endPoints.target}`,
-        `--archive=${basePath}/${endPoints.source}_${date}`,
-        '--gzip'
-    ]
-    if (mongoCmd === 'mongorestore') {
-        mongoParams.push(`--nsFrom=${endPoints.source}.*`);
-        mongoParams.push(`--nsTo=${endPoints.target}.*`);
-        mongoParams.push(`--drop`);
-    }
-    console.log(mongoParams)
-    await spawnProcess(mongoCmd, mongoParams);
-}
-
-const spawnProcess = async (mongoCmd, mongoParams) => {
-    console.log(`process ${mongoCmd} started`);
-    try {
-        const bl = await spawn(mongoCmd, mongoParams)
-        // console.log(bl.toString())
-    } catch (e) {
-        console.log(e.stderr.toString())
-    }
-    console.log(`process ${mongoCmd} finished`);
 }
 
 mongoTransfer('prod2file2dev');
