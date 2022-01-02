44f0cd21438194b6e60093994af9f23e44c4ca3e
Krisztian Szucher <krisztian@mindboard.io>
2022-01-02 12:12:59 +0100

    group by condition

--- client/map/MapVisualizeSvg.js
+++ client/map/MapVisualizeSvg.js
@@ -20,15 +20,17 @@ export const mapVisualizeSvg = {
             animationInit = 'l';
         }
         let svgElementData = [{},{},{},{},{},{}];
-        let selfHadj = isOdd(cm.selfH) ? cm.selfH + 1 : cm.selfH;
-        let maxHadj = isOdd(cm.maxH) ? cm.maxH + 1 : cm.maxH;
-        let nsx = cm.path[3] ? cm.nodeEndX : cm.nodeStartX;
-        let nex = cm.path[3] ? cm.nodeStartX : cm.nodeEndX;
-        let nsy = cm.nodeY - selfHadj/2;
-        let ney = cm.nodeY + selfHadj/2;
-        let dir = cm.path[3] ? -1 : 1;
-        let r = 8;
+
+
+
+
+
+
         let conditions = resolveConditions(cm);
+
+
+
+
         if (conditions.backgroundRect) {
             svgElementData[0].backgroundRect = {
                 type: 'rect',
@@ -41,6 +43,77 @@ export const mapVisualizeSvg = {
                 fill: COLORS.MAP_BACKGROUND,
             };
         }
+        if (cm.moveData.length) {
+            let x1, y1, c1x, c1y, c2x, c2y, x2, y2;
+            let deltaX = cm.moveData[2] - cm.moveData[0];
+            let deltaY = cm.moveData[3] - cm.moveData[1];
+            // the elegant solution would be the inheritance of the target line type
+            x1 = cm.moveData[0];
+            y1 = cm.moveData[1];
+            c1x = cm.moveData[0] + deltaX / 4;
+            c1y = cm.moveData[1];
+            c2x = cm.moveData[0] + deltaX / 4;
+            c2y = cm.moveData[1] + deltaY;
+            x2 = cm.moveData[2];
+            y2 = cm.moveData[3];
+            svgElementData[5].moveLine = {
+                type: 'path',
+                path: `M${x1},${y1} C${c1x},${c1y} ${c2x},${c2y} ${x2},${y2}`,
+                stroke: '#5f0a87',
+                strokeWidth: 1,
+                preventTransition: 1,
+            }
+            svgElementData[5].moveRect = {
+                type: 'rect',
+                x: cm.moveData[2] - 10,
+                y: cm.moveData[3] - 10,
+                width: 20,
+                height: 20,
+                rx: 8,
+                ry: 8,
+                fill: COLORS.MAP_BACKGROUND,
+                fillOpacity: 1,
+                stroke: '#5f0a87',
+                strokeWidth: 5,
+                preventTransition: 1,
+            };
+        }
+        if (cm.selectionRect.length) {
+            svgElementData[5].selectionRect = {
+                type: 'rect',
+                x: cm.selectionRect[0],
+                y: cm.selectionRect[1],
+                width: cm.selectionRect[2],
+                height: cm.selectionRect[3],
+                rx: 8,
+                ry: 8,
+                fill: '#5f0a87',
+                fillOpacity: 0.05,
+                strokeWidth: 2,
+                preventTransition: 1,
+            };
+        }
+
+
+
+
+
+
+
+
+
+
+        let selfHadj = isOdd(cm.selfH) ? cm.selfH + 1 : cm.selfH;
+        let maxHadj = isOdd(cm.maxH) ? cm.maxH + 1 : cm.maxH;
+        let nsx = cm.path[3] ? cm.nodeEndX : cm.nodeStartX;
+        let nex = cm.path[3] ? cm.nodeStartX : cm.nodeEndX;
+        let nsy = cm.nodeY - selfHadj/2;
+        let ney = cm.nodeY + selfHadj/2;
+        let dir = cm.path[3] ? -1 : 1;
+        let r = 8;
+
+
+
         if (conditions.branchFill ||
             conditions.nodeFill ||
             conditions.branchBorder ||
@@ -258,56 +331,11 @@ export const mapVisualizeSvg = {
                 };
             }
         }
-        if (cm.moveData.length) {
-            let x1, y1, c1x, c1y, c2x, c2y, x2, y2;
-            let deltaX = cm.moveData[2] - cm.moveData[0];
-            let deltaY = cm.moveData[3] - cm.moveData[1];
-            // the elegant solution would be the inheritance of the target line type
-            x1 = cm.moveData[0];
-            y1 = cm.moveData[1];
-            c1x = cm.moveData[0] + deltaX / 4;
-            c1y = cm.moveData[1];
-            c2x = cm.moveData[0] + deltaX / 4;
-            c2y = cm.moveData[1] + deltaY;
-            x2 = cm.moveData[2];
-            y2 = cm.moveData[3];
-            svgElementData[5].moveLine = {
-                type: 'path',
-                path: `M${x1},${y1} C${c1x},${c1y} ${c2x},${c2y} ${x2},${y2}`,
-                stroke: '#5f0a87',
-                strokeWidth: 1,
-                preventTransition: 1,
-            }
-            svgElementData[5].moveRect = {
-                type: 'rect',
-                x: cm.moveData[2] - 10,
-                y: cm.moveData[3] - 10,
-                width: 20,
-                height: 20,
-                rx: 8,
-                ry: 8,
-                fill: COLORS.MAP_BACKGROUND,
-                fillOpacity: 1,
-                stroke: '#5f0a87',
-                strokeWidth: 5,
-                preventTransition: 1,
-            };
-        }
-        if (cm.selectionRect.length) {
-            svgElementData[5].selectionRect = {
-                type: 'rect',
-                x: cm.selectionRect[0],
-                y: cm.selectionRect[1],
-                width: cm.selectionRect[2],
-                height: cm.selectionRect[3],
-                rx: 8,
-                ry: 8,
-                fill: '#5f0a87',
-                fillOpacity: 0.05,
-                strokeWidth: 2,
-                preventTransition: 1,
-            };
-        }
+
+
+
+
+
         let svgGroupList = [];
         if (!mapSvgData.hasOwnProperty(cm.svgId) ||
             ((mapSvgData.hasOwnProperty(cm.svgId) && mapSvgData[cm.svgId].keepHash === keepHash))) {
