bff3e5daaf09c22d156ea1c435ce506b35915a72
Krisztian Szucher <krisztian@mindboard.io>
2022-02-13 11:22:12 +0100

    clean semicolons

--- client/core/MapStackFlow.js
+++ client/core/MapStackFlow.js
@@ -1,102 +1,102 @@
-import {mapAssembly} from "../map/MapAssembly";
-import {copy, subsref} from "./Utils";
-import {mapAlgo} from "../map/MapAlgo";
-import {mapInit} from "../map/MapInit";
-import {mapChain} from "../map/MapChain";
-import {mapTaskCheck} from "../map/MapTaskCheck";
-import {mapMeasure} from "../map/MapMeasure";
-import {mapPlace} from "../map/MapPlace";
-import {mapTaskCalc} from "../map/MapTaskCalc";
-import {mapTaskColor} from "../map/MapTaskColor";
-import {mapDeinit} from "../map/MapDeinit";
-import {mapDisassembly} from "../map/MapDisassembly";
-import {flagDomData, updateDomData} from "./DomFlow";
-import {mapVisualizeDiv} from "../map/MapVisualizeDiv";
-import {mapVisualizeSvg} from "../map/MapVisualizeSvg";
-import {initSelectionState, updateSelectionState} from "./SelectionFlow";
-import {mapCollect} from "../map/MapCollect";
+import {mapAssembly} from "../map/MapAssembly"
+import {copy, subsref} from "./Utils"
+import {mapAlgo} from "../map/MapAlgo"
+import {mapInit} from "../map/MapInit"
+import {mapChain} from "../map/MapChain"
+import {mapTaskCheck} from "../map/MapTaskCheck"
+import {mapMeasure} from "../map/MapMeasure"
+import {mapPlace} from "../map/MapPlace"
+import {mapTaskCalc} from "../map/MapTaskCalc"
+import {mapTaskColor} from "../map/MapTaskColor"
+import {mapDeinit} from "../map/MapDeinit"
+import {mapDisassembly} from "../map/MapDisassembly"
+import {flagDomData, updateDomData} from "./DomFlow"
+import {mapVisualizeDiv} from "../map/MapVisualizeDiv"
+import {mapVisualizeSvg} from "../map/MapVisualizeSvg"
+import {initSelectionState, updateSelectionState} from "./SelectionFlow"
+import {mapCollect} from "../map/MapCollect"
 import { mapPrint } from '../map/MapPrint'
 
 export let mapStack = {
     data: [],
     dataIndex: 0,
-};
+}
 
 export function mapStackDispatch(action, payload) {
-    console.log('MAPDISPATCH: ' + action);
-    mapStackReducer(action, payload);
-    recalc();
+    console.log('MAPDISPATCH: ' + action)
+    mapStackReducer(action, payload)
+    recalc()
 }
 
 function mapStackReducer(action, payload) {
     switch (action) {
         case 'initMapState': {
-            mapStack.data = [mapAssembly(payload.mapStorage)];
-            mapStack.dataIndex = 0;
-            break;
+            mapStack.data = [mapAssembly(payload.mapStorage)]
+            mapStack.dataIndex = 0
+            break
         }
         case 'undo': {
             if (mapStack.dataIndex > 0) {
-                mapStack.dataIndex--;
+                mapStack.dataIndex--
             }
-            break;
+            break
         }
         case 'redo': {
             if (mapStack.dataIndex < mapStack.data.length - 1) {
-                mapStack.dataIndex++;
+                mapStack.dataIndex++
             }
-            break;
+            break
         }
     }
 }
 
 const getMapData = () => {
-    return mapStack.data[mapStack.dataIndex];
-};
+    return mapStack.data[mapStack.dataIndex]
+}
 
 export function recalc() {
-    initSelectionState();
-    let m = mapref(['m']);
+    initSelectionState()
+    let m = mapref(['m'])
     for (let i = 0; i < mapref(['r']).length; i++) {
-        let cr = mapref(['r', i]);
-        mapAlgo.start(m, cr);
-        mapInit.start(m, cr);
-        mapChain.start(m, cr, i);
-        mapTaskCheck.start(m, cr);
-        mapMeasure.start(m, cr);
-        mapPlace.start(m, cr);
-        mapTaskCalc.start(m, cr);
-        mapTaskColor.start(m, cr);
-        mapCollect.start(m, cr);
+        let cr = mapref(['r', i])
+        mapAlgo.start(m, cr)
+        mapInit.start(m, cr)
+        mapChain.start(m, cr, i)
+        mapTaskCheck.start(m, cr)
+        mapMeasure.start(m, cr)
+        mapPlace.start(m, cr)
+        mapTaskCalc.start(m, cr)
+        mapTaskColor.start(m, cr)
+        mapCollect.start(m, cr)
         // mapPrint.start(m, cr)
     }
-    updateSelectionState();
+    updateSelectionState()
 }
 
 export function redraw() {
-    flagDomData();
-    let m = mapref(['m']);
+    flagDomData()
+    let m = mapref(['m'])
     for (let i = 0; i < mapref(['r']).length; i++) {
-        let cr = mapref(['r', i]);
-        mapVisualizeSvg.start(m, cr);
-        mapVisualizeDiv.start(m, cr);
+        let cr = mapref(['r', i])
+        mapVisualizeSvg.start(m, cr)
+        mapVisualizeDiv.start(m, cr)
     }
-    updateDomData();
+    updateDomData()
 }
 
 export function push() {
     if (mapStack.data.length > mapStack.dataIndex + 1) {
-        mapStack.data.length = mapStack.dataIndex + 1;
+        mapStack.data.length = mapStack.dataIndex + 1
     }
-    mapStack.data.push(JSON.parse(JSON.stringify(getMapData())));
-    mapStack.dataIndex++;
+    mapStack.data.push(JSON.parse(JSON.stringify(getMapData())))
+    mapStack.dataIndex++
 }
 
 export function checkPop(dispatch) {
     if (JSON.stringify(mapStack.data[mapStack.dataIndex]) ===
         JSON.stringify(mapStack.data[mapStack.dataIndex - 1])) {
-        mapStack.data.length--;
-        mapStack.dataIndex--;
+        mapStack.data.length--
+        mapStack.dataIndex--
     } else {
         dispatch({ type: 'MAP_STATE_CHANGED' })
     }
@@ -107,15 +107,15 @@ export function mapref(path) {
 }
 
 export function pathMerge(path1, path2) {
-    let retPath = path1.slice();
+    let retPath = path1.slice()
     for (let i = 0; i < Object.keys(path2).length; i++) {
-        retPath.push(path2[i]);
+        retPath.push(path2[i])
     }
-    return retPath;
+    return retPath
 }
 
 export function saveMap() {
-    let cm = copy(getMapData());
-    mapDeinit.start(cm);
-    return mapDisassembly.start(cm);
+    let cm = copy(getMapData())
+    mapDeinit.start(cm)
+    return mapDisassembly.start(cm)
 }
