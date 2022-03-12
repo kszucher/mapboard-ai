141ea480e190b14789e0bb75ca90417cf92941e8
Krisztian Szucher <krisztian@mindboard.io>
2022-03-12 22:44:16 +0100

    mapping restructured

--- client/component-side/CommandTexts.js
+++ client/component-side/CommandTexts.js
@@ -9,23 +9,39 @@ import StyledButtonGroup from "../component-styled/StyledButtonGroup";
 export function CommandTexts () {
     const {UNAUTHORIZED, VIEW} = MAP_RIGHTS
 
+    const DENSITY_TYPES = ['small', 'large']
+    const ALIGNMENT_TYPES = ['adaptive', 'centered']
+    const LINE_WIDTH_TYPES = ['w1', 'w2', 'w3']
+    const LINE_TYPE_TYPES = ['bezier', 'edge']
+    const BORDER_WIDTH_TYPES = ['w1', 'w2', 'w3']
+    const FONT_SIZE_TYPES = ['h1', 'h2', 'h3', 'h4', 't']
+
     const formatMode = useSelector(state => state.formatMode)
     const mapRight = useSelector(state => state.mapRight)
-    const density = useSelector(state => state.node.density)
-    const alignment = useSelector(state => state.node.alignment)
-    const lineWidth = useSelector(state => state.node.lineWidth)
-    const lineType = useSelector(state => state.node.lineType)
-    const borderWidth = useSelector(state => state.node.borderWidth)
-    const fontSize = useSelector(state => state.node.fontSize)
+    const density = {['small']: 'small', ['large']: 'large'}[useSelector(state => state.node.density)]
+    const alignment = {['adaptive']: 'adaptive', ['centered']: 'centered'}[useSelector(state => state.node.alignment)]
+    const lineWidth = {[1]: 'w1', [2]: 'w2', [3]: 'w3'}[useSelector(state => state.node.lineWidth)]
+    const lineType = {['b']: 'bezier', ['e']: 'edge'}[useSelector(state => state.node.lineType)]
+    const borderWidth = {[1]: 'w1', [2]: 'w2', [3]: 'w3'}[useSelector(state => state.node.borderWidth)]
+    const fontSize = {[36]: 'h1', [24]: 'h2', [18]: 'h3', [16]: 'h4', [14]: 't'}[useSelector(state => state.node.fontSize)]
 
     const dispatch = useDispatch()
-    const setNodeParam = (nodeParamObj) => dispatch({type: 'SET_NODE_PARAM', payload: nodeParamObj })
+    const setNodeParam = obj => dispatch({type: 'SET_NODE_PARAM', payload: obj })
+    const setDensity = value => setNodeParam({density: {['small']: 'small', ['large']: 'large'}[value]})
+    const setAlignment = value => setNodeParam({alignment: {['adaptive']: 'adaptive', ['centered']: 'centered'}[value]})
+    const setLineWidth = value => setNodeParam({lineWidth: {['w1']: 1, ['w2']: 2, ['w3']: 3}[value]})
+    const setLineType = value => setNodeParam({lineType: {['bezier']: 'b', ['edge']: 'e'}[value]})
+    const setBorderWidth = value => setNodeParam({borderWidth: {['w1']: 1, ['w2']: 2, ['w3']: 3}[value]})
+    const setFontSize = value => setNodeParam({fontSize: {['h1']: 36, ['h2']: 24, ['h3']: 18, ['h4']: 16, ['t']: 14}[value]})
+
     const openPalette = e => dispatch({type: 'OPEN_PALETTE', payload: e})
     const createMapInMap = _ => dispatch({type: 'CREATE_MAP_IN_MAP'})
 
     const mapResetAll = _ =>    {push(); mapDispatch('resetAll');               redraw(); checkPop(dispatch)}
     const mapReset = _ =>       {push(); mapDispatch('reset', {formatMode});    redraw(); checkPop(dispatch)}
     const mapToggleTask = _ =>  {push(); mapDispatch('toggleTask');             redraw(); checkPop(dispatch)}
+
+    const disabled = [UNAUTHORIZED, VIEW].includes(mapRight)
 
     return (
         <div style={{
@@ -53,80 +69,80 @@ export function CommandTexts () {
             }}>
                 <StyledButtonGroup
                     open={true}
-                    valueList={['small', 'large']}
-                    value={{['small']: 'small', ['large']: 'large'}[density]}
-                    action={value => setNodeParam({density: {['small']: 'small', ['large']: 'large'}[value]})}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    valueList={DENSITY_TYPES}
+                    value={density}
+                    action={setDensity}
+                    disabled={disabled}
                 />
                 <StyledButtonGroup
                     open={true}
-                    valueList={['adaptive', 'centered']}
-                    value={{['adaptive']: 'adaptive', ['centered']: 'centered'}[alignment]}
-                    action={value => setNodeParam({alignment: {['adaptive']: 'adaptive', ['centered']: 'centered'}[value]})}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    valueList={ALIGNMENT_TYPES}
+                    value={alignment}
+                    action={setAlignment}
+                    disabled={disabled}
                 />
                 <StyledButtonGroup
                     open={true}
                     valueList={['line', 'border', 'fill', 'text']}
                     value={formatMode}
                     action={openPalette}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    disabled={disabled}
                 />
                 <StyledButtonGroup
                     open={formatMode === '' }
                     valueList={['reset format']}
                     value={''}
                     action={mapResetAll}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    disabled={disabled}
                 />
                 <StyledButtonGroup
                     open={formatMode !== '' }
                     valueList={['reset ' + formatMode]}
                     value={''}
                     action={mapReset}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    disabled={disabled}
                 />
                 <StyledButtonGroup
                     open={formatMode === 'line'}
-                    valueList={['w1', 'w2', 'w3']}
-                    value={{[1]: 'w1', [2]: 'w2', [3]: 'w3'}[lineWidth]}
-                    action={value => setNodeParam({lineWidth: {['w1']: 1, ['w2']: 2, ['w3']: 3}[value]})}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    valueList={LINE_WIDTH_TYPES}
+                    value={lineWidth}
+                    action={setLineWidth}
+                    disabled={disabled}
                 />
                 <StyledButtonGroup
                     open={formatMode === 'line'}
-                    valueList={['bezier', 'edge']}
-                    value={{['b']: 'bezier', ['e']: 'edge'}[lineType]}
-                    action={value => setNodeParam({lineType: {['bezier']: 'b', ['edge']: 'e'}[value]})}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    valueList={LINE_TYPE_TYPES}
+                    value={lineType}
+                    action={setLineType}
+                    disabled={disabled}
                 />
                 <StyledButtonGroup
                     open={formatMode === 'border'}
-                    valueList={['w1', 'w2', 'w3']}
-                    value={{[1]: 'w1', [2]: 'w2', [3]: 'w3'}[borderWidth]}
-                    action={value => setNodeParam({borderWidth: {['w1']: 1, ['w2']: 2, ['w3']: 3}[value]})}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    valueList={BORDER_WIDTH_TYPES}
+                    value={borderWidth}
+                    action={setBorderWidth}
+                    disabled={disabled}
                 />
                 <StyledButtonGroup
                     open={formatMode === 'text'}
-                    valueList={['h1', 'h2', 'h3', 'h4', 't']}
-                    value={{[36]: 'h1', [24]: 'h2', [18]: 'h3', [16]: 'h4', [14]: 't'}[fontSize]}
-                    action={value => setNodeParam({fontSize: {['h1']: 36, ['h2']: 24, ['h3']: 18, ['h4']: 16, ['t']: 14}[value]})}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    valueList={FONT_SIZE_TYPES}
+                    value={fontSize}
+                    action={setFontSize}
+                    disabled={disabled}
                 />
                 <StyledButtonGroup
                     open={formatMode === ''}
                     valueList={['convert to task']}
                     value={''}
                     action={mapToggleTask}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    disabled={disabled}
                 />
                 <StyledButtonGroup
                     open={formatMode === ''}
                     valueList={['convert to submap']}
                     value={''}
                     action={createMapInMap}
-                    disabled={[UNAUTHORIZED, VIEW].includes(mapRight)}
+                    disabled={disabled}
                 />
             </div>
         </div>
