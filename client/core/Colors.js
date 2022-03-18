91fc21639be3b897174f319d09c551f6a22bea93
Krisztian Szucher <krisztian@mindboard.io>
2022-03-19 00:14:08 +0100

    pragmatic black and white task color scheme

--- client/core/Colors.js
+++ client/core/Colors.js
@@ -29,25 +29,25 @@ const COLOR_DARK = {
     TEXT_COLOR: '#dddddd',
     TABLE_GRID: '#aaaaaa',
 
-    TASK_CIRCLE_0_ACTIVE: '#eeeeee',
-    TASK_CIRCLE_0_INACTIVE: '#eeeeee',
+    TASK_CIRCLE_0_ACTIVE: '#333333',
+    TASK_CIRCLE_0_INACTIVE: '#333333',
 
-    TASK_LINE_1: '#2c9dfc',
-    TASK_CIRCLE_1_ACTIVE: '#2c9dfc',
-    TASK_FILL_1: '#d4ebfe',
-    TASK_CIRCLE_1_INACTIVE: '#e5f3fe',
+    TASK_LINE_1: '#1e6db0',
+    TASK_CIRCLE_1_ACTIVE: '#1e6db0',
+    TASK_FILL_1: '#1e6db0',
+    TASK_CIRCLE_1_INACTIVE: '#113e64',
 
-    TASK_LINE_2: '#d5802a',
-    TASK_CIRCLE_2_ACTIVE: '#d5802a',
-    TASK_FILL_2: '#f6e5d4',
-    TASK_CIRCLE_2_INACTIVE: '#f6e5d4',
+    TASK_LINE_2: '#95591d',
+    TASK_CIRCLE_2_ACTIVE: '#95591d',
+    TASK_FILL_2: '#95591d',
+    TASK_CIRCLE_2_INACTIVE: '#553310',
 
-    TASK_LINE_3: '#25bf25',
-    TASK_CIRCLE_3_ACTIVE: '#25bf25',
-    TASK_FILL_3: '#d4f6d4',
-    TASK_CIRCLE_3_INACTIVE: '#e5f9e5',
+    TASK_LINE_3: '#198519',
+    TASK_CIRCLE_3_ACTIVE: '#198519',
+    TASK_FILL_3: '#198519',
+    TASK_CIRCLE_3_INACTIVE: '#0e4c0e',
 
-    TASK_LINE: '#eeeeee',
+    TASK_LINE: '#555555',
 }
 
 export const getColors = (colorMode) => {
