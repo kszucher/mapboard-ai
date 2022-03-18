f839dcfb7e82255c64b29fe887314ae05f4868ad
Krisztian Szucher <krisztian@mindboard.io>
2022-03-19 00:03:07 +0100

    group

--- client/core/Colors.js
+++ client/core/Colors.js
@@ -3,20 +3,24 @@ const COLOR_LIGHT = {
     TEXT_COLOR: '#222222',
     TABLE_GRID: '#dddddd',
 
-    TASK_LINE_1: '#2c9dfc',
-    TASK_LINE_2: '#d5802a',
-    TASK_LINE_3: '#25bf25',
-    TASK_FILL_1: '#d4ebfe',
-    TASK_FILL_2: '#f6e5d4',
-    TASK_FILL_3: '#d4f6d4',
     TASK_CIRCLE_0_ACTIVE: '#eeeeee',
-    TASK_CIRCLE_1_ACTIVE: '#2c9dfc',
-    TASK_CIRCLE_2_ACTIVE: '#d5802a',
-    TASK_CIRCLE_3_ACTIVE: '#25bf25',
     TASK_CIRCLE_0_INACTIVE: '#eeeeee',
+
+    TASK_LINE_1: '#2c9dfc',
+    TASK_CIRCLE_1_ACTIVE: '#2c9dfc',
+    TASK_FILL_1: '#d4ebfe',
     TASK_CIRCLE_1_INACTIVE: '#e5f3fe',
+
+    TASK_LINE_2: '#d5802a',
+    TASK_CIRCLE_2_ACTIVE: '#d5802a',
+    TASK_FILL_2: '#f6e5d4',
     TASK_CIRCLE_2_INACTIVE: '#f6e5d4',
+
+    TASK_LINE_3: '#25bf25',
+    TASK_CIRCLE_3_ACTIVE: '#25bf25',
+    TASK_FILL_3: '#d4f6d4',
     TASK_CIRCLE_3_INACTIVE: '#e5f9e5',
+
     TASK_LINE: '#eeeeee',
 }
 
@@ -24,21 +28,25 @@ const COLOR_DARK = {
     MAP_BACKGROUND: '#222529',
     TEXT_COLOR: '#dddddd',
     TABLE_GRID: '#aaaaaa',
-    // TODO define
-    TASK_LINE_1: '#2c9dfc',
-    TASK_LINE_2: '#d5802a',
-    TASK_LINE_3: '#25bf25',
-    TASK_FILL_1: '#d4ebfe',
-    TASK_FILL_2: '#f6e5d4',
-    TASK_FILL_3: '#d4f6d4',
+
     TASK_CIRCLE_0_ACTIVE: '#eeeeee',
-    TASK_CIRCLE_1_ACTIVE: '#2c9dfc',
-    TASK_CIRCLE_2_ACTIVE: '#d5802a',
-    TASK_CIRCLE_3_ACTIVE: '#25bf25',
     TASK_CIRCLE_0_INACTIVE: '#eeeeee',
+
+    TASK_LINE_1: '#2c9dfc',
+    TASK_CIRCLE_1_ACTIVE: '#2c9dfc',
+    TASK_FILL_1: '#d4ebfe',
     TASK_CIRCLE_1_INACTIVE: '#e5f3fe',
+
+    TASK_LINE_2: '#d5802a',
+    TASK_CIRCLE_2_ACTIVE: '#d5802a',
+    TASK_FILL_2: '#f6e5d4',
     TASK_CIRCLE_2_INACTIVE: '#f6e5d4',
+
+    TASK_LINE_3: '#25bf25',
+    TASK_CIRCLE_3_ACTIVE: '#25bf25',
+    TASK_FILL_3: '#d4f6d4',
     TASK_CIRCLE_3_INACTIVE: '#e5f9e5',
+
     TASK_LINE: '#eeeeee',
 }
 
