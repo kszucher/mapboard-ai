520ddebc9fd3bae87d0f2048c822615cd09a4cbd
Krisztian Szucher <krisztian@mindboard.io>
2022-03-19 21:43:41 +0100

    files moved

--- client/index/index.js
+++ client/index/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
-import {store} from "../core/EditorFlow";
-import {Page} from "../component/Page";
-import '../css/Layout.css'
+import {store} from "../../client-vite/core/EditorFlow";
+import {Page} from "../../client-vite/component/Page";
+import '../../client-vite/css/Layout.css'
 
 ReactDOM.render(
     <Provider store={store}>
