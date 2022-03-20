53893395155509392bb0dd2993a1935b24456325
Krisztian Szucher <krisztian@mindboard.io>
2022-03-20 22:45:12 +0100

    started using mui system instead of mui styles

--- client-vite/component-side/Entries.jsx
+++ client-vite/component-side/Entries.jsx
@@ -1,27 +1,7 @@
 import React from 'react';
 import {useSelector, useDispatch} from "react-redux";
 import { Tab, Tabs } from '@mui/material'
-import { makeStyles } from '@mui/styles'
 import { getColors } from '../core/Colors'
-
-const getStyle = (theme) => {
-    return {
-        root: {
-            flexGrow: 1,
-            display: 'flex-start',
-        },
-        tabs: {
-            borderRight: `0px solid ${theme.palette.divider}`,
-        },
-        indicator: {
-            left: "0px",
-            width: "8px",
-            borderTopRightRadius: "16px",
-            borderBottomRightRadius: "16px",
-            backgroundImage: "linear-gradient(180deg, #a4508b 0%, #5f0a87 74%)",
-        }
-    }
-}
 
 export default function Entries() {
     const tabMapNameList = useSelector(state => state.tabMapNameList)
@@ -29,7 +9,6 @@ export default function Entries() {
     const mapSource = useSelector(state => state.mapSource)
     const dispatch = useDispatch()
     const openMapFromTab = (e, value) =>  dispatch({type: 'OPEN_MAP_FROM_TAB', payload: {tabMapSelected: value}})
-    const classes = makeStyles((theme) => (getStyle(theme)))()
     return (
         <div style={{
             position: 'fixed',
@@ -43,24 +22,30 @@ export default function Entries() {
             borderColor: '#dddddd',
             borderLeft: 0
         }}>
-            <div className={classes.root}>
-                <Tabs
-                    classes={{indicator: classes.indicator}}
-                    orientation={'vertical'}
-                    variant="scrollable"
-                    aria-label="Vertical tabs example"
-                    className={classes.tabs}
-                    value={tabMapSelected}
-                    onChange={openMapFromTab}
-                    indicatorColor="primary" >
-                    {tabMapNameList.map((name, index) => (
-                        <Tab
-                            disabled={mapSource==='dataPlayback'}
-                            label={name}
-                            key={index}/>
-                    ))}>
-                </Tabs>
-            </div>
+            <Tabs
+                sx={{
+                    '.MuiTabs-indicator': {
+                        left: "0px",
+                        width: "8px",
+                        borderTopRightRadius: "16px",
+                        borderBottomRightRadius: "16px",
+                        backgroundImage: "linear-gradient(180deg, #a4508b 0%, #5f0a87 74%)",
+                    }
+                }}
+                orientation={'vertical'}
+                variant="scrollable"
+                aria-label="Vertical tabs example"
+                value={tabMapSelected}
+                onChange={openMapFromTab}
+                indicatorColor="primary" >
+                {tabMapNameList.map((name, index) => (
+                    <Tab
+                        disabled={mapSource==='dataPlayback'}
+                        label={name}
+                        key={index}
+                    />
+                ))}>
+            </Tabs>
         </div>
     )
 }
