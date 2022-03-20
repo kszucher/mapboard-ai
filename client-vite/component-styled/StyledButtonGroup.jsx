228dcb1fbbaebeb0087955e95f68a6a63e3e42ba
Krisztian Szucher <krisztian@mindboard.io>
2022-03-20 23:09:27 +0100

    no more makestyles

--- client-vite/component-styled/StyledButtonGroup.jsx
+++ client-vite/component-styled/StyledButtonGroup.jsx
@@ -1,23 +1,10 @@
 import React from "react";
 import { Button, ButtonGroup } from '@mui/material'
-import { makeStyles } from '@mui/styles'
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        '& > *': {
-            margin: theme.spacing(1),
-        }
-    }
-}))
 
 export default function StyledButtonGroup (arg) {
     const {open, valueList, value, action, size, disabled, valueListDisabled} = arg;
-    const classes = useStyles();
     return (
-        open && <div className={classes.root}>
+        open && <div>
             <ButtonGroup
                 disabled={disabled || false}
                 size={size || 'small'}
