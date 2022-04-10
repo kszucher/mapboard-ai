0f4da69dca253b7ee78723ac0532c1f68a7c7ad8
Krisztian Szucher <krisztian@mindboard.io>
2022-04-10 23:40:59 +0200

    sSelect, fSelect

--- client-vite/component-side/IconBar.jsx
+++ client-vite/component-side/IconBar.jsx
@@ -11,6 +11,9 @@ import PersonIcon from '@mui/icons-material/Person'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import IconBarMore from './IconBarMore'
 
+const sSelectionSvg = "M 120 144 L 360 144 C 408 144 432 168 432 216 L 432 264 C 432 312 408 336 360 336 L 120 336 C 72 336 48 312 48 264 C 48 216 48 264 48 216 C 48 168 72 144 120 144 Z"
+const fSelectionSvg = "M 312 72 L 360 72 C 408 72 432 96 432 144 L 432 336 C 432 384 408 408 360 408 L 312 408 C 264 408 120 312 72 312 C 24 312 24 168 72 168 C 120 168 264 72 312 72 Z"
+
 export function IconBar () {
     const colorMode = useSelector(state => state.colorMode)
     const {MAP_BACKGROUND} = getColors(colorMode)
@@ -43,9 +46,6 @@ export function IconBar () {
         }}>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
 
-
-
-
                 <IconButton color='secondary' onClick={_=>console.log('clicked')}>
                         <svg viewBox="0 0 480 480" width="24px" height="24px" >
                             <path style={{ fill: 'none', stroke: '#5f0a87', strokeWidth:24}}
@@ -56,20 +56,17 @@ export function IconBar () {
                 <IconButton color='secondary' onClick={_=>console.log('clicked')}>
                     <svg viewBox="0 0 480 480" width="24px" height="24px" >
                         <path style={{ fill: 'none', stroke: '#5f0a87', strokeWidth:24 }}
-                            d="M 312 72 L 360 72 C 408 72 432 96 432 144 L 432 336 C 432 384 408 408 360 408 L 312 408 C 264 408 120 312 72 312 C 24 312 24 168 72 168 C 120 168 264 72 312 72 Z"
+                              d={sSelectionSvg}
                         />
                     </svg>
                 </IconButton>
                 <IconButton color='secondary' onClick={_=>console.log('clicked')}>
                     <svg viewBox="0 0 480 480" width="24px" height="24px" >
                         <path style={{ fill:'#5f0a87', stroke: '#5f0a87', strokeWidth:24 }}
-                              d="M 312 72 L 360 72 C 408 72 432 96 432 144 L 432 336 C 432 384 408 408 360 408 L 312 408 C 264 408 120 312 72 312 C 24 312 24 168 72 168 C 120 168 264 72 312 72 Z"
+                              d={sSelectionSvg}
                         />
                     </svg>
                 </IconButton>
-
-
-
 
                 <IconButton color='secondary' onClick={changeDensity}>
                     {density === 'small' && <DensitySmallIcon/>}
