adce94f3dfe8d63a70e4ab943d7c340ab49a0a1a
Krisztian Szucher <krisztian@mindboard.io>
2022-04-17 11:50:56 +0200

    gestalt proximity

--- client-vite/component-side/Formatter.jsx
+++ client-vite/component-side/Formatter.jsx
@@ -48,7 +48,7 @@ export function Formatter () {
         <div style={{
             position: 'fixed',
             top: 48*2,
-            right: 40 + 2*12 + 48,
+            right: 40 + 2*12 + 0,
             width: width + 2*12,
             display: 'flex',
             flexDirection: 'column',
