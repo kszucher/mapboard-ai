9f255d6bfd15adc725c3a7f8e495f9906789b5ef
Krisztian Szucher <krisztian@mindboard.io>
2022-05-01 17:34:18 +0200

    fix mapGetProp bug

--- client-vite/map/MapGetProp.js
+++ client-vite/map/MapGetProp.js
@@ -1,9 +1,7 @@
 export const mapGetProp = {
     start: (m, cm, prop) => {
         let firstProp = {[prop]: undefined}
-        if (Object.keys(cm.d).length) {
-            firstProp = {[prop]: cm.d[0][prop]}
-        } else if (Object.keys(cm.s).length) {
+        if (Object.keys(cm.s).length) {
             firstProp = {[prop]: cm.s[0][prop]}
         }
         mapGetProp.iterate(m, cm, prop, firstProp)
@@ -11,19 +9,10 @@ export const mapGetProp = {
     },
 
     iterate: (m, cm, prop, firstProp) => {
-        let dCount = Object.keys(cm.d).length
-        if (dCount) {
-            for (let i = 0; i < dCount; i++) {
-                mapGetProp.iterate(m, cm.d[i])
-                if (cm.d[i][prop] !== firstProp[prop]) {
-                    firstProp[prop] = undefined
-                }
-            }
-        }
         let sCount = Object.keys(cm.s).length
         if (sCount) {
             for (let i = 0; i < sCount; i++) {
-                mapGetProp.iterate(m, cm.s[i])
+                mapGetProp.iterate(m, cm.s[i], prop, firstProp)
                 if (cm.s[i][prop] !== firstProp[prop]) {
                     firstProp[prop] = undefined
                 }
