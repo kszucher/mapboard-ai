d7a3f11d34fe95576acf86e6fb929ce0eed06687
Krisztian Szucher <krisztian@mindboard.io>
2022-06-30 10:38:44 +0200

    css simplified

--- client-vite/component/SideBarRight.jsx
+++ client-vite/component/SideBarRight.jsx
@@ -5,9 +5,6 @@ import DensitySmallIcon from '@mui/icons-material/DensitySmall'
 import DensityMediumIcon from '@mui/icons-material/DensityMedium'
 import CenterFocusWeakIcon from '@mui/icons-material/CenterFocusWeak'
 import CenterFocusStrongIcon from '@mui/icons-material/CenterFocusStrong'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeIcon from '@mui/icons-material/DarkMode'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
 import DynamicFeedIcon from '@mui/icons-material/DynamicFeed'
 import ShareIcon from '@mui/icons-material/Share'
 import More from './More'
@@ -19,6 +16,21 @@ import CloseIcon from '@mui/icons-material/Close'
 const svgCommonParams = {viewBox:"0 0 480 480", width:"24px", height:"24px"}
 const sSelectionSvg = "M 120 144 L 360 144 C 408 144 432 168 432 216 L 432 264 C 432 312 408 336 360 336 L 120 336 C 72 336 48 312 48 264 C 48 216 48 264 48 216 C 48 168 72 144 120 144 Z"
 const fSelectionSvg = "M 312 72 L 360 72 C 408 72 432 96 432 144 L 432 336 C 432 384 408 408 360 408 L 312 408 C 264 408 120 312 72 312 C 24 312 24 168 72 168 C 120 168 264 72 312 72 Z"
+
+const commonCss = (MAP_BACKGROUND, PAGE_BACKGROUND) => ({
+    position: 'fixed',
+    right: 0,
+    width: 40,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '12px 12px 12px 12px',
+    backgroundColor: MAP_BACKGROUND,
+    borderTop: `1px solid ${PAGE_BACKGROUND}`,
+    borderLeft: `1px solid ${PAGE_BACKGROUND}`,
+    borderBottom: `1px solid ${PAGE_BACKGROUND}`,
+    borderRight: 0,
+})
 
 export function SideBarRight () {
     const colorMode = useSelector(state => state.colorMode)
@@ -50,148 +62,97 @@ export function SideBarRight () {
     const closeFrameEditor = _ => dispatch({type: 'CLOSE_FRAME_EDITOR'})
     return (
         <>
-            <div style={{
-                position: 'fixed',
-                right: 0,
-                top: 48*2,
-                width: 40,
-                display: 'flex',
-                alignItems: 'center',
-                padding: '12px 12px 12px 12px',
-                backgroundColor: MAP_BACKGROUND,
-                borderTop: `1px solid ${PAGE_BACKGROUND}`,
-                borderLeft: `1px solid ${PAGE_BACKGROUND}`,
-                borderBottom: `1px solid ${PAGE_BACKGROUND}`,
-                borderRight: 0,
-                borderRadius: '16px 0 0 0',
-            }}>
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-
-                    <IconButton color='secondary' onClick={changeDensity}>
-                        {density === 'small' && <DensitySmallIcon/>}
-                        {density === 'large' && <DensityMediumIcon/>}
-                    </IconButton>
-                    <IconButton color='secondary' onClick={changeAlignment}>
-                        {alignment === 'adaptive' && <CenterFocusWeakIcon/>}
-                        {alignment === 'centered' && <CenterFocusStrongIcon/>}
-                    </IconButton>
-                    <IconButton color='secondary' onClick={formatMode === 'line' ? closePalette : setFormatModeLine}>
-                        <svg {...svgCommonParams}>
-                            <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }}
-                                  d="M 408 72 C 72 72 408 408 72 408"
-                            />
-                        </svg>
-                    </IconButton>
-                    <IconButton color='secondary' onClick={formatMode === 'border' ? closePalette : setFormatModeBorder}>
-                        <svg {...svgCommonParams}>
-                            <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }}
-                                  d={selection === 's' ? sSelectionSvg : fSelectionSvg}
-                            />
-                        </svg>
-                    </IconButton>
-                    <IconButton color='secondary' onClick={formatMode === 'fill' ? closePalette : setFormatModeFill}>
-                        <svg {...svgCommonParams}>
-                            <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }}
-                                  d={selection === 's' ? sSelectionSvg : fSelectionSvg}
-                            />
-                        </svg>
-                    </IconButton>
-                    <IconButton color='secondary' onClick={formatMode === 'text' ? closePalette : setFormatModeText}>
-                        <svg {...svgCommonParams}>
-                            <g>
-                                <line style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} x1={96} y1={96} x2={384} y2={96}/>
-                                <line style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} x1={240} y1={384} x2={240} y2={96}/>
-                            </g>
-                        </svg>
-                    </IconButton>
-                    <IconButton color='secondary' onClick={toggleTask}>
-                        <svg {...svgCommonParams}>
-                            <g>
-                                <ellipse style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }} cx={120} cy={240} rx={80} ry={80}/>
-                                <ellipse style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} cx={360} cy={240} rx={80} ry={80}/>
-                            </g>
-                        </svg>
-                    </IconButton>
-                    <IconButton color='secondary' onClick={showWsCreateMapInMap}>
-                        <svg {...svgCommonParams}>
-                            <g>
-                                <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:48 }} d="M 216 240 L 24 240"/>
-                                <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:48 }} d="M 120 144 L 120 336"/>
-                                <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }} d="M 288 48 L 408 240 L 288 432"/>
-                            </g>
-                        </svg>
-                    </IconButton>
-                    {/*<IconButton color='secondary' onClick={openMoreMenu}>*/}
-                    {/*    <MoreVertIcon/>*/}
-                    {/*</IconButton>*/}
-                    {formatMode !== '' && <span
-                        style={{
-                            position: 'fixed',
-                            top: 2*48 + 12 + 40*({ line: 2, border: 3, fill: 4, text: 5 }[formatMode]),
-                            right: 62,
-                            width: 2,
-                            height: 40,
-                            backgroundColor: MAIN_COLOR,
-                        }}/>
-                    }
-                    <More/>
-                </div>
+            <div style={{ ...commonCss(MAP_BACKGROUND, PAGE_BACKGROUND), top: 48*2, borderRadius: '16px 0 0 0' }}>
+                <IconButton color='secondary' onClick={changeDensity}>
+                    {density === 'small' && <DensitySmallIcon/>}
+                    {density === 'large' && <DensityMediumIcon/>}
+                </IconButton>
+                <IconButton color='secondary' onClick={changeAlignment}>
+                    {alignment === 'adaptive' && <CenterFocusWeakIcon/>}
+                    {alignment === 'centered' && <CenterFocusStrongIcon/>}
+                </IconButton>
+                <IconButton color='secondary' onClick={formatMode === 'line' ? closePalette : setFormatModeLine}>
+                    <svg {...svgCommonParams}>
+                        <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }}
+                              d="M 408 72 C 72 72 408 408 72 408"
+                        />
+                    </svg>
+                </IconButton>
+                <IconButton color='secondary' onClick={formatMode === 'border' ? closePalette : setFormatModeBorder}>
+                    <svg {...svgCommonParams}>
+                        <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }}
+                              d={selection === 's' ? sSelectionSvg : fSelectionSvg}
+                        />
+                    </svg>
+                </IconButton>
+                <IconButton color='secondary' onClick={formatMode === 'fill' ? closePalette : setFormatModeFill}>
+                    <svg {...svgCommonParams}>
+                        <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }}
+                              d={selection === 's' ? sSelectionSvg : fSelectionSvg}
+                        />
+                    </svg>
+                </IconButton>
+                <IconButton color='secondary' onClick={formatMode === 'text' ? closePalette : setFormatModeText}>
+                    <svg {...svgCommonParams}>
+                        <g>
+                            <line style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} x1={96} y1={96} x2={384} y2={96}/>
+                            <line style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} x1={240} y1={384} x2={240} y2={96}/>
+                        </g>
+                    </svg>
+                </IconButton>
+                <IconButton color='secondary' onClick={toggleTask}>
+                    <svg {...svgCommonParams}>
+                        <g>
+                            <ellipse style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }} cx={120} cy={240} rx={80} ry={80}/>
+                            <ellipse style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} cx={360} cy={240} rx={80} ry={80}/>
+                        </g>
+                    </svg>
+                </IconButton>
+                <IconButton color='secondary' onClick={showWsCreateMapInMap}>
+                    <svg {...svgCommonParams}>
+                        <g>
+                            <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:48 }} d="M 216 240 L 24 240"/>
+                            <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:48 }} d="M 120 144 L 120 336"/>
+                            <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }} d="M 288 48 L 408 240 L 288 432"/>
+                        </g>
+                    </svg>
+                </IconButton>
+                {/*<IconButton color='secondary' onClick={openMoreMenu}>*/}
+                {/*    <MoreVertIcon/>*/}
+                {/*</IconButton>*/}
+                {formatMode !== '' && <span
+                    style={{
+                        position: 'fixed',
+                        top: 2*48 + 12 + 40*({ line: 2, border: 3, fill: 4, text: 5 }[formatMode]),
+                        right: 62,
+                        width: 2,
+                        height: 40,
+                        backgroundColor: MAIN_COLOR,
+                    }}/>
+                }
+                <More/>
             </div>
-            <div>
-                <div style={{
-                    position: 'fixed',
-                    right: 0,
-                    top: 48*2 + 40*8 + 2*12 + 0,
-                    width: 40,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    padding: '12px 12px 12px 12px',
-                    backgroundColor: MAP_BACKGROUND,
-                    borderTop: `1px solid ${PAGE_BACKGROUND}`,
-                    borderLeft: `1px solid ${PAGE_BACKGROUND}`,
-                    borderBottom: `1px solid ${PAGE_BACKGROUND}`,
-                    borderRight: 0,
-                    borderRadius: '0 0 0 0',
-                }}>
-                    <IconButton color='secondary' onClick={openFrameEditor} disabled={frameEditorVisible===1}>
-                        <DynamicFeedIcon/>
-                    </IconButton>
-                    <IconButton color='secondary' onClick={importFrame} disabled={!frameEditorVisible}>
-                        <InputIcon/>
-                    </IconButton>
-                    <IconButton color='secondary' onClick={duplicateFrame} disabled={!frameEditorVisible || frameLen === 0}>
-                        <ContentCopyIcon/>
-                    </IconButton>
-                    <IconButton color='secondary' onClick={deleteFrame} disabled={!frameEditorVisible || frameLen === 0}>
-                        <DeleteIcon/>
-                    </IconButton>
-                    <IconButton color='secondary' onClick={closeFrameEditor} disabled={!frameEditorVisible}>
-                        <CloseIcon/>
-                    </IconButton>
-                </div>
+            <div style={{ ...commonCss(MAP_BACKGROUND, PAGE_BACKGROUND), top: 48*2 + 40*8 + 2*12, borderRadius: '0 0 0 0' }}>
+                <IconButton color='secondary' onClick={openFrameEditor} disabled={frameEditorVisible===1}>
+                    <DynamicFeedIcon/>
+                </IconButton>
+                <IconButton color='secondary' onClick={importFrame} disabled={!frameEditorVisible}>
+                    <InputIcon/>
+                </IconButton>
+                <IconButton color='secondary' onClick={duplicateFrame} disabled={!frameEditorVisible || frameLen === 0}>
+                    <ContentCopyIcon/>
+                </IconButton>
+                <IconButton color='secondary' onClick={deleteFrame} disabled={!frameEditorVisible || frameLen === 0}>
+                    <DeleteIcon/>
+                </IconButton>
+                <IconButton color='secondary' onClick={closeFrameEditor} disabled={!frameEditorVisible}>
+                    <CloseIcon/>
+                </IconButton>
             </div>
-            <div>
-                <div style={{
-                    position: 'fixed',
-                    right: 0,
-                    top: 48*2 + 40*8 + 2*12 + 0 + 5*40 + 2*12 + 0,
-                    width: 40,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    padding: '12px 12px 12px 12px',
-                    backgroundColor: MAP_BACKGROUND,
-                    borderTop: `1px solid ${PAGE_BACKGROUND}`,
-                    borderLeft: `1px solid ${PAGE_BACKGROUND}`,
-                    borderBottom: `1px solid ${PAGE_BACKGROUND}`,
-                    borderRight: 0,
-                    borderRadius: '0 0 0 16px',
-                }}>
-                    <IconButton color='secondary' onClick={_=>{}}>
-                        <ShareIcon/>
-                    </IconButton>
-                </div>
+            <div style={{ ...commonCss(MAP_BACKGROUND, PAGE_BACKGROUND), top: 48*2 + 40*8 + 2*12 + 5*40 + 2*12, borderRadius: '0 0 0 16px' }}>
+                <IconButton color='secondary' onClick={_=>{}}>
+                    <ShareIcon/>
+                </IconButton>
             </div>
         </>
     )
