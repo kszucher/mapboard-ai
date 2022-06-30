c499034aa7c97a4e1c502ac7ef454113eeb6380c
Krisztian Szucher <krisztian@mindboard.io>
2022-06-30 16:29:52 +0200

    icons moved out

---
+++ client-vite/component/Icons.jsx
@@ -0,0 +1,60 @@
+const svgCommonParams = {
+    viewBox:"0 0 480 480",
+    width:"24px",
+    height:"24px"
+
+}
+const sSelectionSvg = "M 120 144 L 360 144 C 408 144 432 168 432 216 L 432 264 C 432 312 408 336 360 336 L 120 336 C 72 336 48 312 48 264 C 48 216 48 264 48 216 C 48 168 72 144 120 144 Z"
+const fSelectionSvg = "M 312 72 L 360 72 C 408 72 432 96 432 144 L 432 336 C 432 384 408 408 360 408 L 312 408 C 264 408 120 312 72 312 C 24 312 24 168 72 168 C 120 168 264 72 312 72 Z"
+
+export const LineIcon = ({MAIN_COLOR}) => (
+    <svg {...svgCommonParams}>
+        <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }}
+              d="M 408 72 C 72 72 408 408 72 408"
+        />
+    </svg>
+)
+
+export const BorderIcon = ({MAIN_COLOR, selection}) => (
+    <svg {...svgCommonParams}>
+        <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }}
+              d={selection === 's' ? sSelectionSvg : fSelectionSvg}
+        />
+    </svg>
+)
+
+export const FillIcon = ({MAIN_COLOR, selection}) => (
+    <svg {...svgCommonParams}>
+        <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }}
+              d={selection === 's' ? sSelectionSvg : fSelectionSvg}
+        />
+    </svg>
+)
+
+export const TextIcon = ({MAIN_COLOR}) => (
+    <svg {...svgCommonParams}>
+        <g>
+            <line style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} x1={96} y1={96} x2={384} y2={96}/>
+            <line style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} x1={240} y1={384} x2={240} y2={96}/>
+        </g>
+    </svg>
+)
+
+export const TaskIcon = ({MAIN_COLOR}) => (
+    <svg {...svgCommonParams}>
+        <g>
+            <ellipse style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }} cx={120} cy={240} rx={80} ry={80}/>
+            <ellipse style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} cx={360} cy={240} rx={80} ry={80}/>
+        </g>
+    </svg>
+)
+
+export const CreateMapInMapIcon = ({MAIN_COLOR}) => (
+    <svg {...svgCommonParams}>
+        <g>
+            <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:48 }} d="M 216 240 L 24 240"/>
+            <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:48 }} d="M 120 144 L 120 336"/>
+            <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }} d="M 288 48 L 408 240 L 288 432"/>
+        </g>
+    </svg>
+)
--- client-vite/component/SideBarRight.jsx
+++ client-vite/component/SideBarRight.jsx
@@ -12,10 +12,7 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import DeleteIcon from '@mui/icons-material/Delete'
 import CloseIcon from '@mui/icons-material/Close'
 import ImageIcon from '@mui/icons-material/Image';
-
-const svgCommonParams = {viewBox:"0 0 480 480", width:"24px", height:"24px"}
-const sSelectionSvg = "M 120 144 L 360 144 C 408 144 432 168 432 216 L 432 264 C 432 312 408 336 360 336 L 120 336 C 72 336 48 312 48 264 C 48 216 48 264 48 216 C 48 168 72 144 120 144 Z"
-const fSelectionSvg = "M 312 72 L 360 72 C 408 72 432 96 432 144 L 432 336 C 432 384 408 408 360 408 L 312 408 C 264 408 120 312 72 312 C 24 312 24 168 72 168 C 120 168 264 72 312 72 Z"
+import { BorderIcon, CreateMapInMapIcon, FillIcon, LineIcon, TaskIcon, TextIcon } from './Icons'
 
 const commonCss = (MAP_BACKGROUND, PAGE_BACKGROUND) => ({
     position: 'fixed',
@@ -31,58 +28,6 @@ const commonCss = (MAP_BACKGROUND, PAGE_BACKGROUND) => ({
     borderBottom: `1px solid ${PAGE_BACKGROUND}`,
     borderRight: 0,
 })
-
-const LineIcon = ({MAIN_COLOR}) => (
-    <svg {...svgCommonParams}>
-        <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }}
-              d="M 408 72 C 72 72 408 408 72 408"
-        />
-    </svg>
-)
-
-const BorderIcon = ({MAIN_COLOR, selection}) => (
-    <svg {...svgCommonParams}>
-        <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }}
-              d={selection === 's' ? sSelectionSvg : fSelectionSvg}
-        />
-    </svg>
-)
-
-const FillIcon = ({MAIN_COLOR, selection}) => (
-    <svg {...svgCommonParams}>
-        <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }}
-              d={selection === 's' ? sSelectionSvg : fSelectionSvg}
-        />
-    </svg>
-)
-
-const TextIcon = ({MAIN_COLOR}) => (
-    <svg {...svgCommonParams}>
-        <g>
-            <line style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} x1={96} y1={96} x2={384} y2={96}/>
-            <line style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} x1={240} y1={384} x2={240} y2={96}/>
-        </g>
-    </svg>
-)
-
-const TaskIcon = ({MAIN_COLOR}) => (
-    <svg {...svgCommonParams}>
-        <g>
-            <ellipse style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }} cx={120} cy={240} rx={80} ry={80}/>
-            <ellipse style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:24 }} cx={360} cy={240} rx={80} ry={80}/>
-        </g>
-    </svg>
-)
-
-const CreateMapInMapIcon = ({MAIN_COLOR}) => (
-    <svg {...svgCommonParams}>
-        <g>
-            <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:48 }} d="M 216 240 L 24 240"/>
-            <path style={{ fill: MAIN_COLOR, stroke: MAIN_COLOR, strokeWidth:48 }} d="M 120 144 L 120 336"/>
-            <path style={{ fill: 'none', stroke: MAIN_COLOR, strokeWidth:24 }} d="M 288 48 L 408 240 L 288 432"/>
-        </g>
-    </svg>
-)
 
 const SpanHighlight = ({MAIN_COLOR, formatMode}) => (
     <>
