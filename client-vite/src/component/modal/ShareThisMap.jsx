c011fe91d16a6aea3c80db4233a474931c9a34fc
Krisztian Szucher <krisztian@mindboard.io>
2022-07-20 15:07:56 +0200

    more css moved to css

--- client-vite/src/component/modal/ShareThisMap.jsx
+++ client-vite/src/component/modal/ShareThisMap.jsx
@@ -1,86 +1,41 @@
 import {useSelector, useDispatch} from "react-redux";
 import { MAP_RIGHTS } from '../../core/EditorFlow'
 import { Button, FormControlLabel, FormLabel, Modal, RadioGroup, TextField, Typography, Radio } from '@mui/material'
-import { getColors } from '../../core/Colors'
 
 export function ShareThisMap() {
     const {VIEW, EDIT} = MAP_RIGHTS
-    const colorMode = useSelector(state => state.colorMode)
     const shareEmail = useSelector(state => state.shareEmail)
     const shareAccess = useSelector(state => state.shareAccess)
     const shareFeedbackMessage = useSelector(state => state.shareFeedbackMessage)
-    const {MAP_BACKGROUND} = getColors(colorMode)
     const dispatch = useDispatch()
     const setShareEmail = e => dispatch({type: 'SET_SHARE_EMAIL', payload: e.target.value})
     const setShareAccess = e => dispatch({type: 'SET_SHARE_ACCESS', payload: e.target.value})
     const createShare = _ => dispatch({type: 'CREATE_SHARE', payload: {shareEmail, shareAccess}})
     const showWs = _ => dispatch({type: 'SHOW_WS'})
     return(
-        <Modal
-            open={true}
-            onClose={_=>{}}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description">
-            <div style={{
-                position: 'relative',
-                left: '50%',
-                transform: 'translate(-50%)',
-                top: 96,
-                width: 48*8,
-                flexDirection: 'column',
-                alignItems: 'center',
-                display: 'inline-flex',
-                flexWrap: 'wrap',
-                gap: 16,
-                backgroundColor: MAP_BACKGROUND,
-                padding: 20,
-                border: `1px solid ${MAP_BACKGROUND}`,
-                borderRadius: '16px'
-            }}>
+        <Modal open={true} onClose={_=>{}} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">
+            <div id="share-this-map-modal">
                 <Typography component="h1" variant="h5" color="primary">
                     {'Share This Map'}
                 </Typography>
-                <TextField variant="outlined"
-                           fullWidth
-                           label="Share email"
-                           value={shareEmail}
-                           onChange={setShareEmail}/>
+                <TextField variant="outlined" fullWidth label="Share email" value={shareEmail} onChange={setShareEmail}/>
                 <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', gap: 32 }}>
                     <FormLabel component="legend">
                         {'Access'}
                     </FormLabel>
-                    <RadioGroup
-                        aria-label="my-aria-label"
-                        name="my-name"
-                        value={shareAccess}
-                        onChange={setShareAccess}
-                        row={true}>
+                    <RadioGroup aria-label="my-aria-label" name="my-name" value={shareAccess} onChange={setShareAccess} row={true}>
                         {[VIEW, EDIT].map((name, index) =>
-                            <FormControlLabel
-                                value={name}
-                                control={<Radio />}
-                                label={name}
-                                key={index}/>
-                        )}
+                            <FormControlLabel value={name} control={<Radio />} label={name} key={index}/>)}
                     </RadioGroup>
                 </div>
                 {shareFeedbackMessage !== '' &&
-                <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    align="center">
+                <Typography variant="body2" color="textSecondary" align="center">
                     {shareFeedbackMessage}
                 </Typography>}
-                <Button
-                    color="primary"
-                    variant="outlined"
-                    onClick={createShare}>
+                <Button color="primary" variant="outlined" onClick={createShare}>
                     {'SHARE'}
                 </Button>
-                <Button
-                    color="primary"
-                    variant="outlined"
-                    onClick={showWs}>
+                <Button color="primary" variant="outlined" onClick={showWs}>
                     {'CLOSE'}
                 </Button>
             </div>
--- client-vite/src/main.jsx
+++ client-vite/src/main.jsx
@@ -5,6 +5,12 @@ import {Provider} from "react-redux";
 import {store} from "./core/EditorFlow";
 import {Page} from "./component/unsorted/Page";
 import './component-css/Layout.css';
+import './component-css/modal/CreateTable.css'
+import './component-css/modal/Settings.css'
+import './component-css/modal/Shares.css'
+import './component-css/modal/ShareThisMap.css'
+import './component-css/modal/UpdateMapInMap.css'
+import './component-css/modal/UpdateTask.css'
 import './component-css/side/BreadcrumbMaps.css'
 import './component-css/side/Formatter.css'
 import './component-css/side/FrameCarousel.css'
@@ -16,6 +22,8 @@ import './component-css/side/ControlsRight.css'
 import './component-css/side/TabMaps.css'
 import './component-css/side/UndoRedo.css'
 import './component-css/unsorted/Auth.css'
+import './component-css/unsorted/Icons.css'
+import './component-css/unsorted/Page.css'
 
 
 ReactDOM.render(
