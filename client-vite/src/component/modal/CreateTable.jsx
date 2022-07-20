f479d652d6d981ece75b2e41ec43c6a42502d2a0
Krisztian Szucher <krisztian@mindboard.io>
2022-07-20 15:14:04 +0200

    more more css

--- client-vite/src/component/modal/CreateTable.jsx
+++ client-vite/src/component/modal/CreateTable.jsx
@@ -1,45 +1,17 @@
 import {useSelector, useDispatch} from "react-redux";
-import {
-    Box,
-    Button,
-    FormControl,
-    InputLabel,
-    Link,
-    MenuItem,
-    Modal,
-    Select,
-    TextField,
-    Typography
-} from '@mui/material'
-import { getColors } from '../../core/Colors'
+import { Box, Button, FormControl, InputLabel, MenuItem, Modal, Select, Typography } from '@mui/material'
 import { useState } from 'react'
 
 export function CreateTable() {
     const [row, setRow] = useState(1)
     const [col, setCol] = useState(1)
-    const colorMode = useSelector(state => state.colorMode)
     const interactionDisabled = useSelector(state => state.interactionDisabled)
-    const {PAGE_BACKGROUND, MAP_BACKGROUND} = getColors(colorMode)
     const dispatch = useDispatch()
     const showWs = _ => dispatch({type: 'SHOW_WS'})
     const insertTable = _=> dispatch({type: 'INSERT_TABLE', payload: {rowLen: row, colLen: col}})
     return(
         <Modal open={true} onClose={_=>{}} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">
-            {<div
-                style={{
-                    position: 'fixed',
-                    top: 48*2,
-                    right: 64,
-                    width: 6*32,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    borderRadius: 16,
-                    background: MAP_BACKGROUND,
-                    border: `1px solid ${PAGE_BACKGROUND}`,
-                    flexWrap: 'wrap',
-                    gap: 12,
-                    padding: '12px 12px 12px 12px'
-                }}>
+            {<div id="create-table">
                 <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
                     <Typography variant="string" color='primary'>{'CREATE TABLE?'}</Typography>
                 </div>
