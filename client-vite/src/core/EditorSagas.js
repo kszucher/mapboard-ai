1a2d79896aa3cba517659ec10d8deb6b278c6120
Krisztian Szucher <krisztian@mindboard.io>
2022-07-22 23:51:08 +0200

    no need for another saga

--- client-vite/src/core/EditorSagas.js
+++ client-vite/src/core/EditorSagas.js
@@ -51,28 +51,6 @@ const fetchPost = (req) => {
     }).then(resp => resp.json())
 }
 
-function* serverCallSaga({ type, payload }) {
-    yield put({type: 'INTERACTION_DISABLED'})
-    const { resp: { error, data } } = yield call(fetchPost, { type, payload })
-    yield put({type: 'INTERACTION_ENABLED'})
-    switch (error) {
-        case 'authFailWrongCred':
-            yield put({ type: 'SET_AUTH_FEEDBACK_MESSAGE', payload: 'Authentication failed, wrong credentials' })
-            localStorage.clear()
-            break
-        case 'authFailIncompleteRegistration':
-            yield put({ type: 'SET_AUTH_FEEDBACK_MESSAGE', payload: 'Authentication failed, incomplete registration' })
-            break
-        case 'signUpStep2FailWrongEmailOrConfirmationCode':
-            yield put({ type: 'SET_AUTH_FEEDBACK_MESSAGE', payload: 'Wrong email or code' })
-            break
-        case 'signUpStep2FailAlreadyActivated':
-            yield put({ type: 'SET_AUTH_FEEDBACK_MESSAGE', payload: 'Already activated' })
-            break
-    }
-    return { error, data }
-}
-
 function* authSaga () {
     while (true) {
         const { type, payload } = yield take([
@@ -94,7 +72,24 @@ function* authSaga () {
         } else if (type === 'CHECK_SET_CONFIRMATION_CODE') {
             yield put({ type: 'SET_AUTH_FEEDBACK_MESSAGE', payload: 'Invalid character' })
         } else {
-            const { error, data } = yield call(serverCallSaga, { type, payload })
+            yield put({type: 'INTERACTION_DISABLED'})
+            const { resp: { error, data } } = yield call(fetchPost, { type, payload })
+            yield put({type: 'INTERACTION_ENABLED'})
+            switch (error) {
+                case 'authFailWrongCred':
+                    yield put({ type: 'SET_AUTH_FEEDBACK_MESSAGE', payload: 'Authentication failed, wrong credentials' })
+                    localStorage.clear()
+                    break
+                case 'authFailIncompleteRegistration':
+                    yield put({ type: 'SET_AUTH_FEEDBACK_MESSAGE', payload: 'Authentication failed, incomplete registration' })
+                    break
+                case 'signUpStep2FailWrongEmailOrConfirmationCode':
+                    yield put({ type: 'SET_AUTH_FEEDBACK_MESSAGE', payload: 'Wrong email or code' })
+                    break
+                case 'signUpStep2FailAlreadyActivated':
+                    yield put({ type: 'SET_AUTH_FEEDBACK_MESSAGE', payload: 'Already activated' })
+                    break
+            }
             if (error === '') {
                 switch (type) {
                     case 'LIVE_DEMO':
