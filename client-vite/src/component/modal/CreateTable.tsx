0b2567840d69c1ead8e5d4b4e42c8a56544a4244
Krisztian Szucher <krisztian@mindboard.io>
2022-08-02 17:20:00 +0200

    the first correct tsx file

--- client-vite/src/component/modal/CreateTable.tsx
+++ client-vite/src/component/modal/CreateTable.tsx
@@ -1,38 +1,52 @@
-import {useSelector, useDispatch} from "react-redux";
-import { Box, Button, FormControl, InputLabel, MenuItem, Modal, Select, Typography } from '@mui/material'
+import {useSelector, useDispatch, RootStateOrAny} from "react-redux";
+import { Box, Button, FormControl, InputLabel, MenuItem, Modal, Select, Typography, SelectChangeEvent } from '@mui/material'
 import { useState } from 'react'
 
 export function CreateTable() {
-    const [row, setRow] = useState(1)
-    const [col, setCol] = useState(1)
-    const interactionDisabled = useSelector(state => state.interactionDisabled)
+    const [row, setRow] = useState('1')
+    const [col, setCol] = useState('1')
+    const interactionDisabled = useSelector((state: RootStateOrAny) => state.interactionDisabled)
     const dispatch = useDispatch()
-    const showWs = _ => dispatch({type: 'SHOW_WS'})
-    const insertTable = _=> dispatch({type: 'INSERT_TABLE', payload: {rowLen: row, colLen: col}})
+    const showWs = () => dispatch({type: 'SHOW_WS'})
+    const insertTable = () => dispatch({type: 'INSERT_TABLE', payload: {rowLen: parseInt(row), colLen: parseInt(col)}})
     return(
-        <Modal open={true} onClose={_=>{}} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">
-            {<div id="create-table">
+        <Modal
+            open={true}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+        >
+            <div id="create-table">
                 <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                    <Typography variant="string" color='primary'>{'CREATE TABLE?'}</Typography>
+                    <Typography variant='subtitle2' color='primary'>{'CREATE TABLE?'}</Typography>
                 </div>
                 <div style={{ display: "flex", flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap', gap: 12 }}>
                     <Box sx={{ minWidth: 120 }}>
                         <FormControl fullWidth size="small" variant="standard" >
                             <InputLabel>{'Row'}</InputLabel>
-                            <Select value={row} onChange={e => setRow(e.target.value)}>
-                                {[1,2,3,4,5,6,7,8,9,10].map((el, idx) => (
-                                    <MenuItem value={el} key={idx}>{el}</MenuItem>
-                                ))}>
+                            <Select
+                                value={row}
+                                onChange={(event: SelectChangeEvent) => setRow(event.target.value as string)}
+                            >
+                                {
+                                    [1,2,3,4,5,6,7,8,9,10].map((el, idx) => (
+                                        <MenuItem value={el} key={idx}>{el}</MenuItem>
+                                    ))
+                                }
                             </Select>
                         </FormControl>
                     </Box>
                     <Box sx={{ minWidth: 120 }}>
                         <FormControl fullWidth size="small" variant="standard" >
                             <InputLabel>{'Column'}</InputLabel>
-                            <Select value={col} onChange={e => setCol(e.target.value)}>
-                                {[1,2,3,4,5,6,7,8,9,10].map((el, idx) => (
-                                    <MenuItem value={el} key={idx}>{el}</MenuItem>
-                                ))}>
+                            <Select
+                                value={col}
+                                onChange={(event: SelectChangeEvent) => setCol(event.target.value as string)}
+                            >
+                                {
+                                    [1,2,3,4,5,6,7,8,9,10].map((el, idx) => (
+                                        <MenuItem value={el} key={idx}>{el}</MenuItem>
+                                    ))
+                                }
                             </Select>
                         </FormControl>
                     </Box>
@@ -43,7 +57,7 @@ export function CreateTable() {
                         {'CANCEL'}
                     </Button>
                 </div>
-            </div>}
+            </div>
         </Modal>
     )
 }
