a45e0004a6220438f955911fc41046cb6a2bb2f5
Krisztian Szucher <krisztian@mindboard.io>
2022-08-04 23:37:24 +0200

    auth ok

--- client-vite/src/component/unsorted/Auth.tsx
+++ client-vite/src/component/unsorted/Auth.tsx
@@ -14,19 +14,19 @@ export default function Auth() {
     const authFeedbackMessage = useSelector((state: RootStateOrAny) => state.authFeedbackMessage)
 
     const dispatch = useDispatch()
-    const setName = e => dispatch({type: 'SET_NAME', payload: e.target.value})
-    const setEmail = e => dispatch({type: 'SET_EMAIL', payload: e.target.value})
-    const setPassword = e => dispatch({type: 'SET_PASSWORD', payload: e.target.value})
-    const setPasswordAgain = e => dispatch({type: 'SET_PASSWORD_AGAIN', payload: e.target.value})
-    const checkSetConfirmationCode = e => dispatch({type: 'CHECK_SET_CONFIRMATION_CODE', payload: e.target.value})
-    const signInPanel = _ => dispatch({type: 'SIGN_IN_PANEL'})
-    const signUpPanel = _ => dispatch({type: 'SIGN_UP_PANEL'})
-    const signUpStep1Panel = _ => dispatch({type: 'SIGN_UP_STEP_1_PANEL'})
-    const signUpStep2Panel = _ => dispatch({type: 'SIGN_UP_STEP_2_PANEL'})
-    const signIn = _ => dispatch({type: 'SIGN_IN', payload: { cred: { email, password } }})
-    const signUpStep1 = _ => dispatch({type: 'SIGN_UP_STEP_1', payload: { cred: { name, email, password } } })
-    const signUpStep2 = _ => dispatch({type: 'SIGN_UP_STEP_2', payload: { cred: { email, confirmationCode: parseInt(confirmationCode) } } })
-    const liveDemo = _ => dispatch({type: 'LIVE_DEMO'})
+    const setName = (value: string) => dispatch({type: 'SET_NAME', payload: value})
+    const setEmail = (value: string) => dispatch({type: 'SET_EMAIL', payload: value})
+    const setPassword = (value: string) => dispatch({type: 'SET_PASSWORD', payload: value})
+    const setPasswordAgain = (value: string) => dispatch({type: 'SET_PASSWORD_AGAIN', payload: value})
+    const checkSetConfirmationCode = (value: string) => dispatch({type: 'CHECK_SET_CONFIRMATION_CODE', payload: value})
+    const signInPanel = () => dispatch({type: 'SIGN_IN_PANEL'})
+    const signUpPanel = () => dispatch({type: 'SIGN_UP_PANEL'})
+    const signUpStep1Panel = () => dispatch({type: 'SIGN_UP_STEP_1_PANEL'})
+    const signUpStep2Panel = () => dispatch({type: 'SIGN_UP_STEP_2_PANEL'})
+    const signIn = () => dispatch({type: 'SIGN_IN', payload: { cred: { email, password } }})
+    const signUpStep1 = () => dispatch({type: 'SIGN_UP_STEP_1', payload: { cred: { name, email, password } } })
+    const signUpStep2 = () => dispatch({type: 'SIGN_UP_STEP_2', payload: { cred: { email, confirmationCode: parseInt(confirmationCode) } } })
+    const liveDemo = () => dispatch({type: 'LIVE_DEMO'})
 
     return (
         <div id="auth">
@@ -65,7 +65,7 @@ export default function Auth() {
                     fullWidth
                     label="Your First Name"
                     value={name}
-                    onChange={setName}
+                    onChange={({target: {value}}) => setName(value)}
                     autoFocus
                 />
             }
@@ -77,7 +77,7 @@ export default function Auth() {
                     fullWidth
                     label="Email"
                     value={email}
-                    onChange={setEmail}
+                    onChange={({target: {value}}) => setEmail(value)}
                 />
             }
             {
@@ -88,7 +88,7 @@ export default function Auth() {
                     fullWidth
                     label="Password"
                     value={password}
-                    onChange={setPassword}
+                    onChange={({target: {value}}) => setPassword(value)}
                     type="password"
                 />
             }
@@ -100,7 +100,7 @@ export default function Auth() {
                     fullWidth
                     label="Password Again"
                     value={passwordAgain}
-                    onChange={setPasswordAgain}
+                    onChange={({target: {value}}) => setPasswordAgain(value)}
                     type="password"
                 />
             }
@@ -112,7 +112,7 @@ export default function Auth() {
                     fullWidth
                     label="Confirmation Code"
                     value={confirmationCode}
-                    onChange={checkSetConfirmationCode}
+                    onChange={({target: {value}}) => checkSetConfirmationCode(value)}
                     autoFocus
                 />
             }
