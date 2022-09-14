f246b127e30af1ab8a8f1d406dd6476f3bf2c1a5
Krisztian Szucher <krisztian@szucher.com>
2022-09-14 23:46:51 +0200

    one more css converted

--- client-vite/src/component/modal/CreateTable.tsx
+++ client-vite/src/component/modal/CreateTable.tsx
@@ -10,23 +10,20 @@ export function CreateTable() {
     const showWs = () => dispatch({type: 'SHOW_WS'})
     const insertTable = () => dispatch({type: 'INSERT_TABLE', payload: {rowLen: parseInt(row), colLen: parseInt(col)}})
     return(
-        <Modal
-            open={true}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-        >
-            <div id="create-table">
+        <Modal open={true} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">
+            <div className="_bg fixed top-[calc(48*2px)] right-[64px] w-[calc(6*32px)] flex flex-col gap-3 p-3 rounded-2xl">
                 <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                    <Typography variant='subtitle2' color='primary'>{'CREATE TABLE?'}</Typography>
+                    <Typography variant='subtitle2' color='primary'>
+                        {'CREATE TABLE?'}
+                    </Typography>
                 </div>
                 <div style={{ display: "flex", flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap', gap: 12 }}>
                     <Box sx={{ minWidth: 120 }}>
                         <FormControl fullWidth size="small" variant="standard" >
-                            <InputLabel>{'Row'}</InputLabel>
-                            <Select
-                                value={row}
-                                onChange={(event: SelectChangeEvent) => setRow(event.target.value as string)}
-                            >
+                            <InputLabel>
+                                {'Row'}
+                            </InputLabel>
+                            <Select value={row} onChange={(event: SelectChangeEvent) => setRow(event.target.value as string)}>
                                 {
                                     [1,2,3,4,5,6,7,8,9,10].map((el, idx) => (
                                         <MenuItem value={el} key={idx}>{el}</MenuItem>
@@ -37,11 +34,10 @@ export function CreateTable() {
                     </Box>
                     <Box sx={{ minWidth: 120 }}>
                         <FormControl fullWidth size="small" variant="standard" >
-                            <InputLabel>{'Column'}</InputLabel>
-                            <Select
-                                value={col}
-                                onChange={(event: SelectChangeEvent) => setCol(event.target.value as string)}
-                            >
+                            <InputLabel>
+                                {'Column'}
+                            </InputLabel>
+                            <Select value={col} onChange={(event: SelectChangeEvent) => setCol(event.target.value as string)}>
                                 {
                                     [1,2,3,4,5,6,7,8,9,10].map((el, idx) => (
                                         <MenuItem value={el} key={idx}>{el}</MenuItem>
