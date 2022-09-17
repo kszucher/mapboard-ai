8cc95697766ae688211fb16aab668b8140b95135
Krisztian Szucher <krisztian@szucher.com>
2022-09-17 15:01:56 +0200

    one more removed

--- client-vite/src/component/side/ProfileButton.tsx
+++ client-vite/src/component/side/ProfileButton.tsx
@@ -7,7 +7,7 @@ export function ProfileButton () {
     const dispatch = useDispatch()
     const openMoreMenu = (currentTarget: EventTarget) => dispatch({type: 'OPEN_MORE_MENU', payload: {currentTarget}})
     return (
-        <div id="profile-button">
+        <div className="_bg fixed right-0 w-[40px] h-[40px] py-1 px-3 flex flex-row flex-center items-center border-t-0 border-r-0 rounded-bl-2xl">
             <IconButton
                 color='secondary'
                 onClick={({currentTarget}) => openMoreMenu(currentTarget)}>
