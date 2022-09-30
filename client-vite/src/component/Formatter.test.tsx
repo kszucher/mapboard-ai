cccb8ec1e6b657ef5db28fdbc481a9530bba48a1
Krisztian Szucher <krisztian@szucher.com>
2022-09-30 19:17:17 +0200

    test Formatter

--- client-vite/src/component/Formatter.test.tsx
+++ client-vite/src/component/Formatter.test.tsx
@@ -1,16 +1,15 @@
-import {render} from "@testing-library/react"
+import {render, screen} from "@testing-library/react"
 import {Provider} from "react-redux"
-import {CreateTable} from "./CreateTable"
+import {Formatter} from "./Formatter"
 import { describe, it } from 'vitest'
 import {store} from "../core/EditorFlow";
 import React from "react";
 
-describe("CreateTable test", () => {
+describe("Formatter test", () => {
   test("should work", () => {
     const {} = render(
       <Provider store={store}>
-        <CreateTable/>
+        <Formatter/>
       </Provider>,
     )
-  })
-})
+  })})
