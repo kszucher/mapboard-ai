9e2dd36be5bee796e5e4ccf3de52c14f8f90f054
Krisztian Szucher <krisztian@szucher.com>
2022-10-02 17:20:44 +0200

    cleanup

--- client-vite/src/component/Formatter.test.tsx
+++ client-vite/src/component/Formatter.test.tsx
@@ -14,14 +14,5 @@ describe("Formatter test", () => {
     )
   })})
 
-// ok it is now very clear what I need to do
-// - setNodeParamsNoTrigger parametrically --> this will change redux, but WON'T update map
-// - now I can see if the UI loads the correct things
-// - also, I can trigger ui changes, and see if the state values will be updated accordingly
-// - this is simple, but the point of testing is making stuff simple --> inspecting if clicks result in state changes
-// - believe me, this is a GOOD exercise to get the BASIC syntax, and to click on and inspect on things programmatically
-
+// click through the ui element and check if redux changes accordingly
 // https://stackoverflow.com/questions/68731656/how-to-test-a-redux-action-that-dispatch-other-action
-
-
-// let's say that I am putting mapStack into redux, that I can do...
