67a3889948cfb0860dd31b0cf2775957fc745266
Krisztian Szucher <krisztian@szucher.com>
2022-10-09 13:05:59 +0200

    first working test with interaction to the store

--- client-vite/src/component/Formatter.test.tsx
+++ client-vite/src/component/Formatter.test.tsx
@@ -1,18 +1,26 @@
-import {render, screen} from "@testing-library/react"
+import {render, screen,} from "@testing-library/react"
+import user from "@testing-library/user-event"
 import {Provider} from "react-redux"
 import {Formatter} from "./Formatter"
 import { describe, it } from 'vitest'
-import {store} from "../core/EditorFlow";
+import {actions, MapRight, store} from "../core/EditorFlow";
 import React from "react";
+import {TextTypes} from "../core/DefaultProps";
 
 describe("Formatter test", () => {
-  test("should work", () => {
+  test("should work", async () => {
+    store.dispatch(actions.parseRespPayload({mapRight: MapRight.EDIT})) // allow buttons to be pressed
     const {} = render(
       <Provider store={store}>
         <Formatter/>
       </Provider>,
     )
+
+    await user.click(screen.getByRole('button', {name: /h1/i}))
+    expect(store.getState().node.textFontSize).toEqual(TextTypes.h1)
+
+    await user.click(screen.getByRole('button', {name: /h2/i}))
+    expect(store.getState().node.textFontSize).toEqual(TextTypes.h2)
   })})
 
-// click through the ui element and check if redux changes accordingly
 // https://stackoverflow.com/questions/68731656/how-to-test-a-redux-action-that-dispatch-other-action
