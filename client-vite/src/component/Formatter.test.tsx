12a7f83c07d747dc0bccce4716d8c246778dfbd8
Krisztian Szucher <krisztian@szucher.com>
2022-10-09 15:11:12 +0200

    organized tests

--- client-vite/src/component/Formatter.test.tsx
+++ client-vite/src/component/Formatter.test.tsx
@@ -5,7 +5,7 @@ import {Formatter} from "./Formatter"
 import { describe, it } from 'vitest'
 import {actions, MapRight, store} from "../core/EditorFlow";
 import React from "react";
-import {TextTypes, WidthTypes} from "../core/DefaultProps";
+import {LineTypes, TextTypes, WidthTypes} from "../core/DefaultProps";
 import {colorList} from "../core/Colors";
 
 describe("Formatter test", () => {
@@ -16,14 +16,14 @@ describe("Formatter test", () => {
         density: undefined,
         alignment: undefined,
         selection: undefined,
-        lineWidth: undefined,
-        lineType: undefined,
+        lineWidth: WidthTypes.w1,
+        lineType: LineTypes.bezier,
         lineColor: colorList[0][0],
         borderWidth: WidthTypes.w1,
-        borderColor: colorList[0][1],
-        fillColor: undefined,
+        borderColor: colorList[0][0],
+        fillColor: colorList[0][0],
         textFontSize: TextTypes.h1,
-        textColor: colorList[0][2],
+        textColor: colorList[0][0],
         taskStatus: undefined,
       }
     }))
@@ -33,31 +33,67 @@ describe("Formatter test", () => {
       </Provider>,
     )
   })
-  test('text', async () => {
+  test('set', async () => {
     await user.click(screen.getByRole('button', {name: 'text'}))
-    // textFontSize
-    await user.click(screen.getByRole('button', {name: /h2/i}))
-    expect(store.getState().node.textFontSize).toEqual(TextTypes.h2)
-    // textColor
-    const element = screen.getByLabelText(colorList[1][2])
+    await user.click(screen.getByLabelText(colorList[0][1]))
+    await user.click(screen.getByRole('button', {name: 'h2'}))
+    await user.click(screen.getByRole('button', {name: 'border'}))
+    await user.click(screen.getByLabelText(colorList[0][1]))
+    await user.click(screen.getByRole('button', {name: 'w2'}))
+    await user.click(screen.getByRole('button', {name: 'fill'}))
+    await user.click(screen.getByLabelText(colorList[0][1]))
+    await user.click(screen.getByRole('button', {name: 'line'}))
+    await user.click(screen.getByLabelText(colorList[0][1]))
+    await user.click(screen.getByRole('button', {name: 'w2'}))
+    await user.click(screen.getByRole('button', {name: 'edge'}))
+
+    expect(store.getState().node).toEqual({
+      density: undefined,
+      alignment: undefined,
+      selection: undefined,
+      lineWidth: WidthTypes.w2,
+      lineType: LineTypes.edge,
+      lineColor: colorList[0][1],
+      borderWidth: WidthTypes.w2,
+      borderColor: colorList[0][1],
+      fillColor: colorList[0][1],
+      textFontSize: TextTypes.h2,
+      textColor: colorList[0][1],
+      taskStatus: undefined,
+    })
+  })
+  test('reset', async () => {
+    await user.click(screen.getByRole('button', {name: 'text'}))
+    await user.click(screen.getByRole('button', {name: 'RESET'}))
+    await user.click(screen.getByRole('button', {name: 'border'}))
+    await user.click(screen.getByRole('button', {name: 'RESET'}))
+    await user.click(screen.getByRole('button', {name: 'fill'}))
+    await user.click(screen.getByRole('button', {name: 'RESET'}))
+    await user.click(screen.getByRole('button', {name: 'line'}))
+    await user.click(screen.getByRole('button', {name: 'edge'}))
+    await user.click(screen.getByRole('button', {name: 'RESET'}))
+
+    expect(store.getState().node).toEqual({
+      density: undefined,
+      alignment: undefined,
+      selection: undefined,
+      lineWidth: 'clear',
+      lineType: 'clear',
+      lineColor: 'clear',
+      borderWidth: 'clear',
+      borderColor: 'clear',
+      fillColor: 'clear',
+      textFontSize: 'clear',
+      textColor: 'clear',
+      taskStatus: undefined,
+    })
+  })
+
+  test('color change', async () => {
+    const element = screen.getByLabelText(colorList[0][2])
     expect(element.getAttribute('stroke')).toEqual('none')
     await user.click(element)
     expect(element.getAttribute('stroke')).toEqual('#9040b8')
-    expect(store.getState().node.textColor).toEqual(colorList[1][2])
-    // reset
-    await user.click(screen.getByRole('button', {name: 'RESET'}))
-    expect(store.getState().node.textColor).toEqual('clear')
-    expect(store.getState().node.textFontSize).toEqual('clear')
-  })
-  test('border', async () => {
-    await user.click(screen.getByRole('button', {name: 'border'}))
-    // borderWidth
-    await user.click(screen.getByRole('button', {name: 'w2'}))
-    expect(store.getState().node.borderWidth).toEqual(WidthTypes.w2)
-    // borderColor
-    await user.click(screen.getByLabelText(colorList[1][1]))
-    expect(store.getState().node.borderColor).toEqual(colorList[1][1])
-    // reset
   })
 })
 
