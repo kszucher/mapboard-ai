ae795c9bed58e5a245760c0c008db94c58f95879
Krisztian Szucher <krisztian@szucher.com>
2022-10-09 14:29:48 +0200

    more compact

--- client-vite/src/component/Formatter.test.tsx
+++ client-vite/src/component/Formatter.test.tsx
@@ -9,7 +9,6 @@ import {TextTypes, WidthTypes} from "../core/DefaultProps";
 import {colorList} from "../core/Colors";
 
 describe("Formatter test", () => {
-
   beforeEach(() => {
     store.dispatch(actions.parseRespPayload({
       mapRight: MapRight.EDIT,
@@ -34,41 +33,32 @@ describe("Formatter test", () => {
       </Provider>,
     )
   })
-
   test('text', async () => {
     await user.click(screen.getByRole('button', {name: 'text'}))
-
     // textFontSize
     await user.click(screen.getByRole('button', {name: /h2/i}))
     expect(store.getState().node.textFontSize).toEqual(TextTypes.h2)
-
     // textColor
     const element = screen.getByLabelText(colorList[1][2])
     expect(element.getAttribute('stroke')).toEqual('none')
     await user.click(element)
     expect(element.getAttribute('stroke')).toEqual('#9040b8')
     expect(store.getState().node.textColor).toEqual(colorList[1][2])
-
     // reset
     await user.click(screen.getByRole('button', {name: 'RESET'}))
     expect(store.getState().node.textColor).toEqual('clear')
     expect(store.getState().node.textFontSize).toEqual('clear')
     // TODO may figure out how to select ALL to see NONE has the stroke attribute
   })
-
   test('border', async () => {
     await user.click(screen.getByRole('button', {name: 'border'}))
-
     // borderWidth
     await user.click(screen.getByRole('button', {name: 'w2'}))
     expect(store.getState().node.borderWidth).toEqual(WidthTypes.w2)
-
     // borderColor
     await user.click(screen.getByLabelText(colorList[1][1]))
     expect(store.getState().node.borderColor).toEqual(colorList[1][1])
-
     // reset
-
   })
 })
 
