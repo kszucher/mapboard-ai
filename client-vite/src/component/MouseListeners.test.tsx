43a62eb612b25b6d20b578c00b49b4356ca34b91
Krisztian Szucher <krisztian@szucher.com>
2022-10-29 12:33:01 +0200

    test fix

--- client-vite/src/component/MouseListeners.test.tsx
+++ client-vite/src/component/MouseListeners.test.tsx
@@ -5,3 +5,6 @@
 // also we can use some custom coordinates, that shall do it
 // and this way EVERY mouse feature will be tested
 // sounds like a great plan!
+
+import { describe, it } from 'vitest'
+describe("MouseListeners test", () => {})
