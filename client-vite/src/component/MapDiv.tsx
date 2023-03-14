6314f56761d4d48e635f6c92a5614050e77abd54
Krisztian Szucher <krisztian@szucher.com>
2023-03-14 18:21:24 +0100

    cleanup

--- client-vite/src/component/MapDiv.tsx
+++ client-vite/src/component/MapDiv.tsx
@@ -1,13 +1,12 @@
 // @ts-ignore
 import katex from "katex/dist/katex.mjs"
-import {FC, Fragment, useEffect, useRef} from "react"
+import {FC, Fragment, useEffect} from "react"
 import {RootStateOrAny, useDispatch, useSelector} from "react-redux"
 import {getColors} from "../core/Colors"
-import {M, N} from "../types/DefaultProps"
+import {N} from "../types/DefaultProps"
 import {getNodeById, m2ml} from "../core/MapUtils"
-import {copy, getLatexString} from "../core/Utils"
+import {getLatexString} from "../core/Utils"
 import {actions} from "../core/EditorFlow";
-import {useMapDispatch} from "../hooks/UseMapDispatch";
 import {setEndOfContentEditable} from "./MapDivUtils";
 
 const getInnerHtml = (n: N) => {
@@ -35,21 +34,9 @@ export const MapDiv: FC = () => {
 
   const dispatch = useDispatch()
 
-  // const nodesRef = useRef(null) as any
-  // const getNodeRef = () => {
-  //   if (!nodesRef.current) {
-  //     nodesRef.current = new Map()
-  //   }
-  //   return nodesRef.current
-  // }
-
   useEffect(() => {
     if (editedNodeId.length) {
-
-      console.log('should start edit...')
-
       const editedDiv = document.getElementById(`${editedNodeId}_div`) as HTMLDivElement
-      // editedDiv.focus()
       if (editType === 'append') {
         editedDiv.innerHTML = getNodeById(ml, editedNodeId).content
       }
@@ -63,9 +50,7 @@ export const MapDiv: FC = () => {
       style={{
         position: 'absolute',
         display: 'flex',
-        // pointerEvents: 'none'
       }}
-
     >
       <>
         {ml.map((n: N) => (
@@ -75,18 +60,10 @@ export const MapDiv: FC = () => {
               !n.hasCell &&
               <div
                 id={`${n.nodeId}_div`}
-                // ref={(el) => {
-                //   const nodeRef = getNodeRef()
-                //   if (el) {
-                //     nodeRef.set(n.nodeId, el)
-                //   } else {
-                //     nodeRef.delete(n.nodeId)
-                //   }
-                // }}
                 style = {{
                   left: 1 + n.nodeStartX,
                   top: 1 + n.nodeY - n.selfH / 2,
-                  minWidth: (m.g.density === 'large'? 0 : -3) + n.selfW - m.g.padding - 2 + 10, // do we need +10?
+                  minWidth: (m.g.density === 'large'? 0 : -3) + n.selfW - m.g.padding - 2,
                   minHeight: (m.g.density === 'large'? -2 : -1) + n.selfH - m.g.padding,
                   paddingLeft: (m.g.density === 'large'? 0 : 3) + m.g.padding - 2,
                   paddingTop: (m.g.density === 'large'? 0 : 0) + m.g.padding - 2,
@@ -143,7 +120,6 @@ export const MapDiv: FC = () => {
   )
 }
 
-// TODO add doubleclick and finish up with all the things... !!!!
 // TODO do insert SOU stuff
 // TODO do move/selection stuff so useMD can be replaced
 // reintroduce column: isGeneric: 1/0 and based on that we either call an action, or a genericMapAction
