d02f07680636f8605a9ca9478204b7e94fe3db47
Krisztian Szucher <krisztian@szucher.com>
2023-04-17 13:49:41 +0200

    insertCellNodeList

--- client-vite/src/map/MapInsert.ts
+++ client-vite/src/map/MapInsert.ts
@@ -1,7 +1,8 @@
 import {M, P} from "../state/MapPropTypes"
-import {genHash, get_cc_indices, get_table_indices} from "../core/Utils"
+import {genHash, get_cc_indices, get_cr_indices, get_table_indices} from "../core/Utils"
 import {selectNode} from "./MapReducer"
 import {
+  get_CC_siblingCount,
   get_CR_siblingCount,
   getDefaultNode,
   getInsertParentNode,
@@ -16,14 +17,19 @@ import {
   sortPath
 } from "./MapUtils"
 
-const insertNode = (m, attributes: object) => {
+const insertNode = (m: M, attributes: object) => {
   m.push(getDefaultNode({...attributes, nodeId: 'node' + genHash(8)}))
   m.sort(sortPath)
 }
 
-const insertNodeList = (m, pList: P[]) => {
+const insertNodeList = (m: M, pList: P[]) => {
   m.push(...pList.map(p => getDefaultNode({path: structuredClone(p), nodeId: 'node' + genHash(8)})))
   m.sort(sortPath)
+}
+
+const insertCellNodeList = (m: M, p: P, indices: number[][]) => {
+  insertNodeList(m, indices.map(el => [...p, 'c', ...el]))
+  insertNodeList(m, indices.map(el => [...p, 'c', ...el, 's', 0]))
 }
 
 export const insert_select_S_O = (m: M, attributes: object) => {
@@ -48,27 +54,33 @@ export const insert_select_S_D = (m: M, attributes: object) => {
 
 export const insert_select_table = (m: M, r: number, c: number) => {
   insert_select_S_O(m, {}) // note: getLS will be up-to-date in the next phase!!!
-  insertNodeList(m, get_table_indices(r, c).map(el => [...getLS(m).path, 'c', ...el]))
-  insertNodeList(m, get_table_indices(r, c).map(el => [...getLS(m).path, 'c', ...el, 's', 0]))
+  insertCellNodeList(m, getLS(m).path, get_table_indices(r, c))
 }
 
 export const insert_CC_R = (m: M) => {
   inc_gt_C_R(m)
   const r = get_CR_siblingCount(m, getLS(m).path)
-  const c = getLS(m).path.at(-1) + 1
-  const cc_indices = get_cc_indices(r, c)
-  insertNodeList(m, cc_indices.map(el => [...getLS(m).path.slice(0, -2), ...el]))
-  insertNodeList(m, cc_indices.map(el => [...getLS(m).path.slice(0, -2), ...el, 's', 0]))
+  const c = getLS(m).path.at(-1) as number + 1
+  insertCellNodeList(m, getLS(m).path.slice(0, -3), get_cc_indices(r, c))
 }
 
 export const insert_CC_L = (m: M) => {
   inc_gte_C_R(m)
+  const r = get_CR_siblingCount(m, getLS(m).path)
+  const c = getLS(m).path.at(-1) as number
+  insertCellNodeList(m, getLS(m).path.slice(0, -3), get_cc_indices(r, c))
 }
 
 export const insert_CR_U = (m: M) => {
   inc_gt_C_D(m)
+  const r = getLS(m).path.at(-2) as number + 1
+  const c = get_CC_siblingCount(m, getLS(m).path)
+  insertCellNodeList(m, getLS(m).path.slice(0, -3), get_cr_indices(r, c))
 }
 
 export const insert_CR_D = (m: M) => {
   inc_gte_C_D(m)
+  const r = getLS(m).path.at(-2) as number
+  const c = get_CC_siblingCount(m, getLS(m).path)
+  insertCellNodeList(m, getLS(m).path.slice(0, -3), get_cr_indices(r, c))
 }
