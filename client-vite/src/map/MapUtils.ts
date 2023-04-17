98b5e3bfa35506330302ef1760e391fce68ee258
Krisztian Szucher <krisztian@szucher.com>
2023-04-17 13:59:30 +0200

    reorder

--- client-vite/src/map/MapUtils.ts
+++ client-vite/src/map/MapUtils.ts
@@ -7,27 +7,6 @@ export const sortPath = (a, b) => a.path.map(el => isNaN(el) ? el: 1000 + el).jo
 export const sortNode = (a, b) => a.nodeId > b.nodeId ? 1 : -1
 
 export const getPattern = (p: P) => p.filter(pi => isNaN(pi as any)).join('')
-export const getParentPath = (p: P) => (getPattern(p).endsWith('d') || getPattern(p).endsWith('s')) ? p.slice(0, -2) : p.slice(0, -3)
-export const getClosestStructParentPath = (p: P) => (getPattern(p).endsWith('ds') || getPattern(p).endsWith('ss')) ? p.slice(0, -2) : p.slice(0, -5)
-export const getClosestStructChildPath = (p: P) => getPattern(p).endsWith('c') ? [...p, 's', 0] : p
-export const getPathDir = (p: P) => p[3] ? -1 : 1
-export const getNodeById = (m: M, nodeId: string) => m.find(n => n.nodeId === nodeId) as GN
-export const getNodeByPath = (m: M, p: P) => m.find(n => isEqual(n.path, p)) as GN
-export const getParentNodeByPath = (m: M, p: P) => getNodeByPath(m, getParentPath(p)) as N
-export const getG = (m: M) => m.filter(n => n.path.length === 1).at(0) as G
-export const getLS = (m: M) => m.filter(n => n.path.length > 1).reduce((a, b) => a.selected > b.selected ? a : b)
-export const getDefaultNode = (attributes?: any) => structuredClone({...nSaveAlways, ...nSaveOptional, ...nSaveNever, ...attributes})
-export const getEditedNode = (m: M, p: P) => getNodeByPath(m, getClosestStructChildPath(p))
-export const getInsertParentNode = (m: M) => getNodeByPath(m, getLS(m).path.length === 2 ? ['r', 0, 'd', 0] as P: getLS(m).path)
-export const getSelection = (m: M) => m.filter(n => n.selected)
-export const getSelectionFamily = (m: M) => m.filter(n => getSelection(m).map(n => n.path).some(p => is_S_S_O(p, n.path)))
-export const getSelectionProp = (m: M, prop: keyof N) => isArrayOfEqualValues(getSelection(m).map(n => n[prop])) ? getLS(m)[prop] : null
-export const getSelectionFamilyProp = (m: M, prop: keyof N) => isArrayOfEqualValues(getSelectionFamily(m).map(n => n[prop])) ? getLS(m)[prop] : null
-export const get_CR_siblingCount = (m: M, p: P) => m.filter(n => is_CR(p, n.path)).length
-export const get_CC_siblingCount = (m: M, p: P) => m.filter(n => is_CC(p, n.path)).length
-
-export const setSelection = (m: M, prop: keyof N, value: any) => getSelection(m).forEach(n => n[prop] = value)
-export const setSelectionFamily = (m: M, prop: keyof N, value: any) => getSelectionFamily(m).forEach(n => n[prop] = value)
 
 export const is_G = (p: P) => getPattern(p).endsWith('g')
 export const is_R = (p: P) => getPattern(p).endsWith('r')
@@ -55,6 +34,28 @@ export const isDirStructSelected = (m: M) => getLS(m).path.length === 6
 export const isCellSelected = (m: M) => is_C(getLS(m).path) && !m.find(n => n.selected && !is_C(n.path))
 export const isCellRowSelected = (m: M) => is_C(getLS(m).path) && !m.find(n => n.selected && !is_C(n.path) || !n.selected && is_CR(getLS(m).path, n.path))
 export const isCellColSelected = (m: M) => is_C(getLS(m).path) && !m.find(n => n.selected && !is_C(n.path) || !n.selected && is_CC(getLS(m).path, n.path))
+
+export const getParentPath = (p: P) => (getPattern(p).endsWith('d') || getPattern(p).endsWith('s')) ? p.slice(0, -2) : p.slice(0, -3)
+export const getClosestStructParentPath = (p: P) => (getPattern(p).endsWith('ds') || getPattern(p).endsWith('ss')) ? p.slice(0, -2) : p.slice(0, -5)
+export const getClosestStructChildPath = (p: P) => getPattern(p).endsWith('c') ? [...p, 's', 0] : p
+export const getPathDir = (p: P) => p[3] ? -1 : 1
+export const getNodeById = (m: M, nodeId: string) => m.find(n => n.nodeId === nodeId) as GN
+export const getNodeByPath = (m: M, p: P) => m.find(n => isEqual(n.path, p)) as GN
+export const getParentNodeByPath = (m: M, p: P) => getNodeByPath(m, getParentPath(p)) as N
+export const getG = (m: M) => m.filter(n => n.path.length === 1).at(0) as G
+export const getLS = (m: M) => m.filter(n => n.path.length > 1).reduce((a, b) => a.selected > b.selected ? a : b)
+export const getDefaultNode = (attributes?: any) => structuredClone({...nSaveAlways, ...nSaveOptional, ...nSaveNever, ...attributes})
+export const getEditedNode = (m: M, p: P) => getNodeByPath(m, getClosestStructChildPath(p))
+export const getInsertParentNode = (m: M) => getNodeByPath(m, getLS(m).path.length === 2 ? ['r', 0, 'd', 0] as P: getLS(m).path)
+export const getSelection = (m: M) => m.filter(n => n.selected)
+export const getSelectionFamily = (m: M) => m.filter(n => getSelection(m).map(n => n.path).some(p => is_S_S_O(p, n.path)))
+export const getSelectionProp = (m: M, prop: keyof N) => isArrayOfEqualValues(getSelection(m).map(n => n[prop])) ? getLS(m)[prop] : null
+export const getSelectionFamilyProp = (m: M, prop: keyof N) => isArrayOfEqualValues(getSelectionFamily(m).map(n => n[prop])) ? getLS(m)[prop] : null
+export const get_CR_siblingCount = (m: M, p: P) => m.filter(n => is_CR(p, n.path)).length
+export const get_CC_siblingCount = (m: M, p: P) => m.filter(n => is_CC(p, n.path)).length
+
+export const setSelection = (m: M, prop: keyof N, value: any) => getSelection(m).forEach(n => n[prop] = value)
+export const setSelectionFamily = (m: M, prop: keyof N, value: any) => getSelectionFamily(m).forEach(n => n[prop] = value)
 
 export const inc_pi = (p: P, at: number) => structuredClone(p).map((p, i) => i === at ? p + 1 : p)
 export const dec_pi = (p: P, at: number) => structuredClone(p).map((p, i) => i === at ? p - 1 : p)
