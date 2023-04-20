6c0a754b8f1686c9c048021097b8c3c5d2418c36
Krisztian Szucher <krisztian@szucher.com>
2023-04-20 23:28:43 +0200

    little cleanup

--- client-vite/src/node/NodeDelete.ts
+++ client-vite/src/node/NodeDelete.ts
@@ -1,57 +1,5 @@
 import {M} from "../state/MapPropTypes";
 
-export const structDeleteReselect = (m: M) => {
-  // let ln = getMapData(m, sc.lastPath)
-  // // calculate jump back
-  // let closestNode = ln
-  // for (let i = 0; i < sc.structSelectedPathList.length; i++) {
-  //   let n = getMapData(m, sc.structSelectedPathList[i])
-  //   if (n.path.length < ln.path.length && isEqual(n.path.slice(0, ln.path.length), ln.path)) {
-  //     closestNode = n
-  //   }
-  // }
-  // let closestNodeParent = getMapData(m, closestNode.parentPath) // FIXME getParentPath
-  // let closestNodeParentChildLen = closestNodeParent.sCount
-  // let closestNodeParentChildDelLen = 0
-  // for (let i = closestNode.path.at(-1); i > -1; i--) {
-  //   if (closestNodeParent.s[i].selected > 0) {
-  //     closestNodeParentChildDelLen++
-  //   }
-  // }
-  // // delete
-  // for (let i = sc.structSelectedPathList.length - 1; i > -1; i--) {
-  //   let n = getMapData(m, sc.structSelectedPathList[i])
-  //   let cmParent = getMapData(m, n.parentPath) // FIXME getParentPath
-  //   cmParent.taskStatus = n.taskStatus
-  //   cmParent.s.splice(n.path.at(-1), 1)
-  // }
-  // // reselect on jump back
-  // if (closestNodeParentChildLen === closestNodeParentChildDelLen) {
-  //   if (endsWithPathPattern(closestNode.path, 'ds')) {
-  //     m.r[0].selected = 1
-  //   } else {
-  //     closestNodeParent.selected = 1
-  //   }
-  // } else {
-  //   if (closestNode.path.at(-1) === 0) {
-  //     if (closestNodeParent.sCount > 0) {
-  //       closestNodeParent.s[0].selected = 1
-  //     } else {
-  //       if (endsWithPathPattern(closestNode.path, 'ds')) {
-  //         m.r[0].selected = 1
-  //       } else {
-  //         closestNodeParent.selected = 1
-  //       }
-  //     }
-  //   } else {
-  //     if (closestNode.path.at(-1) - closestNodeParentChildDelLen >= 0) {
-  //       closestNodeParent.s[closestNode.path.at(-1) - closestNodeParentChildDelLen].selected = 1
-  //     } else {
-  //       closestNodeParent.s[0].selected = 1
-  //     }
-  //   }
-  // }
-}
 
 export const cellDeleteReselect = (m: M) => {
   // const { lastPath, isCellRowSelected, cellRow, isCellColSelected, cellCol, sameParentPath } = sc
