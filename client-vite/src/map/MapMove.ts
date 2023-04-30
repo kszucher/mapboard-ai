aee487be9aaabe8fdd9db105f706baa82a39cb5b
Krisztian Szucher <krisztian@szucher.com>
2023-05-01 00:36:45 +0200

    rename

--- client-vite/src/map/MapMove.ts
+++ client-vite/src/map/MapMove.ts
@@ -29,7 +29,7 @@ export const deleteStuff = (m: M) => {
   }
 }
 
-export const move = (m: M, spacePath: P, insertPath: P) => {
+export const moveS = (m: M, spacePath: P, insertPath: P) => {
   const cb = m2cb(m)
   makeSpaceFrom(m, spacePath)
   deleteStuff(m)
@@ -37,17 +37,21 @@ export const move = (m: M, spacePath: P, insertPath: P) => {
   m.sort(sortPath)
 }
 
-export const moveSO = (m: M) => move(m, [], [...getXFSU1(m), 's', getCountXFSU1SO1(m)])
-export const moveSI = (m: M) => move(m, getXI1D1(m), getXI1D1(m))
-export const moveSIR = (m: M) => move(m, [], ['r', 0, 'd', 1, 's', getCountR0D1S(m)])
-export const moveSIL = (m: M) => move(m, [], ['r', 0, 'd', 0, 's', getCountR0D0S(m)])
-export const moveSD = (m: M) => move(m, getXLPSD2(m), getXFPSD1(m))
-export const moveST = (m: M) => move(m, [...getXI1(m), 's', 0], [...getXI1(m), 's', 0])
-export const moveSU = (m: M) => move(m, getXFPSU1(m), getXFPSU1(m))
-export const moveSB = (m: M) => move(m, [], [...getXI1(m), 's', getCountXLSD(m)])
+export const moveSO = (m: M) => moveS(m, [], [...getXFSU1(m), 's', getCountXFSU1SO1(m)])
+export const moveSI = (m: M) => moveS(m, getXI1D1(m), getXI1D1(m))
+export const moveSIR = (m: M) => moveS(m, [], ['r', 0, 'd', 1, 's', getCountR0D1S(m)])
+export const moveSIL = (m: M) => moveS(m, [], ['r', 0, 'd', 0, 's', getCountR0D0S(m)])
+export const moveSD = (m: M) => moveS(m, getXLPSD2(m), getXFPSD1(m))
+export const moveST = (m: M) => moveS(m, [...getXI1(m), 's', 0], [...getXI1(m), 's', 0])
+export const moveSU = (m: M) => moveS(m, getXFPSU1(m), getXFPSU1(m))
+export const moveSB = (m: M) => moveS(m, [], [...getXI1(m), 's', getCountXLSD(m)])
 
 export const moveCCR = (m: M) => {
-  // similar to move, but instead of makeSpaceFrom (aka makeSpaceAfterS), makeSpaceAfterC or makeSpaceBeforeC
+  // const cb = m2cb(m) // TODO use a version that start with 'c' and does NOT use XF as this is nonexistent here anyways
+  // TODO USE THE APPROPRIATE INCREMENTER
+  // deleteStuff(m)
+  // m.push(...cb2ip(cb, insertPath)) // TODO use an appropriate version
+  // m.sort(sortPath)
 }
 
 export const moveCCL = (m: M) => {
