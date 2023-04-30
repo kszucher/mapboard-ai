67f4a4151d369e29742e7f461aaac1091d840278
Krisztian Szucher <krisztian@szucher.com>
2023-04-30 23:29:22 +0200

    crazy levels of compression

--- client-vite/src/map/MapMove.ts
+++ client-vite/src/map/MapMove.ts
@@ -1,26 +1,7 @@
 import {M, P} from "../state/MapPropTypes"
-import {
-  decPiN,
-  getNodeByPath,
-  getIL,
-  isSD,
-  sortPath,
-  m2cb,
-  getXFSU1,
-  getCountXFSU1SO1,
-  cb2ip,
-  getXI2,
-  getCountXSI1SU,
-  getCountR0D1S,
-  getCountR0D0S,
-  getXI1,
-  getXFP,
-  makeSpaceFrom,
-  getXA,
-  getCountXLSD, getXFPSU1, getXFPSD1, getXLP, getXLPSD2,
-} from "./MapUtils"
+import {decPiN, getNodeByPath, getIL, isSD, sortPath, m2cb, getXFSU1, getCountXFSU1SO1, cb2ip, getXI2, getCountXSI1SU, getCountR0D1S, getCountR0D0S, getXI1, makeSpaceFrom, getCountXLSD, getXFPSU1, getXFPSD1, getXLPSD2,} from "./MapUtils"
 
-const deleteStuff = (m: M) => {
+export const deleteStuff = (m: M) => {
   for (let i = m.length - 1; i > 0; i--) {
     const n = m[i]
     const parentPathList = [...getIL(n.path), n.path]
@@ -29,9 +10,7 @@ const deleteStuff = (m: M) => {
   }
 }
 
-export const moveSO = (m: M) => {
-  const insertPath = [...getXFSU1(m), 's', getCountXFSU1SO1(m)] as P
-  const spacePath = [] as P
+export const move = (m: M, insertPath: P, spacePath: P) => {
   const cb = m2cb(m)
   makeSpaceFrom(m, spacePath)
   deleteStuff(m)
@@ -39,75 +18,14 @@ export const moveSO = (m: M) => {
   m.sort(sortPath)
 }
 
-export const moveSI = (m: M) => {
-  const insertPath = [...getXI2(m), 's', getCountXSI1SU(m) + 1] as P
-  const spacePath = [...getXI2(m), 's', getCountXSI1SU(m) + 1] as P
-  const cb = m2cb(m)
-  makeSpaceFrom(m, spacePath)
-  deleteStuff(m)
-  m.push(...cb2ip(cb, insertPath))
-  m.sort(sortPath)
-}
-
-export const moveSIR = (m: M) => {
-  const insertPath = ['r', 0, 'd', 1, 's', getCountR0D1S(m)] as P
-  const spacePath = [] as P
-  const cb = m2cb(m)
-  makeSpaceFrom(m, spacePath)
-  deleteStuff(m)
-  m.push(...cb2ip(cb, insertPath))
-  m.sort(sortPath)
-}
-
-export const moveSIL = (m: M) => {
-  const insertPath = ['r', 0, 'd', 0, 's', getCountR0D0S(m)] as P
-  const spacePath = [] as P
-  const cb = m2cb(m)
-  makeSpaceFrom(m, spacePath)
-  deleteStuff(m)
-  m.push(...cb2ip(cb, insertPath))
-  m.sort(sortPath)
-}
-
-export const moveSD = (m: M) => {
-  const insertPath = getXFPSD1(m)
-  const spacePath = getXLPSD2(m)
-  const cb = m2cb(m)
-  makeSpaceFrom(m, spacePath)
-  deleteStuff(m)
-  m.push(...cb2ip(cb, insertPath))
-  m.sort(sortPath)
-}
-
-export const moveST = (m: M) => {
-  const insertPath = [...getXI1(m), 's', 0] as P
-  const spacePath = [...getXI1(m), 's', 0] as P
-  const cb = m2cb(m)
-  makeSpaceFrom(m, spacePath)
-  deleteStuff(m)
-  m.push(...cb2ip(cb, insertPath))
-  m.sort(sortPath)
-}
-
-export const moveSU = (m: M) => {
-  const insertPath = getXFPSU1(m)
-  const spacePath = getXFPSU1(m)
-  const cb = m2cb(m)
-  makeSpaceFrom(m, spacePath)
-  deleteStuff(m)
-  m.push(...cb2ip(cb, insertPath))
-  m.sort(sortPath)
-}
-
-export const moveSB = (m: M) => {
-  const insertPath = [...getXI1(m), 's', getCountXLSD(m)] as P
-  const spacePath = [] as P
-  const cb = m2cb(m)
-  makeSpaceFrom(m, spacePath)
-  deleteStuff(m)
-  m.push(...cb2ip(cb, insertPath))
-  m.sort(sortPath)
-}
+export const moveSO = (m: M) => move(m, [...getXFSU1(m), 's', getCountXFSU1SO1(m)] as P, [] as P)
+export const moveSI = (m: M) => move(m, [...getXI2(m), 's', getCountXSI1SU(m) + 1] as P, [...getXI2(m), 's', getCountXSI1SU(m) + 1] as P)
+export const moveSIR = (m: M) => move(m, ['r', 0, 'd', 1, 's', getCountR0D1S(m)] as P, [] as P)
+export const moveSIL = (m: M) => move(m, ['r', 0, 'd', 0, 's', getCountR0D0S(m)] as P, [] as P)
+export const moveSD = (m: M) => move(m, getXFPSD1(m),  getXLPSD2(m))
+export const moveST = (m: M) => move(m, [...getXI1(m), 's', 0] as P, [...getXI1(m), 's', 0] as P)
+export const moveSU = (m: M) => move(m, getXFPSU1(m), getXFPSU1(m))
+export const moveSB = (m: M) => move(m, [...getXI1(m), 's', getCountXLSD(m)] as P, [] as P)
 
 export const moveCCI = (m: M) => {
 
