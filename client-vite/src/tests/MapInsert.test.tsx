7d80e67ecca9583692d86d1801228219be513c6e
Krisztian Szucher <krisztian@szucher.com>
2023-05-07 14:30:39 +0200

    increased coverage

--- client-vite/src/tests/MapInsert.test.tsx
+++ client-vite/src/tests/MapInsert.test.tsx
@@ -7,9 +7,10 @@ const insertSDTest = [
   {selected: 0, selection: 's', taskStatus: 0, nodeId: 'b', path: ['r', 0]},
   {selected: 0, selection: 's', taskStatus: 0, nodeId: 'c', path: ['r', 0, 'd', 0]},
   {selected: 0, selection: 's', taskStatus: 0, nodeId: 'd', path: ['r', 0, 'd', 0, 's', 0]},
-  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'e', path: ['r', 0, 'd', 0, 's', 0, 's', 0]},
-  {selected: 1, selection: 's', taskStatus: 0, nodeId: 'f', path: ['r', 0, 'd', 0, 's', 0, 's', 1]},
+  {selected: 1, selection: 's', taskStatus: 0, nodeId: 'e', path: ['r', 0, 'd', 0, 's', 0, 's', 0]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'f', path: ['r', 0, 'd', 0, 's', 0, 's', 1]},
   {selected: 0, selection: 's', taskStatus: 0, nodeId: 'g', path: ['r', 0, 'd', 0, 's', 0, 's', 2]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'h', path: ['r', 0, 'd', 0, 's', 0, 's', 3]},
 ] as M
 
 const insertSDResult = [
@@ -18,12 +19,38 @@ const insertSDResult = [
   {selected: 0, selection: 's', taskStatus: 0, nodeId: 'c', path: ['r', 0, 'd', 0]},
   {selected: 0, selection: 's', taskStatus: 0, nodeId: 'd', path: ['r', 0, 'd', 0, 's', 0]},
   {selected: 0, selection: 's', taskStatus: 0, nodeId: 'e', path: ['r', 0, 'd', 0, 's', 0, 's', 0]},
-  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'f', path: ['r', 0, 'd', 0, 's', 0, 's', 1]},
-  {selected: 1, selection: 's', taskStatus: 0, nodeId: 'x', path: ['r', 0, 'd', 0, 's', 0, 's', 2]},
+  {selected: 1, selection: 's', taskStatus: 0, nodeId: 'x', path: ['r', 0, 'd', 0, 's', 0, 's', 1]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'f', path: ['r', 0, 'd', 0, 's', 0, 's', 2]},
   {selected: 0, selection: 's', taskStatus: 0, nodeId: 'g', path: ['r', 0, 'd', 0, 's', 0, 's', 3]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'h', path: ['r', 0, 'd', 0, 's', 0, 's', 4]},
+] as M
+
+const insertSUTest = [
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'a', path: ['g']},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'b', path: ['r', 0]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'c', path: ['r', 0, 'd', 0]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'd', path: ['r', 0, 'd', 0, 's', 0]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'e', path: ['r', 0, 'd', 0, 's', 0, 's', 0]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'f', path: ['r', 0, 'd', 0, 's', 0, 's', 1]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'g', path: ['r', 0, 'd', 0, 's', 0, 's', 2]},
+  {selected: 1, selection: 's', taskStatus: 0, nodeId: 'h', path: ['r', 0, 'd', 0, 's', 0, 's', 3]},
+] as M
+
+const insertSUResult = [
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'a', path: ['g']},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'b', path: ['r', 0]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'c', path: ['r', 0, 'd', 0]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'd', path: ['r', 0, 'd', 0, 's', 0]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'e', path: ['r', 0, 'd', 0, 's', 0, 's', 0]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'f', path: ['r', 0, 'd', 0, 's', 0, 's', 1]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'g', path: ['r', 0, 'd', 0, 's', 0, 's', 2]},
+  {selected: 1, selection: 's', taskStatus: 0, nodeId: 'x', path: ['r', 0, 'd', 0, 's', 0, 's', 3]},
+  {selected: 0, selection: 's', taskStatus: 0, nodeId: 'h', path: ['r', 0, 'd', 0, 's', 0, 's', 4]},
 ] as M
 
 describe("InsertTests", () => {
   beforeEach(() => setIsTesting())
   test('insertSD', () => {mapReducerAtomic(insertSDTest, 'insertSD', {}); expect(insertSDTest).toEqual(insertSDResult)})
+  test('insertSU', () => {mapReducerAtomic(insertSUTest, 'insertSU', {}); expect(insertSUTest).toEqual(insertSUResult)})
+
 })
