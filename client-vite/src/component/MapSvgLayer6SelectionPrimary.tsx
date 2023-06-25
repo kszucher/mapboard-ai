ab434a8d062216b0d111808f68d426796ca394b0
Krisztian Szucher <krisztian@szucher.com>
2023-06-25 13:08:31 +0200

    added back where it was needed...

--- client-vite/src/component/MapSvgLayer6SelectionPrimary.tsx
+++ client-vite/src/component/MapSvgLayer6SelectionPrimary.tsx
@@ -2,7 +2,7 @@ import React, {FC} from "react"
 import {useSelector} from "react-redux"
 import { useOpenWorkspaceQuery} from "../core/Api"
 import {getColors} from "./Colors"
-import { getX, isXACC, isXACR, isXC} from "../core/MapUtils"
+import {getG, getX, isXACC, isXACR, isXC} from "../core/MapUtils"
 import {defaultUseOpenWorkspaceQueryState} from "../state/ApiState"
 import {mSelector} from "../state/EditorState"
 import {RootState} from "../core/EditorReducer"
@@ -11,6 +11,7 @@ import {getPolygonC, getPolygonPath, getPolygonS} from "./MapSvgUtils"
 
 export const MapSvgLayer6SelectionPrimary: FC = () => {
   const m = useSelector((state:RootState) => mSelector(state))
+  const g = getG(m)
   const x = getX(m)
   const selectionRectCoords = useSelector((state: RootState) => state.editor.selectionRectCoords)
   const { data } = useOpenWorkspaceQuery()
@@ -21,6 +22,7 @@ export const MapSvgLayer6SelectionPrimary: FC = () => {
       {
         !selectionRectCoords.length &&
         <path
+          key={`${g.nodeId}_svg_selectionBorderPrimary`}
           d={getPolygonPath(x, (isXC(m) || isXACR(m) || isXACC(m)) ? getPolygonC(m) : getPolygonS(m, x, x.selection), x.selection, getSelectionMargin(m, x))}
           stroke={C.SELECTION_COLOR}
           strokeWidth={1}
