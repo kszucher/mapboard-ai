f343e8e2d16dffce57e8fce0a3b16fad56bf3faf
Krisztian Szucher <krisztian@szucher.com>
2023-06-27 08:56:48 +0200

    insert doesnt change selection for real

--- client-vite/src/core/MapInsert.ts
+++ client-vite/src/core/MapInsert.ts
@@ -13,16 +13,14 @@ export const insertS = (m: M, ip: P, attributes: object) => {
 export const insertCR = (m: M, ip: P) => {
   const rowIndices = Array(getCountSCC(m, getSI1(ip))).fill(null).map((el, i) => [ip.at(-2), i])
   makeSpaceFromCR(m, ip)
-  // unselectNodes(m)
-  m.push(...rowIndices.map((el, i) => ({/*selected: 1, selection: 's', */nodeId: IS_TESTING ? generateCharacter(i) : 'node' + genHash(8), path: [...ip.slice(0, -3), 'c', ...el]}  as GN)))
+  m.push(...rowIndices.map((el, i) => ({selected: 0, selection: 's', nodeId: IS_TESTING ? generateCharacter(i) : 'node' + genHash(8), path: [...ip.slice(0, -3), 'c', ...el]}  as GN)))
   m.sort(sortPath)
 }
 
 export const insertCC = (m: M, ip: P) => {
   const colIndices = Array(getCountSCR(m, getSI1(ip))).fill(null).map((el, i) => [i, ip.at(-1)])
   makeSpaceFromCC(m, ip)
-  // unselectNodes(m)
-  m.push(...colIndices.map((el, i) => ({/*selected: 1, selection: 's', */nodeId: IS_TESTING ? generateCharacter(i) : 'node' + genHash(8), path: [...ip.slice(0, -3), 'c', ...el]}  as GN)))
+  m.push(...colIndices.map((el, i) => ({selected: 0, selection: 's', nodeId: IS_TESTING ? generateCharacter(i) : 'node' + genHash(8), path: [...ip.slice(0, -3), 'c', ...el]}  as GN)))
   m.sort(sortPath)
 }
 
@@ -31,6 +29,6 @@ export const insertTable = (m: M, ip: P, payload: {rowLen: number, colLen: numbe
   makeSpaceFromS(m, ip, 1)
   unselectNodes(m)
   m.push({selected: 1, selection: 's', nodeId: IS_TESTING ? 't' : 'node' + genHash(8), path: ip} as GN)
-  m.push(...tableIndices.map((el, i) => ({/*selected: 0, selection: 's', */nodeId: IS_TESTING ? generateCharacter(i) : 'node' + genHash(8), path: [...ip, 'c', ...el]}  as GN)))
+  m.push(...tableIndices.map((el, i) => ({selected: 0, selection: 's', nodeId: IS_TESTING ? generateCharacter(i) : 'node' + genHash(8), path: [...ip, 'c', ...el]}  as GN)))
   m.sort(sortPath)
 }
