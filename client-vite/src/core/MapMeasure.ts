348e3f8d75001ed7e2666e10c23d6e68468f0016
Krisztian Szucher <krisztian@szucher.com>
2023-06-28 12:10:42 +0200

    simplify

--- client-vite/src/core/MapMeasure.ts
+++ client-vite/src/core/MapMeasure.ts
@@ -28,12 +28,8 @@ const measureText = (g: G, pn: N, n: N) => {
       n.dimH = dim[1]
     }
   }
-  let paddingW = g.padding * 2
-  let paddingH = g.padding
-  let densityW = g.density === 'large' ? 0 : 12
-  let densityH = g.density === 'large' ? 0 : 2
-  n.selfW = (n.dimW > 20 ? n.dimW : 20) + paddingW + densityW
-  n.selfH = (n.dimH / 17 <= 1 ? g.defaultH  : n.dimH + paddingH ) + densityH
+  n.selfW = (n.dimW > 20 ? n.dimW : 20) +  g.padding * 2 + (g.density === 'large' ? 0 : 12)
+  n.selfH = (n.dimH / 17 <= 1 ? g.defaultH  : n.dimH + g.padding ) + (g.density === 'large' ? 0 : 2)
 }
 
 const measureTable = (m: M, g: G, n: N) => {
