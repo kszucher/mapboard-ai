964a102c98881e57848f6fbdf9c7d5c29fd0bc4a
Krisztian Szucher <krisztian@szucher.com>
2023-06-28 12:53:21 +0200

    restore dimensions as they are needed for clickability

--- client-vite/src/component/MapDiv.tsx
+++ client-vite/src/component/MapDiv.tsx
@@ -45,7 +45,8 @@ export const MapDiv: FC = () => {
               style={{
                 left: adjust(n.nodeStartX),
                 top: adjust( n.nodeY - n.selfH / 2),
-                minWidth: n.selfW,
+                minWidth: n.selfW + (g.density === 'large'? -10 : -8),
+                minHeight: n.selfH + (g.density === 'large'? -10 : 0),
                 paddingLeft: g.density === 'large'? 8 : 8,
                 paddingTop: g.density === 'large'? 4 : 2,
                 position: 'absolute',
