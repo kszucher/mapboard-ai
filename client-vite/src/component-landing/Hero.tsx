2bdf353166a7b56ff97bbd657b3dccf3d53de953
Krisztian Szucher <krisztian@szucher.com>
2023-06-29 21:38:00 +0200

    centered is better

--- client-vite/src/component-landing/Hero.tsx
+++ client-vite/src/component-landing/Hero.tsx
@@ -23,68 +23,58 @@ export const Hero: FC = () => {
               clipRule="evenodd"></path>
           </svg>
         </a>
-
-
         <div>
           <section className="bg-white dark:bg-gray-900">
-            <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-              <div className="mr-auto place-self-center lg:col-span-7">
-                <div className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-                  {/*<h1>Your</h1>*/}
+            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+              <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                <div className="">
+                  <h1 className="flex justify-center">Hello, MapBoard!</h1>
 
-                  <div className="">
-                    <h1 className="rotate-wrap leading-normal flex justify-center ">
-                      <span>&nbsp;</span>
-                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Meeting Notes</span>
-                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-teal-600 from-green-400">Team Documents</span>
-                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-orange-600 from-amber-400">Strategic Plans</span>
-                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-sky-600 from-indigo-400">Forecasts</span>
-                    </h1>
-                    <h1 className="flex justify-start">On a Visual Board</h1>
-                  </div>
+                  <h1 className="rotate-wrap leading-normal flex justify-center ">
+                    <span> &nbsp;</span>
+                    <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Summaries</span>
+                    <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-teal-600 from-green-400">Actions</span>
+                    <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-orange-600 from-amber-400">Plans</span>
+                    <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-sky-600 from-indigo-400">Forecasts</span>
+                  </h1>
+                  <h1 className="flex justify-center">Visually</h1>
                 </div>
-                <p
-                  className="umax-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-                  We at MapBoard craft hybrid AI-visual workspaces,
-                  <br/>
-                  to unlock new dimensions of team collaboration.
-                </p>
-
+              </h1>
+              <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+                We craft AI-powered visual workflows,
+                <br/>
+                to unlock new dimensions of team collaboration.
+              </p>
+              <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                <a href="#"
+                   className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                  Learn more
+                  <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                       xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"></path>
+                  </svg>
+                </a>
+                <a href="#"
+                   className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                  <svg className="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                       xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
+                  </svg>
+                  Watch video
+                </a>
               </div>
+
               <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
                 <img src="https://lh3.googleusercontent.com/drive-viewer/AFGJ81o4ierqrkzojD9lu5fvJWjgVaRqbfJfkJhjQyuZN8V7ZCbXCtSq2s3FstHw9s1n-LNOdK7Ct7LwGJcQUtlrc2Odiztp=w2559-h1315" alt="mockup" id="img"/>
               </div>
             </div>
           </section>
         </div>
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <img src="https://lh3.googleusercontent.com/drive-viewer/AFGJ81o4ierqrkzojD9lu5fvJWjgVaRqbfJfkJhjQyuZN8V7ZCbXCtSq2s3FstHw9s1n-LNOdK7Ct7LwGJcQUtlrc2Odiztp=w2559-h1315" alt="mockup" id="img"/>
-        </div>
-
         <br/>
-        <br/>
-        <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-          <a href="#"
-             className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-            Learn more
-            <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                 xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"></path>
-            </svg>
-          </a>
-          <a href="#"
-             className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-            <svg className="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                 xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
-            </svg>
-            Watch video
-          </a>
-        </div>
       </div>
     </section>
   )
