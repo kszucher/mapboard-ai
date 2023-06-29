f8ffa2ca5c2a7dc1fa5f2093ab2d5c3a422c5fe3
Krisztian Szucher <krisztian@szucher.com>
2023-06-29 17:53:24 +0200

    slowly getting to something usable

--- client-vite/src/component-landing/Hero.tsx
+++ client-vite/src/component-landing/Hero.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import React, {FC} from "react";
 import {useDispatch} from "react-redux";
 import {actions, AppDispatch} from "../core/EditorReducer";
 
@@ -23,30 +23,40 @@ export const Hero: FC = () => {
               clipRule="evenodd"></path>
           </svg>
         </a>
-        <div className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          {/*<h1>Your</h1>*/}
 
-          <div className="">
-            <h1 className="rotate-wrap flex justify-start ">
-              <div>Your &nbsp;</div>
-              <div className="">
-              <span>
-                <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Meeting Notes</span>
-                <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-teal-600 from-green-400">Team Documents</span>
-                <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-orange-600 from-amber-400">Strategic Plans</span>
-                <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-sky-600 from-indigo-400">Numerical Forecasts</span>
-              </span>
+
+        <div>
+          <section className="bg-white dark:bg-gray-900">
+            <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+              <div className="mr-auto place-self-center lg:col-span-7">
+                <div className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                  {/*<h1>Your</h1>*/}
+
+                  <div className="">
+                    <h1 className="rotate-wrap leading-normal flex justify-center ">
+                      <span>&nbsp;</span>
+                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Meeting Notes</span>
+                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-teal-600 from-green-400">Team Documents</span>
+                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-orange-600 from-amber-400">Strategic Plans</span>
+                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-sky-600 from-indigo-400">Forecasts</span>
+                    </h1>
+                    <h1 className="flex justify-start">On a Visual Board</h1>
+                  </div>
+                </div>
+                <p
+                  className="umax-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                  We at MapBoard craft AI-visual hybrid workspaces,
+                  <br/>
+                  to unlock new dimensions of team collaboration.
+                </p>
+
               </div>
-            </h1>
-            <h1 className="flex justify-start">on a visual board</h1>
-          </div>
-
+              <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" id="img"/>
+              </div>
+            </div>
+          </section>
         </div>
-        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-          Here at MapBoard we focus on crafting hybrid AI-visual workspaces,
-          <br/>
-          to unlock new dimensions of AI-powered team collaboration.
-        </p>
         <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a href="#"
              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
