6247a688f78a5b171d6a91fc142a7b807df9cda1
Krisztian Szucher <krisztian@szucher.com>
2023-06-29 12:35:46 +0200

    hero section added

--- client-vite/src/component/Landing.tsx
+++ client-vite/src/component/Landing.tsx
@@ -1,5 +1,6 @@
 import {useDispatch, useSelector} from "react-redux"
 import {Button, CircularProgress, Link, ThemeProvider, Typography} from '@mui/material'
+import {Hero} from "../component-landing/Hero";
 import {actions, AppDispatch, RootState} from "../core/EditorReducer"
 import React, {FC, useEffect, useState} from "react"
 import { PageState} from "../state/Enums"
@@ -45,81 +46,101 @@ export const Landing: FC = () => {
   }, [token])
 
   return (
-    <ThemeProvider theme={getMuiTheme(colorMode)}>
+    <>
 
 
+      <Hero/>
 
 
       <div className="mbg">
 
+
+        <svg>
+          <filter id='noiseFilter'>
+            <feTurbulence
+              type='fractalNoise'
+              baseFrequency='6.29'
+              numOctaves='6'
+              stitchTiles='stitch'/>
+          </filter>
+        </svg>
+
+
       </div>
-      <svg>
-        <filter id='noiseFilter'>
-          <feTurbulence
-            type='fractalNoise'
-            baseFrequency='6.29'
-            numOctaves='6'
-            stitchTiles='stitch'/>
-        </filter>
-      </svg>
+    </>
 
 
-
-      <div className=" relative left-1/2 top-1/8 -translate-x-1/2  flex flex-col items-center inline-flex">
-        <div style={{fontFamily: 'Okta Neue', fontStyle: 'oblique', fontSize: 100, color: '#ffffff'}}>
-          <center>
-            <b>AI Visual Workflow</b>
-          </center>
-        </div>
-        <div style={{fontFamily: 'Okta Neue', fontSize: 40, color: '#ffffff'}}>
-          <center>
-            Transcribe <b>anything</b>,
-            <br/>
-            Think <b>visually</b>,
-            <br/>
-            Mark <b>done</b>.
-
-          </center>
-
-
-        </div>
-      </div>
-
-
-      <div className="_bg relative bottom-1/2 left-1/2 -translate-x-1/2 top-[40px] w-[640px] h-[160px] flex flex-col items-center inline-flex gap-4 p-5 rounded-lg">
-        <Typography color="primary" component="h1" variant="h5">
-          {'mapboard'}
-        </Typography>
-        <Typography color="primary" component="h1" variant="h6">
-          {'Private Beta'}
-        </Typography>
-        {
-          isWaiting
-            ?
-            <CircularProgress />
-            :
-            <Button
-              id="sign-in" color="primary" variant='contained' fullWidth
-              disabled={false}
-              onClick={() => loginWithRedirect()}>
-              {'SIGN IN / SIGN UP'}
-            </Button>
-        }
-        {/*<Button*/}
-        {/*  id="live-demo" color="primary" variant='contained' fullWidth*/}
-        {/*  onClick={()=> {dispatch(actions.setPageState(PageState.DEMO))}}>*/}
-        {/*  {'LIVE DEMO'}*/}
-        {/*</Button>*/}
-        <Typography variant="body2" color="textSecondary" align="center">
-          {'Copyright © '}
-          <Link color="inherit" href="https://mapboard.io/">
-            MapBoard
-          </Link>
-          {' '}
-          {new Date().getFullYear()}
-          {'.'}
-        </Typography>
-      </div>
-    </ThemeProvider>
   )
 }
+
+// {/*// <ThemeProvider theme={getMuiTheme(colorMode)}>*/}
+//
+//
+//
+//
+// {/*<div className="mbg">*/}
+//
+// {/*</div>*/}
+// {/*<svg>*/}
+// {/*  <filter id='noiseFilter'>*/}
+// {/*    <feTurbulence*/}
+// {/*      type='fractalNoise'*/}
+// {/*      baseFrequency='6.29'*/}
+// {/*      numOctaves='6'*/}
+// {/*      stitchTiles='stitch'/>*/}
+// {/*  </filter>*/}
+// {/*</svg>*/}
+//
+//
+//
+// {/*<div className=" relative left-1/2 top-1/8 -translate-x-1/2  flex flex-col items-center inline-flex">*/}
+// {/*  <div style={{fontFamily: 'Okta Neue', fontStyle: 'oblique', fontSize: 100, color: '#ffffff'}}>*/}
+// {/*    <center>*/}
+// {/*      <b>AI Visual Workflow</b>*/}
+// {/*    </center>*/}
+// {/*  </div>*/}
+// {/*  <div style={{fontFamily: 'Okta Neue', fontSize: 40, color: '#ffffff'}}>*/}
+// {/*    <center>*/}
+// {/*      Transcribe <b>anything</b>,*/}
+// {/*      <br/>*/}
+// {/*      Think <b>visually</b>,*/}
+// {/*      <br/>*/}
+// {/*      Mark <b>done</b>.*/}
+//
+// {/*    </center>*/}
+//
+//
+// {/*  </div>*/}
+// {/*</div>*/}
+//
+//
+// {/*<div className="_bg relative bottom-1/2 left-1/2 -translate-x-1/2 top-[40px] w-[640px] h-[160px] flex flex-col items-center inline-flex gap-4 p-5 rounded-lg">*/}
+// {/*  <Typography color="primary" component="h1" variant="h5">*/}
+// {/*    {'mapboard'}*/}
+// {/*  </Typography>*/}
+// {/*  <Typography color="primary" component="h1" variant="h6">*/}
+// {/*    {'Private Beta'}*/}
+// {/*  </Typography>*/}
+// {/*  {*/}
+// {/*    isWaiting*/}
+// {/*      ?*/}
+// {/*      <CircularProgress />*/}
+// {/*      :*/}
+// {/*      <Button*/}
+// {/*        id="sign-in" color="primary" variant='contained' fullWidth*/}
+// {/*        disabled={false}*/}
+// {/*        onClick={() => loginWithRedirect()}>*/}
+// {/*        {'SIGN IN / SIGN UP'}*/}
+// {/*      </Button>*/}
+// {/*  }*/}
+// {/*  <Typography variant="body2" color="textSecondary" align="center">*/}
+// {/*    {'Copyright © '}*/}
+// {/*    <Link color="inherit" href="https://mapboard.io/">*/}
+// {/*      MapBoard*/}
+// {/*    </Link>*/}
+// {/*    {' '}*/}
+// {/*    {new Date().getFullYear()}*/}
+// {/*    {'.'}*/}
+// {/*  </Typography>*/}
+// {/*</div>*/}
+// {/*// </ThemeProvider>*/}
--- client-vite/tailwind.config.js
+++ client-vite/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,8 +10,57 @@ module.exports = {
     extend: {
       colors: {
         'mb-pink': '#9040b8',
-        'mb-purple': '#5f0a87'
+        'mb-purple': '#5f0a87',
+        primary: {
+          '50': '#f5f3ff',
+          '100': '#ede9fe',
+          '200': '#ddd6fe',
+          '300': '#c4b5fd',
+          '400': '#a78bfa',
+          '500': '#8b5cf6',
+          '600': '#7c3aed',
+          '700': '#6d28d9',
+          '800': '#5b21b6',
+          '900': '#4c1d95',
+          '950': '#2e1065'
+        }
       },
+    },
+    fontFamily: {
+      'body': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      'sans': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ]
     }
   },
   plugins: [
@@ -18,5 +68,7 @@ module.exports = {
   ],
   corePlugins: {
     preflight: false, // https://tailwindcss.com/docs/preflight
-  }
+  },
 }
+
+// simply merge... easy as dat
