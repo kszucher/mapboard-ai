c8acd40b582020cc48ca4ef06d0056ae83ac2ab8
Krisztian Szucher <krisztian@szucher.com>
2023-08-18 18:04:23 +0200

    simpler classname

--- client-vite/src/component/MapSvgLayer9DecorationIcons.tsx
+++ client-vite/src/component/MapSvgLayer9DecorationIcons.tsx
@@ -26,18 +26,7 @@ export const MapSvgLayer9DecorationIcons: FC = () => {
     >
       <rect width="24" height="24" rx={4} ry={4} fill={'#666666'}/>
       {
-        <g
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          stroke="#ffffff"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="icon icon-tabler icon-tabler-square-rounded-plus"
-          viewBox="0 0 24 24"
-        >
+        <g xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
           {r0.note === '' &&
             <g>
               <path stroke="none" d="M0 0h24v24H0z"></path>
