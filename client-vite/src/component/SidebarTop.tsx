b8baed00f04ed00e64285ff7c40bb2631809ba04
Krisztian Szucher <krisztian@szucher.com>
2023-08-21 20:20:40 +0200

    direct modal access from top right button bar

--- client-vite/src/component/SidebarTop.tsx
+++ client-vite/src/component/SidebarTop.tsx
@@ -1,17 +1,18 @@
-import DynamicFeedIcon from "@mui/icons-material/DynamicFeed";
-import Tooltip from "@mui/material/Tooltip";
+import Tooltip from "@mui/material/Tooltip"
 import {FC} from "react"
 import { useDispatch } from 'react-redux'
 import { IconButton } from '@mui/material'
 import PersonIcon from '@mui/icons-material/Person'
+import SettingsIcon from '@mui/icons-material/Settings'
 import {actions, AppDispatch} from "../core/EditorReducer"
+import {PageState} from "../state/Enums"
 
 export const SidebarTop: FC = () => {
   const dispatch = useDispatch<AppDispatch>()
   return (
     <div className="_bg fixed top-0 right-0 w-[96px] h-[40px] flex flex-row flex-center border-t-0 border-r-0 py-1 px-2 rounded-bl-lg z-50">
-      <Tooltip title="Frames" placement="top-end"><IconButton color='secondary' onClick={({currentTarget}) => {dispatch(actions.openFrameMenu(Boolean(currentTarget)))}}><DynamicFeedIcon/></IconButton></Tooltip>
-      <Tooltip title="Profile" placement="top-end"><IconButton color='secondary' onClick={({currentTarget}) => {dispatch(actions.openMoreMenu(Boolean(currentTarget)))}}><PersonIcon/></IconButton></Tooltip>
+      <Tooltip title="Settings" placement="top-end"><IconButton color='secondary' onClick={({currentTarget}) => {dispatch(actions.setPageState(PageState.WS_SETTINGS))}}><SettingsIcon/></IconButton></Tooltip>
+      <Tooltip title="Profile" placement="top-end"><IconButton color='secondary' onClick={({currentTarget}) => {dispatch(actions.setPageState(PageState.WS_PROFILE))}}><PersonIcon/></IconButton></Tooltip>
     </div>
   )
 }
