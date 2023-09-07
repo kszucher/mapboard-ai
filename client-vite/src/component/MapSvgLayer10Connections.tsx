eec8dcf2608a8d778a884d99c527cc856a4e20f7
Krisztian Szucher <krisztian@szucher.com>
2023-09-07 18:58:51 +0200

    moved under helpers

--- client-vite/src/component/MapSvgLayer10Connections.tsx
+++ client-vite/src/component/MapSvgLayer10Connections.tsx
@@ -61,23 +61,25 @@ export const MapSvgLayer10Connections: FC = () => {
             fill={'none'}
             {...pathCommonProps}
           />
-          <g
-            width="24" height="24" viewBox="0 0 24 24"
-            transform={`translate(
-            ${adjustIcon(calculateMiddlePoint(getLinePathBetweenRoots(m, connection)).x) - 12},
-            ${adjustIcon(calculateMiddlePoint(getLinePathBetweenRoots(m, connection)).y) - 12})`}
-            {...{vectorEffect: 'non-scaling-stroke'}}
-            style={{transition: 'all 0.3s', transitionTimingFunction: 'cubic-bezier(0.0,0.0,0.58,1.0)', transitionProperty: 'all'}}>
-            <rect width="24" height="24" rx={4} ry={4} fill={'#666666'}/>
-            <g xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
-              <path d="M4 7h16M10 11v6M14 11v6M5 7l1 12a2 2 0 002 2h8a2 2 0 002-2l1-12M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3"></path>
-            </g>
-            <rect width="24" height="24" style={{opacity: 0}} onMouseDown={(e) => {
-              e.preventDefault()
-              e.stopPropagation()
+          {connectionHelpersVisible &&
+            <g
+              width="24" height="24" viewBox="0 0 24 24"
+              transform={`translate(
+              ${adjustIcon(calculateMiddlePoint(getLinePathBetweenRoots(m, connection)).x) - 12},
+              ${adjustIcon(calculateMiddlePoint(getLinePathBetweenRoots(m, connection)).y) - 12})`}
+              {...{vectorEffect: 'non-scaling-stroke'}}
+              style={{transition: 'all 0.3s', transitionTimingFunction: 'cubic-bezier(0.0,0.0,0.58,1.0)', transitionProperty: 'all'}}>
+              <rect width="24" height="24" rx={4} ry={4} fill={'#666666'}/>
+              <g xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
+                <path d="M4 7h16M10 11v6M14 11v6M5 7l1 12a2 2 0 002 2h8a2 2 0 002-2l1-12M9 7V4a1 1 0 011-1h4a1 1 0 011 1v3"></path>
+              </g>
+              <rect width="24" height="24" style={{opacity: 0}} onMouseDown={(e) => {
+                e.preventDefault()
+                e.stopPropagation()
+                // TODO delete connection
 
-            }}/>
-          </g>
+              }}/>
+            </g>}
         </g>
       ))}
     </g>
