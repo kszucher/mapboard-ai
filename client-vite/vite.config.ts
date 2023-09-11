31fdeebb6a8935a035c3b56a64e155ec915bb079
Krisztian Szucher <krisztian@szucher.com>
2023-09-11 16:38:37 +0200

    dependency update

--- client-vite/vite.config.ts
+++ client-vite/vite.config.ts
@@ -1,16 +1,24 @@
 import { defineConfig } from 'vite'
 import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
+import AutoImport from 'unplugin-auto-import/vite'
 
 import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
 import viteReact from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteReact(), viteBasicSslPlugin()],
+  plugins: [
+    viteReact(), viteBasicSslPlugin(),
+    AutoImport({
+      imports: ['vitest'],
+      dts: true, // generate TypeScript declaration
+    })
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
   },
+
   server: {
     host: 'local.mapboard',
     https: true,
