6fc067dd08f4916c823d28a8dd8a2ac7eb9317c2
Krisztian Szucher <krisztian@szucher.com>
2023-09-12 23:09:31 +0200

    build fix

--- client-vite/src/components/Formatter.test.tsx
+++ client-vite/src/components/Formatter.test.tsx
@@ -4,7 +4,6 @@ import {Provider} from "react-redux"
 import {Formatter} from "./Formatter"
 import { describe, it } from 'vitest'
 import {actions, store} from "../reducers/EditorReducer"
-import {colorList} from "./Colors"
 import {FormatMode, LineTypes, AccessTypes, TextTypes, WidthTypes} from "../state/Enums"
 import {getMap} from "../state/EditorState"
 import {nSaveOptional} from "../state/MapState"
