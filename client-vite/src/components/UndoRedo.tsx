635bc355b4d73864ecb8d11ee006fc85e798a8ff
Krisztian Szucher <krisztian@szucher.com>
2023-09-13 11:20:17 +0200

    composability

--- client-vite/src/components/UndoRedo.tsx
+++ client-vite/src/components/UndoRedo.tsx
@@ -1,15 +1,50 @@
-import React, {FC} from "react"
+import React, {FC, ReactNode} from "react"
 import {useDispatch, useSelector} from 'react-redux'
-import {mSelector} from "../state/EditorState"
 import {AccessTypes} from "../state/Enums"
 import {actions, AppDispatch, RootState} from "../reducers/EditorReducer"
 import {useOpenWorkspaceQuery} from "../apis/NodeApi"
 import {defaultUseOpenWorkspaceQueryState} from "../state/NodeApiState"
 
-const classNameButton = "text-white focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2 text-center inline-flex items-center dark:hover:bg-gray-700 "
+const UndoIcon = () => (
+  <g>
+    <path stroke="none" d="M0 0h24v24H0z"></path>
+    <path d="M9 14l-4-4 4-4"></path>
+    <path d="M5 10h11a4 4 0 110 8h-1"></path>
+  </g>
+)
+
+const RedoIcon = () => (
+  <g>
+    <path stroke="none" d="M0 0h24v24H0z"></path>
+    <path d="M15 14l4-4-4-4"></path>
+    <path d="M19 10H8a4 4 0 100 8h1"></path>
+  </g>
+)
+
+export const IconButton = ({colorMode, disabled, onClick, children} : {colorMode: string, disabled: boolean, onClick: Function, children: ReactNode}) => (
+  <button
+    type="button"
+    className="text-white focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2 text-center inline-flex items-center dark:hover:bg-gray-700"
+    disabled={disabled}
+    onClick={() => onClick()}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke={colorMode === 'dark' ? "#ffffff" : '#000000'}
+      opacity={disabled ? '25%' : '100%'}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24">
+      ({children})
+    </svg>
+    <span className="sr-only">Icon description</span>
+  </button>
+)
 
 export const UndoRedo: FC = () => {
-  const m = useSelector((state:RootState) => mSelector(state))
   const mapList = useSelector((state: RootState) => state.editor.mapList)
   const mapListIndex = useSelector((state: RootState) => state.editor.mapListIndex)
   const { data } = useOpenWorkspaceQuery()
@@ -20,22 +55,8 @@ export const UndoRedo: FC = () => {
   const dispatch = useDispatch<AppDispatch>()
   return (
     <div className="dark:bg-zinc-800 bg-zinc-50 border-2 dark:border-neutral-700 fixed top-0 left-[272px] w-[96px] flex justify-around h-[40px] py-1 border-t-0 rounded-b-lg z-50">
-      <button type="button" className={classNameButton} disabled={undoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'undo', payload: null}))}}>
-        <svg xmlns="http://www.w3.org/2000/svg" className="" width="24" height="24" fill="none" stroke={colorMode === 'dark'? "#ffffff" : '#000000'} opacity={undoDisabled ? '25%' : '100%'} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
-          <path stroke="none" d="M0 0h24v24H0z"></path>
-          <path d="M9 14l-4-4 4-4"></path>
-          <path d="M5 10h11a4 4 0 110 8h-1"></path>
-        </svg>
-        <span className="sr-only">Icon description</span>
-      </button>
-      <button type="button" className={classNameButton} disabled={redoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'redo', payload: null}))}}>
-        <svg xmlns="http://www.w3.org/2000/svg" className="" width="24" height="24" fill="none" stroke={colorMode === 'dark'? "#ffffff" : '#000000'} opacity={redoDisabled ? '25%' : '100%'} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
-          <path stroke="none" d="M0 0h24v24H0z"></path>
-          <path d="M15 14l4-4-4-4"></path>
-          <path d="M19 10H8a4 4 0 100 8h1"></path>
-        </svg>
-        <span className="sr-only">Icon description</span>
-      </button>
+      <IconButton colorMode={colorMode} disabled={undoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'undo', payload: null}))}}><UndoIcon/></IconButton>
+      <IconButton colorMode={colorMode} disabled={redoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'redo', payload: null}))}}><RedoIcon/></IconButton>
     </div>
   )
 }
