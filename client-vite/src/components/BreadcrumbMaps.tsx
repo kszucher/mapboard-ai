cfac11df2a5afd13929d6d22e7a61204affba94c
Krisztian Szucher <krisztian@szucher.com>
2023-09-13 13:29:19 +0200

    flowbite bread added in comments

--- client-vite/src/components/BreadcrumbMaps.tsx
+++ client-vite/src/components/BreadcrumbMaps.tsx
@@ -31,6 +31,34 @@ export const BreadcrumbMaps: FC = () => {
           ))
         }
       </Breadcrumbs>
+      {/*<nav className="flex" aria-label="Breadcrumb">*/}
+      {/*  <ol className="inline-flex items-center space-x-1 md:space-x-3">*/}
+      {/*    <li className="inline-flex items-center">*/}
+      {/*      <a href="#" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">*/}
+      {/*        Home*/}
+      {/*      </a>*/}
+      {/*    </li>*/}
+      {/*    <li>*/}
+      {/*      <div className="flex items-center">*/}
+      {/*        <svg className="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">*/}
+      {/*          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/></svg>*/}
+      {/*        <a href="#" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">*/}
+      {/*          Projects*/}
+      {/*        </a>*/}
+      {/*      </div>*/}
+      {/*    </li>*/}
+      {/*    <li aria-current="page">*/}
+      {/*      <div className="flex items-center">*/}
+      {/*        <svg className="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">*/}
+      {/*          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>*/}
+      {/*        </svg>*/}
+      {/*        <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">*/}
+      {/*          Flowbite*/}
+      {/*        </span>*/}
+      {/*      </div>*/}
+      {/*    </li>*/}
+      {/*  </ol>*/}
+      {/*</nav>*/}
     </div>
   )
 }
