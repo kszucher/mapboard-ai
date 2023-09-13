97be359c485b8b59c3836ae0840c8687a6c8bda9
Krisztian Szucher <krisztian@szucher.com>
2023-09-13 10:16:58 +0200

    dark mode ok

--- client-vite/src/components/UndoRedo.tsx
+++ client-vite/src/components/UndoRedo.tsx
@@ -13,7 +13,7 @@ export const UndoRedo: FC = () => {
   const mapList = useSelector((state: RootState) => state.editor.mapList)
   const mapListIndex = useSelector((state: RootState) => state.editor.mapListIndex)
   const { data } = useOpenWorkspaceQuery()
-  const { access } = data || defaultUseOpenWorkspaceQueryState
+  const { colorMode, access } = data || defaultUseOpenWorkspaceQueryState
   const disabled = [AccessTypes.VIEW, AccessTypes.UNAUTHORIZED].includes(access)
   const undoDisabled = disabled || mapListIndex === 0
   const redoDisabled = disabled || mapListIndex === mapList.length - 1
@@ -21,7 +21,7 @@ export const UndoRedo: FC = () => {
   return (
     <div className="dark:bg-zinc-800 bg-zinc-50 border-2 dark:border-neutral-700 fixed top-0 left-[272px] w-[96px] flex justify-around h-[40px] py-1 border-t-0 rounded-b-lg z-50">
       <button type="button" className={classNameButton} disabled={undoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'undo', payload: null}))}}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="#ffffff" opacity={undoDisabled ? '25%' : '100%'} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
+        <svg xmlns="http://www.w3.org/2000/svg" className="" width="24" height="24" fill="none" stroke={colorMode === 'dark'? "#ffffff" : '#000000'} opacity={undoDisabled ? '25%' : '100%'} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
           <path stroke="none" d="M0 0h24v24H0z"></path>
           <path d="M9 14l-4-4 4-4"></path>
           <path d="M5 10h11a4 4 0 110 8h-1"></path>
@@ -29,7 +29,7 @@ export const UndoRedo: FC = () => {
         <span className="sr-only">Icon description</span>
       </button>
       <button type="button" className={classNameButton} disabled={redoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'redo', payload: null}))}}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="#ffffff" opacity={redoDisabled ? '25%' : '100%'} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
+        <svg xmlns="http://www.w3.org/2000/svg" className="" width="24" height="24" fill="none" stroke={colorMode === 'dark'? "#ffffff" : '#000000'} opacity={redoDisabled ? '25%' : '100%'} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
           <path stroke="none" d="M0 0h24v24H0z"></path>
           <path d="M15 14l4-4-4-4"></path>
           <path d="M19 10H8a4 4 0 100 8h1"></path>
