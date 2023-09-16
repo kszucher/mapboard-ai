a46993c0e8f30004319c2965cc3d0a3d2b796682
Krisztian Szucher <krisztian@szucher.com>
2023-09-16 14:55:37 +0200

    fix share load bug

--- client-vite/src/reducers/EditorReducer.ts
+++ client-vite/src/reducers/EditorReducer.ts
@@ -132,6 +132,10 @@ export const editorSlice = createSlice({
       (state) => {state.pageState = PageState.WS}
     )
     builder.addMatcher(
+      nodeApi.endpoints.getShares.matchFulfilled,
+      (state) => {state.pageState = PageState.WS}
+    )
+    builder.addMatcher(
       nodeApi.endpoints.openWorkspace.matchFulfilled,
       (state, { payload }) => {
         const { mapDataList } = payload
