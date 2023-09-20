1a73071ffecd18336a3324177d44410e23f43251
Krisztian Szucher <krisztian@szucher.com>
2023-09-20 11:30:23 +0200

    revert to using named functions in insert as well

--- client-vite/src/reducers/MapInsert.ts
+++ client-vite/src/reducers/MapInsert.ts
@@ -1,8 +1,9 @@
 import {GN, M, N, P} from "../state/MapStateTypes"
 import {getInsertTemplate} from "./MapInsertTemplates"
 import {unselectNodes} from "./MapSelect"
-import {getCountNSCV, getCountNSCH, getX, sortPath, isCED, isCER, isSEODO} from "../selectors/MapSelector"
+import {getCountNSCV, getCountNSCH, getX, sortPath, isSEODO} from "../selectors/MapSelector"
 import {generateCharacter, genHash, getTableIndices, IS_TESTING} from "../utils/Utils"
+import {makeSpaceFromCc, makeSpaceFromCr, makeSpaceFromS} from "./MapSpace"
 
 export const insertTemplateR = (m: M, templateId: string, ri: number, offsetW: number, offsetH: number) => {
   unselectNodes(m)
@@ -17,7 +18,7 @@ export const insertR = (m: M) => {
 
 export const insertS = (m: M, insertParentNode: N, insertTargetIndex: number, attributes: object) => {
   const ip = [...insertParentNode.path, 's', insertTargetIndex] as P
-  m.forEach(n => isSEODO(ip, n.path) && n.path.splice(ip.length - 1, 1, n.path.at(ip.length - 1) as number + 1))
+  makeSpaceFromS(m, ip, 1)
   unselectNodes(m)
   m.push({selected: 1, selection: 's', nodeId: IS_TESTING ? 't' : 'node' + genHash(8), path: ip, taskStatus: getX(m).taskStatus, ...attributes} as GN)
   m.sort(sortPath)
@@ -25,14 +26,14 @@ export const insertS = (m: M, insertParentNode: N, insertTargetIndex: number, at
 
 export const insertCR = (m: M, insertParentNode: N, insertTargetRowIndex: number) => {
   const ipList = Array(getCountNSCH(m, insertParentNode)).fill(null).map((el, i) => [...insertParentNode.path, 'c', insertTargetRowIndex, i] as P)
-  m.forEach(n => ipList.map(ip => isCED(ip, n.path) && n.path.splice(ip.length - 2, 1, n.path.at(ip.length - 2) as number + 1)))
+  makeSpaceFromCr(m, ipList, 1)
   m.push(...ipList.map((p, i) => ({selected: 0, selection: 's', nodeId: IS_TESTING ? generateCharacter(i) : 'node' + genHash(8), path: p}  as GN)))
   m.sort(sortPath)
 }
 
 export const insertCC = (m: M, insertParentNode: N, insertTargetColumnIndex: number) => {
   const ipList = Array(getCountNSCV(m, insertParentNode)).fill(null).map((el, i) => [...insertParentNode.path, 'c', i, insertTargetColumnIndex] as P)
-  m.forEach(n => ipList.map(ip => isCER(ip, n.path) && n.path.splice(ip.length - 1, 1, n.path.at(ip.length - 1) as number + 1)))
+  makeSpaceFromCc(m, ipList, 1)
   m.push(...ipList.map((p, i) => ({selected: 0, selection: 's', nodeId: IS_TESTING ? generateCharacter(i) : 'node' + genHash(8), path: p} as GN)))
   m.sort(sortPath)
 }
