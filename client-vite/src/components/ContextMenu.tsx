141a5080c416b109e43df6849cd8de1e5ea0200c
Krisztian Szucher <krisztian@szucher.com>
2023-09-28 18:06:30 +0200

    simplify

--- client-vite/src/components/ContextMenu.tsx
+++ client-vite/src/components/ContextMenu.tsx
@@ -9,7 +9,8 @@ import {ControlTypes, PageState} from "../state/Enums"
 import {getMapId} from "../state/NodeApiState"
 
 const Li2 = ({subMenuId, onClick}: {subMenuId: string, onClick: Function}) => {
-  return (<li>
+  return (
+    <li>
       <a className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={() => onClick()}>
         {subMenuId}
       </a>
@@ -28,11 +29,9 @@ const Li1 = ({menuId, children}: {menuId: string, children: ReactNode}) => {
         className="flex items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
       >
         {menuId}
-        {
-          <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-          </svg>
-        }
+        <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
+        </svg>
       </button>
       <div id={'subMenu' + menuId} className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
         <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="doubleDropdownButton">
