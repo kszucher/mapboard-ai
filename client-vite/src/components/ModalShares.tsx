379f532c35aeb372c38bab487e21f502186eb342
Krisztian Szucher <krisztian@szucher.com>
2023-11-05 15:51:36 +0100

    table placeholder

--- client-vite/src/components/ModalShares.tsx
+++ client-vite/src/components/ModalShares.tsx
@@ -1,99 +1,92 @@
-import {FC} from 'react'
+import React, {FC} from 'react'
 import {useDispatch} from "react-redux"
-import {DataGrid} from "@mui/x-data-grid"
-import { Button, IconButton, Modal, Typography } from '@mui/material'
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
-import CheckCircleIcon from '@mui/icons-material/AddCircleOutline'
-import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined'
 import {actions, AppDispatch} from "../reducers/EditorReducer"
 import {PageState} from "../state/Enums"
 import {nodeApi, useGetSharesQuery} from "../apis/NodeApi"
+import {Modal, Table,} from 'flowbite-react'
 
 export const ModalShares: FC = () => {
-  const { data, isFetching } = useGetSharesQuery()
+  const { data } = useGetSharesQuery()
   let { shareDataExport, shareDataImport } = data || { shareDataExport: [], shareDataImport: []}
-  shareDataExport = shareDataExport.map((el: any) => ({...el, id: el._id}))
-  shareDataImport = shareDataImport.map((el: any) => ({...el, id: el._id}))
   const dispatch = useDispatch<AppDispatch>()
-  const columnsExport = [
-    {field: 'sharedMapName',  headerName: 'Map Name',    width: 200, sortable: false, editable: false},
-    {field: 'shareUserEmail', headerName: 'Shared With', width: 250, sortable: false, editable: false},
-    {field: 'access',         headerName: 'Access',      width: 140, sortable: false, editable: false},
-    {field: 'status',         headerName: 'Status',      width: 140, sortable: false, editable: false},
-    {field: ' ',              headerName: 'Actions',     width: 200, renderCell: (params: any) => (
-        <strong>
-          <IconButton
-            aria-label="xxx"
-            size="small"
-            onClick={()=>dispatch(nodeApi.endpoints.deleteMap.initiate({mapId: params.row._id}))}
-            disabled={false}
-          >
-            <CancelOutlinedIcon/>
-          </IconButton>
-        </strong>
-      )
-    }
-  ]
-  const columnsImport = [
-    {field: 'sharedMapName',  headerName: 'Map Name',    width: 200, sortable: false, editable: false},
-    {field: 'ownerUserEmail', headerName: 'Shared By',   width: 250, sortable: false, editable: false},
-    {field: 'access',         headerName: 'Access',      width: 140, sortable: false, editable: false},
-    {field: 'status',         headerName: 'Status',      width: 140, sortable: false, editable: false},
-    {field: ' ',              headerName: 'Actions',     width: 200, renderCell: (params: any) => (
-        <strong>
-          <IconButton
-            aria-label="xxx"
-            size="small"
-            onClick={()=>dispatch(nodeApi.endpoints.acceptShare.initiate({shareId: params.row._id}))}
-            disabled={params.row.status === 'accepted'}
-          >
-            {params.row.status === 'waiting' && <AddCircleOutlineIcon/>}
-            {params.row.status === 'accepted' && <CheckCircleIcon/>}
-          </IconButton>
-        </strong>
-      )
-    }
-  ]
   return (
-    <Modal open={true} onClose={_=>{}} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">
-      <div className="dark:bg-zinc-800 border-2 dark:border-neutral-700 bg-zinc-50 relative left-1/2 -translate-x-1/2 top-[80px] w-[860px] flex flex-col items-center gap-4 p-5 rounded-lg">
-        <Typography component="h1" variant="h5" color="primary">
-          {'Maps I Share With Others'}
-        </Typography>
-        <div style={{ width: '100%' }}>
-          {shareDataExport.length > 0 &&
-            <DataGrid
-              rows={shareDataExport}
-              columns={columnsExport}
-              // pageSize={5}
-              // rowsPerPageOptions={[5]}
-              checkboxSelection
-              // disableSelectionOnClick
-              autoHeight={true}
-            />}
-        </div>
-        <Typography component="h1" variant="h5" color="primary">
-          {'Maps Others Share With Me'}
-        </Typography>
-        <div style={{ width: '100%' }}>
-          {shareDataImport.length > 0 &&
-            <DataGrid
-              rows={shareDataImport}
-              columns={columnsImport}
-              // pageSize={5}
-              // rowsPerPageOptions={[5]}
-              checkboxSelection
-              // disableSelectionOnClick
-              autoHeight={true}
-            />}
-        </div>
-        <Button
-          color="primary"
-          variant='outlined'
-          onClick={() => dispatch(actions.setPageState(PageState.WS))}>
-          {'CLOSE'}
-        </Button>
-      </div>
+    <Modal
+      theme={{
+        root: {
+          show: {
+            on: "flex bg-zinc-700 bg-opacity-25 dark:bg-opacity-40"
+          }
+        },
+        content: {
+          base: "top-[64px] relative h-full w-full p-4 md:h-auto",
+          inner: "relative rounded-lg bg-white shadow dark:bg-zinc-800 flex flex-col max-h-[90vh]"
+        }
+      }}
+      show={true}
+      onClose={() => dispatch(actions.setPageState(PageState.WS))}
+      position="top-center"
+      size="4xl"
+    >
+      <Modal.Header>{'Shares'}</Modal.Header>
+      <Modal.Body>
+        <Table>
+          <Table.Head className="text-xs text-white uppercase bg-blue-600 dark:text-white">
+            <Table.HeadCell className="bg-zinc-300 dark:bg-zinc-600">{'Map Name'}</Table.HeadCell>
+            <Table.HeadCell className="bg-zinc-300 dark:bg-zinc-600">{'Shared With'}</Table.HeadCell>
+            <Table.HeadCell className="bg-zinc-300 dark:bg-zinc-600">{'Access'}</Table.HeadCell>
+            <Table.HeadCell className="bg-zinc-300 dark:bg-zinc-600">{'Status'}</Table.HeadCell>
+            <Table.HeadCell className="bg-zinc-300 dark:bg-zinc-600">{'Actions'}</Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {
+              shareDataExport.map((el: { _id: string, sharedMapName: string, shareUserEmail: string, access: string, status: string  }) =>
+                <Table.Row key={el._id} className="bg-white dark:border-gray-700 dark:bg-zinc-700">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">{el.sharedMapName}</Table.Cell>
+                  <Table.Cell>{el.shareUserEmail}</Table.Cell>
+                  <Table.Cell>{el.access}</Table.Cell>
+                  <Table.Cell>{el.status}</Table.Cell>
+                  <Table.Cell>
+                    <a
+                      onClick={() => dispatch(nodeApi.endpoints.deleteMap.initiate({mapId: el._id}))}
+                      className="font-medium text-cyan-600 hover:underline hover:cursor-pointer dark:text-cyan-500"
+                    >{'Withdraw'}</a>
+                  </Table.Cell>
+                </Table.Row>
+              )
+            }
+          </Table.Body>
+        </Table>
+        <br/>
+        <Table>
+          <Table.Head className="text-xs text-white uppercase bg-blue-600 dark:text-white">
+            <Table.HeadCell className="bg-zinc-300 dark:bg-zinc-600">{'Map Name'}</Table.HeadCell>
+            <Table.HeadCell className="bg-zinc-300 dark:bg-zinc-600">{'Shared By'}</Table.HeadCell>
+            <Table.HeadCell className="bg-zinc-300 dark:bg-zinc-600">{'Access'}</Table.HeadCell>
+            <Table.HeadCell className="bg-zinc-300 dark:bg-zinc-600">{'Status'}</Table.HeadCell>
+            <Table.HeadCell className="bg-zinc-300 dark:bg-zinc-600">{'Actions'}</Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {
+              shareDataImport.map((el: { _id: string, sharedMapName: string, ownerUserEmail: string, access: string, status: string  }) =>
+                <Table.Row key={el._id} className="bg-white dark:border-gray-700 dark:bg-zinc-700">
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">{el.sharedMapName}</Table.Cell>
+                  <Table.Cell>{el.ownerUserEmail}</Table.Cell>
+                  <Table.Cell>{el.access}</Table.Cell>
+                  <Table.Cell>{el.status}</Table.Cell>
+                  {el.status === 'waiting' && <Table.Cell>
+                    <a
+                      onClick={() => {dispatch(nodeApi.endpoints.acceptShare.initiate({shareId: el._id}))}}
+                      className="font-medium text-cyan-600 hover:underline hover:cursor-pointer dark:text-cyan-500"
+                    >{'Accept'}</a></Table.Cell>}
+                  {el.status === 'accepted' && <Table.Cell><a>{'Accepted'}</a></Table.Cell>}
+                </Table.Row>
+              )
+            }
+          </Table.Body>
+        </Table>
+      </Modal.Body>
+      <Modal.Footer>
+      </Modal.Footer>
     </Modal>
   )
 }
