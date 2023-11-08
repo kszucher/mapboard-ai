89ff4c53364f65ec6c5198a0b59dfbf515c6c0f6
Krisztian Szucher <krisztian@szucher.com>
2023-11-08 23:12:34 +0100

    super sleek app bar

--- client-vite/src/components/editor/Editor.tsx
+++ client-vite/src/components/editor/Editor.tsx
@@ -1,13 +1,11 @@
-import {BookmarkIcon} from "@radix-ui/react-icons";
-import {Button, Theme} from "@radix-ui/themes"
+import {BookmarkIcon, CaretDownIcon} from "@radix-ui/react-icons";
+import {Button, DropdownMenu, IconButton, Theme} from "@radix-ui/themes"
 import {Breadcrumb} from "flowbite-react"
 import React, {FC, useEffect} from 'react'
 import {useDispatch, useSelector} from "react-redux"
 import {Backdrop, CircularProgress} from '@mui/material'
 import {actions, AppDispatch, RootState} from "../../reducers/EditorReducer"
 import {mSelector} from "../../state/EditorState"
-import {IconButton} from "../misc/IconButton"
-import {RedoIcon, SettingsIcon, UndoIcon, UserIcon} from "../misc/IconButtonSvg"
 import {ContextMenu} from "../menu/ContextMenu"
 import {EditContentEquationModal} from "../modal/EditContentEquationModal"
 import {EditContentMermaidModal} from "../modal/EditContentMermaidModal"
@@ -60,24 +58,72 @@ export const Editor: FC = () => {
   return (
     <Theme>
       <>
-        <div className="fixed top-0 w-[220px] h-[40px] py-1 flex items-center justify-center bg-gradient-to-r from-purple-900 to-purple-700 text-white z-50">
-          <h5 style={{fontFamily: "Comfortaa"}} className="text-xl dark:text-white">mapboard</h5>
-        </div>
+
         {
           mExists && <>
             <Map/>
-            <div className="dark:bg-zinc-800 bg-zinc-50 top-0 fixed left-[260px] h-[40px] flex items-center rounded-b-lg py-1 px-4 border-2 border-purple-700 border-t-0 z-50">
-              <Breadcrumb aria-label="Default breadcrumb example">
-                {breadcrumbMapNameList.map((el, index) => (
-                  <Breadcrumb.Item
-                    href="/"
-                    onClick={e => {e.preventDefault(); frameId !== '' ? console.log('prevent') : dispatch(nodeApi.endpoints.selectMap.initiate({mapId: breadcrumbMapIdList[index], frameId: ''}))}}
-                    key={index}
-                  >
-                    {el.name}
-                  </Breadcrumb.Item>
-                ))}
-              </Breadcrumb>
+            <div className="dark:bg-zinc-800 bg-zinc-50 dark:border-neutral-700 fixed top-0 left-0 w-screen h-[40px] z-50">
+              <div className="fixed top-0 w-[220px] h-[40px] py-1 flex items-center justify-center bg-gradient-to-r from-purple-900 to-purple-700 text-white z-50">
+                <h5 style={{fontFamily: "Comfortaa"}} className="text-xl dark:text-white">mapboard</h5>
+              </div>
+              <div className="dark:bg-zinc-800 bg-zinc-50 top-0 fixed left-[260px] h-[40px] flex items-center border-0 py-1 px-4 z-50">
+                <Breadcrumb aria-label="Default breadcrumb example">
+                  {breadcrumbMapNameList.map((el, index) => (
+                    <Breadcrumb.Item
+                      href="/"
+                      onClick={e => {e.preventDefault(); frameId !== '' ? console.log('prevent') : dispatch(nodeApi.endpoints.selectMap.initiate({mapId: breadcrumbMapIdList[index], frameId: ''}))}}
+                      key={index}
+                    >
+                      {el.name}
+                    </Breadcrumb.Item>
+                  ))}
+                </Breadcrumb>
+                {/*<div className="dark:bg-zinc-800 bg-zinc-50 border-2 dark:border-neutral-700 fixed top-0 right-[260px] w-[96px] flex justify-around h-[40px] py-1 border-t-0 rounded-b-lg z-50">*/}
+                  {/*<IconButton colorMode={colorMode} disabled={undoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'undo', payload: null}))}}><UndoIcon/></IconButton>*/}
+                  {/*<IconButton colorMode={colorMode} disabled={redoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'redo', payload: null}))}}><RedoIcon/></IconButton>*/}
+                {/*</div>*/}
+                <div className="fixed right-[4px] top-[4px]">
+                  {/*<IconButton colorMode={colorMode} disabled={false} onClick={() => {dispatch(actions.setPageState(PageState.WS_SETTINGS))}}><SettingsIcon/></IconButton>*/}
+                  {/*<IconButton colorMode={colorMode} disabled={false} onClick={() => {dispatch(actions.setPageState(PageState.WS_PROFILE))}}><UserIcon/></IconButton>*/}
+
+                  <DropdownMenu.Root>
+                    <DropdownMenu.Trigger>
+                      <IconButton variant="solid"  color="gray">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="icon icon-tabler icon-tabler-12-hours" viewBox="0 0 24 24">
+                          <path stroke="none" d="M0 0h24v24H0z"></path>
+                          <path d="M20 11A8.1 8.1 0 004.5 9M4 5v4h4M4 13c.468 3.6 3.384 6.546 7 7M18 15h2a1 1 0 011 1v1a1 1 0 01-1 1h-1a1 1 0 00-1 1v1a1 1 0 001 1h2M15 21v-6"></path>
+                        </svg>
+                      </IconButton>
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content>
+                      <DropdownMenu.Item shortcut="⌘ E">Edit</DropdownMenu.Item>
+                      <DropdownMenu.Item shortcut="⌘ D">Duplicate</DropdownMenu.Item>
+                      <DropdownMenu.Separator />
+                      <DropdownMenu.Item shortcut="⌘ N">Archive</DropdownMenu.Item>
+
+                      <DropdownMenu.Sub>
+                        <DropdownMenu.SubTrigger>More</DropdownMenu.SubTrigger>
+                        <DropdownMenu.SubContent>
+                          <DropdownMenu.Item>Move to project…</DropdownMenu.Item>
+                          <DropdownMenu.Item>Move to folder…</DropdownMenu.Item>
+
+                          <DropdownMenu.Separator />
+                          <DropdownMenu.Item>Advanced options…</DropdownMenu.Item>
+                        </DropdownMenu.SubContent>
+                      </DropdownMenu.Sub>
+
+                      <DropdownMenu.Separator />
+                      <DropdownMenu.Item>Share</DropdownMenu.Item>
+                      <DropdownMenu.Item>Add to favorites</DropdownMenu.Item>
+                      <DropdownMenu.Separator />
+                      <DropdownMenu.Item shortcut="⌘ ⌫" color="red">
+                        Delete
+                      </DropdownMenu.Item>
+                    </DropdownMenu.Content>
+                  </DropdownMenu.Root>
+
+                </div>
+              </div>
             </div>
             {!tabShrink &&
               <div className="fixed z-50 w-[224px] top-[80px] dark:bg-zinc-800 bg-zinc-50 border-l-0 border-2 dark:border-neutral-700 pt-4 rounded-r-lg">
@@ -112,14 +158,7 @@ export const Editor: FC = () => {
             <FrameCarousel/>
             <ContextMenu/>
             <Window/>
-            <div className="dark:bg-zinc-800 bg-zinc-50 border-2 dark:border-neutral-700 fixed top-0 right-[120px] w-[96px] flex justify-around h-[40px] py-1 border-t-0 rounded-b-lg z-50">
-              <IconButton colorMode={colorMode} disabled={undoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'undo', payload: null}))}}><UndoIcon/></IconButton>
-              <IconButton colorMode={colorMode} disabled={redoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'redo', payload: null}))}}><RedoIcon/></IconButton>
-            </div>
-            <div className="dark:bg-zinc-800 bg-zinc-50 border-2 dark:border-neutral-700 fixed top-0 right-0 w-[96px] h-[40px] flex flex-row flex-center border-t-0 border-r-0 py-1 px-2 rounded-bl-lg z-50">
-              <IconButton colorMode={colorMode} disabled={false} onClick={() => {dispatch(actions.setPageState(PageState.WS_SETTINGS))}}><SettingsIcon/></IconButton>
-              <IconButton colorMode={colorMode} disabled={false} onClick={() => {dispatch(actions.setPageState(PageState.WS_PROFILE))}}><UserIcon/></IconButton>
-            </div>
+
           </>
         }
         {pageState === PageState.WS_PROFILE && <Profile/>}
