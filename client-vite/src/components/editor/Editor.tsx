971345b40fb294880a11a0e30e41730f09329cd4
Krisztian Szucher <krisztian@szucher.com>
2023-11-10 14:01:25 +0100

    undo/redo replaced with radixui

--- client-vite/src/components/editor/Editor.tsx
+++ client-vite/src/components/editor/Editor.tsx
@@ -63,10 +63,27 @@ export const Editor: FC = () => {
           <>
             <Map/>
             <div className="dark:bg-zinc-800 bg-zinc-50 dark:border-neutral-700 fixed top-0 left-0 w-screen h-[40px] z-50">
-              <div className="fixed top-0 w-[220px] h-[40px] py-1 flex items-center justify-center bg-gradient-to-r from-purple-900 to-purple-700 text-white z-50">
+              <div className="fixed top-0 w-[220px] h-[40px] py-1 flex items-center justify-center bg-gradient-to-r from-purple-900 to-purple-700 text-white z-50 ">
                 <h5 style={{fontFamily: "Comfortaa"}} className="text-xl dark:text-white">mapboard</h5>
               </div>
-              <div className="dark:bg-zinc-800 bg-zinc-50 top-0 fixed left-[260px] h-[40px] flex items-center border-0 py-1 px-4 z-50">
+              <div className="fixed w-[68px] left-[240px] top-[4px] flex flex-row">
+                <IconButton variant="solid"  color="gray" disabled={undoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'undo', payload: null}))}}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="icon icon-tabler icon-tabler-arrow-back-up" viewBox="0 0 24 24">
+                    <path stroke="none" d="M0 0h24v24H0z"></path>
+                    <path d="M9 14l-4-4 4-4"></path>
+                    <path d="M5 10h11a4 4 0 110 8h-1"></path>
+                  </svg>
+                </IconButton>
+                <div className={"w-[4px]"}/>
+                <IconButton variant="solid"  color="gray" disabled={redoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'redo', payload: null}))}}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="icon icon-tabler icon-tabler-arrow-forward-up" viewBox="0 0 24 24">
+                    <path stroke="none" d="M0 0h24v24H0z"></path>
+                    <path d="M15 14l4-4-4-4"></path>
+                    <path d="M19 10H8a4 4 0 100 8h1"></path>
+                  </svg>
+                </IconButton>
+              </div>
+              <div className="fixed left-[280px] flex flex-row">
                 <Breadcrumb aria-label="Default breadcrumb example">
                   {breadcrumbMapNameList.map((el, index) => (
                     <Breadcrumb.Item
@@ -78,64 +95,60 @@ export const Editor: FC = () => {
                     </Breadcrumb.Item>
                   ))}
                 </Breadcrumb>
-                {/*<div className="dark:bg-zinc-800 bg-zinc-50 border-2 dark:border-neutral-700 fixed top-0 right-[260px] w-[96px] flex justify-around h-[40px] py-1 border-t-0 rounded-b-lg z-50">*/}
-                {/*<IconButton colorMode={colorMode} disabled={undoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'undo', payload: null}))}}><UndoIcon/></IconButton>*/}
-                {/*<IconButton colorMode={colorMode} disabled={redoDisabled} onClick={() => {dispatch(actions.mapAction({type: 'redo', payload: null}))}}><RedoIcon/></IconButton>*/}
-                {/*</div>*/}
-                <div className="fixed w-[68px] right-[4px] top-[4px] flex flex-row">
-                  <AlertDialog.Root>
-                    <DropdownMenu.Root>
-                      <DropdownMenu.Trigger>
-                        <IconButton variant="solid"  color="gray">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="icon icon-tabler icon-tabler-settings-2" viewBox="0 0 24 24">
-                            <path stroke="none" d="M0 0h24v24H0z"></path>
-                            <path d="M19.875 6.27A2.225 2.225 0 0121 8.218v7.284c0 .809-.443 1.555-1.158 1.948l-6.75 4.27a2.269 2.269 0 01-2.184 0l-6.75-4.27A2.225 2.225 0 013 15.502V8.217c0-.809.443-1.554 1.158-1.947l6.75-3.98a2.33 2.33 0 012.25 0l6.75 3.98h-.033z"></path>
-                            <path d="M9 12a3 3 0 106 0 3 3 0 10-6 0"></path>
-                          </svg>
-                        </IconButton>
-                      </DropdownMenu.Trigger>
-                      <DropdownMenu.Content className="bg-red-300">
-                        <DropdownMenu.Item onClick={()=>{
-                          dispatch(nodeApi.endpoints.toggleColorMode.initiate())
-                        }}>{'Toggle Color Mode'}</DropdownMenu.Item>
-                      </DropdownMenu.Content>
-                    </DropdownMenu.Root>
-                    <AlertDialog.Content style={{ maxWidth: 450 }}>
-                      <DeleteAccountDialogContent/>
-                    </AlertDialog.Content>
-                    <div className={"w-[4px]"}/>
-                    <DropdownMenu.Root>
-                      <DropdownMenu.Trigger>
-                        <IconButton variant="solid"  color="gray">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="icon icon-tabler icon-tabler-user" viewBox="0 0 24 24">
-                            <path stroke="none" d="M0 0h24v24H0z"></path>
-                            <path d="M8 7a4 4 0 108 0 4 4 0 00-8 0M6 21v-2a4 4 0 014-4h4a4 4 0 014 4v2"></path>
-                          </svg>
-                        </IconButton>
-                      </DropdownMenu.Trigger>
-                      <DropdownMenu.Content className="bg-red-300">
-                        <DropdownMenu.Item onClick={()=>{
-                          logout({ logoutParams: { returnTo: window.location.origin }})
-                          dispatch(actions.resetState())
-                          dispatch(nodeApi.util.resetApiState())
-                        }}>{'Sign Out'}</DropdownMenu.Item>
-                        <DropdownMenu.Item onClick={()=>{
-                          logout({ logoutParams: { returnTo: window.location.origin }})
-                          dispatch(nodeApi.endpoints.signOutEverywhere.initiate())
-                          dispatch(actions.resetState())
-                          dispatch(nodeApi.util.resetApiState())
-                        }}>{'Sign Out All Devices'}</DropdownMenu.Item>
-                        <DropdownMenu.Separator />
-                        <AlertDialog.Trigger>
-                          <DropdownMenu.Item color="red">Delete Account</DropdownMenu.Item>
-                        </AlertDialog.Trigger>
-                      </DropdownMenu.Content>
-                    </DropdownMenu.Root>
-                    <AlertDialog.Content style={{ maxWidth: 450 }}>
-                      <DeleteAccountDialogContent/>
-                    </AlertDialog.Content>
-                  </AlertDialog.Root>
-                </div>
+              </div>
+              <div className="fixed w-[68px] right-[4px] top-[4px] flex flex-row">
+                <AlertDialog.Root>
+                  <DropdownMenu.Root>
+                    <DropdownMenu.Trigger>
+                      <IconButton variant="solid"  color="gray">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="icon icon-tabler icon-tabler-settings-2" viewBox="0 0 24 24">
+                          <path stroke="none" d="M0 0h24v24H0z"></path>
+                          <path d="M19.875 6.27A2.225 2.225 0 0121 8.218v7.284c0 .809-.443 1.555-1.158 1.948l-6.75 4.27a2.269 2.269 0 01-2.184 0l-6.75-4.27A2.225 2.225 0 013 15.502V8.217c0-.809.443-1.554 1.158-1.947l6.75-3.98a2.33 2.33 0 012.25 0l6.75 3.98h-.033z"></path>
+                          <path d="M9 12a3 3 0 106 0 3 3 0 10-6 0"></path>
+                        </svg>
+                      </IconButton>
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content className="bg-red-300">
+                      <DropdownMenu.Item onClick={()=>{
+                        dispatch(nodeApi.endpoints.toggleColorMode.initiate())
+                      }}>{'Toggle Color Mode'}</DropdownMenu.Item>
+                    </DropdownMenu.Content>
+                  </DropdownMenu.Root>
+                  <AlertDialog.Content style={{ maxWidth: 450 }}>
+                    <DeleteAccountDialogContent/>
+                  </AlertDialog.Content>
+                  <div className={"w-[4px]"}/>
+                  <DropdownMenu.Root>
+                    <DropdownMenu.Trigger>
+                      <IconButton variant="solid"  color="gray">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="icon icon-tabler icon-tabler-user" viewBox="0 0 24 24">
+                          <path stroke="none" d="M0 0h24v24H0z"></path>
+                          <path d="M8 7a4 4 0 108 0 4 4 0 00-8 0M6 21v-2a4 4 0 014-4h4a4 4 0 014 4v2"></path>
+                        </svg>
+                      </IconButton>
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content className="bg-red-300">
+                      <DropdownMenu.Item onClick={()=>{
+                        logout({ logoutParams: { returnTo: window.location.origin }})
+                        dispatch(actions.resetState())
+                        dispatch(nodeApi.util.resetApiState())
+                      }}>{'Sign Out'}</DropdownMenu.Item>
+                      <DropdownMenu.Item onClick={()=>{
+                        logout({ logoutParams: { returnTo: window.location.origin }})
+                        dispatch(nodeApi.endpoints.signOutEverywhere.initiate())
+                        dispatch(actions.resetState())
+                        dispatch(nodeApi.util.resetApiState())
+                      }}>{'Sign Out All Devices'}</DropdownMenu.Item>
+                      <DropdownMenu.Separator />
+                      <AlertDialog.Trigger>
+                        <DropdownMenu.Item color="red">Delete Account</DropdownMenu.Item>
+                      </AlertDialog.Trigger>
+                    </DropdownMenu.Content>
+                  </DropdownMenu.Root>
+                  <AlertDialog.Content style={{ maxWidth: 450 }}>
+                    <DeleteAccountDialogContent/>
+                  </AlertDialog.Content>
+                </AlertDialog.Root>
               </div>
             </div>
             {!tabShrink &&
