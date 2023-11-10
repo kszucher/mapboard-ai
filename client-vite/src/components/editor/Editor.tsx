42401a599980880e7b824709e1b3669ccd8d5dbc
Krisztian Szucher <krisztian@szucher.com>
2023-11-10 12:30:29 +0100

    nicer icon

--- client-vite/src/components/editor/Editor.tsx
+++ client-vite/src/components/editor/Editor.tsx
@@ -87,10 +87,10 @@ export const Editor: FC = () => {
                     <DropdownMenu.Root>
                       <DropdownMenu.Trigger>
                         <IconButton variant="solid"  color="gray">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="icon icon-tabler icon-tabler-settings" viewBox="0 0 24 24">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="icon icon-tabler icon-tabler-settings-2" viewBox="0 0 24 24">
                             <path stroke="none" d="M0 0h24v24H0z"></path>
-                            <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37 1 .608 2.296.07 2.572-1.065z"></path>
-                            <path d="M9 12a3 3 0 106 0 3 3 0 00-6 0"></path>
+                            <path d="M19.875 6.27A2.225 2.225 0 0121 8.218v7.284c0 .809-.443 1.555-1.158 1.948l-6.75 4.27a2.269 2.269 0 01-2.184 0l-6.75-4.27A2.225 2.225 0 013 15.502V8.217c0-.809.443-1.554 1.158-1.947l6.75-3.98a2.33 2.33 0 012.25 0l6.75 3.98h-.033z"></path>
+                            <path d="M9 12a3 3 0 106 0 3 3 0 10-6 0"></path>
                           </svg>
                         </IconButton>
                       </DropdownMenu.Trigger>
