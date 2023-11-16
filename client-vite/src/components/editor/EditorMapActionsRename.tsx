9d93d012c26e2e5858175fcbe5dd6b94530aa0b8
Krisztian Szucher <krisztian@szucher.com>
2023-11-16 12:34:02 +0100

    rename map replaced

--- client-vite/src/components/editor/EditorMapActionsRename.tsx
+++ client-vite/src/components/editor/EditorMapActionsRename.tsx
@@ -1,12 +1,20 @@
 import {Button, Dialog, Flex, Text, TextField} from "@radix-ui/themes"
-import React from "react"
+import React, {useState} from "react"
+import {useDispatch} from "react-redux";
+import {nodeApi, useOpenWorkspaceQuery} from "../../apis/NodeApi"
+import {AppDispatch} from "../../reducers/EditorReducer"
+import {defaultUseOpenWorkspaceQueryState, getMapId} from "../../state/NodeApiState"
 
 export const EditorMapActionsRename = () => {
+  const { data } = useOpenWorkspaceQuery()
+  const { tabMapNameList, tabId } = data || defaultUseOpenWorkspaceQueryState
+  const [mapName, setMapName] = useState(tabMapNameList[tabId].name)
+  const dispatch = useDispatch<AppDispatch>()
   return (
     <Dialog.Content style={{ maxWidth: 450 }}>
       <Dialog.Title>{'Rename Map'}</Dialog.Title>
       <Dialog.Description size="2" mb="4">
-        Rename map
+        {'Rename map'}
       </Dialog.Description>
       <Flex direction="column" gap="3">
         <label>
@@ -15,19 +23,22 @@ export const EditorMapActionsRename = () => {
           </Text>
           <TextField.Input
             radius="large"
-            defaultValue="Freja Johnsen"
-            placeholder="Enter your full name"
+            value={mapName}
+            placeholder="Map name"
+            onChange={(e) => setMapName(e.target.value)}
           />
         </label>
       </Flex>
       <Flex gap="3" mt="4" justify="end">
         <Dialog.Close>
           <Button variant="soft" color="gray">
-            Cancel
+            {'Cancel'}
           </Button>
         </Dialog.Close>
         <Dialog.Close>
-          <Button>Save</Button>
+          <Button onClick={() => dispatch(nodeApi.endpoints.renameMap.initiate({mapId: getMapId(), name: mapName}))}>
+            {'Save'}
+          </Button>
         </Dialog.Close>
       </Flex>
     </Dialog.Content>
