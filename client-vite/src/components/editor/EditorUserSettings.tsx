1f387bc0cd3bd6e51187ea58a3d967b942035961
Krisztian Szucher <krisztian@szucher.com>
2023-11-18 16:43:58 +0100

    ok so I need to use isFetching to prevent click

--- client-vite/src/components/editor/EditorUserSettings.tsx
+++ client-vite/src/components/editor/EditorUserSettings.tsx
@@ -1,13 +1,14 @@
 import {DropdownMenu, IconButton} from "@radix-ui/themes"
 import React from "react"
 import {useDispatch, useSelector} from "react-redux"
-import {nodeApi} from "../../apis/NodeApi"
+import {nodeApi, useOpenWorkspaceQuery} from "../../apis/NodeApi"
 import {actions, AppDispatch, RootState} from "../../reducers/EditorReducer"
 import {SettingsIcon} from "../assets/Icons"
 
 export const EditorUserSettings = () => {
   const scrollOverride = useSelector((state: RootState) => state.editor.scrollOverride)
   const connectionHelpersVisible = useSelector((state: RootState) => state.editor.connectionHelpersVisible)
+  const { isFetching } = useOpenWorkspaceQuery()
   const dispatch = useDispatch<AppDispatch>()
   return (
     <DropdownMenu.Root>
@@ -17,7 +18,7 @@ export const EditorUserSettings = () => {
         </IconButton>
       </DropdownMenu.Trigger>
       <DropdownMenu.Content>
-        <DropdownMenu.Item onClick={() => dispatch(nodeApi.endpoints.toggleColorMode.initiate())}>{'Toggle Color Mode'}</DropdownMenu.Item>
+        {!isFetching && <DropdownMenu.Item onClick={() => dispatch(nodeApi.endpoints.toggleColorMode.initiate())}>{'Toggle Color Mode'}</DropdownMenu.Item>}
         {!scrollOverride && <DropdownMenu.Item onClick={() => dispatch(actions.setScrollOverride())}>{'Set Scroll Zoom'}</DropdownMenu.Item>}
         {scrollOverride && <DropdownMenu.Item onClick={() => dispatch(actions.clearScrollOverride())}>{'Clear Scroll Zoom'}</DropdownMenu.Item>}
         {!connectionHelpersVisible && <DropdownMenu.Item onClick={() => dispatch(actions.showConnectionHelpers())}>{'Show Helpers'}</DropdownMenu.Item>}
