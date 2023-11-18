d7315e151e8a66a2e9fb7b85e9b3b5f0a42ff80e
Krisztian Szucher <krisztian@szucher.com>
2023-11-18 16:52:19 +0100

    this is not a solution

--- client-vite/src/components/editor/EditorUserSettings.tsx
+++ client-vite/src/components/editor/EditorUserSettings.tsx
@@ -8,7 +8,6 @@ import {SettingsIcon} from "../assets/Icons"
 export const EditorUserSettings = () => {
   const scrollOverride = useSelector((state: RootState) => state.editor.scrollOverride)
   const connectionHelpersVisible = useSelector((state: RootState) => state.editor.connectionHelpersVisible)
-  const { isFetching } = useOpenWorkspaceQuery()
   const dispatch = useDispatch<AppDispatch>()
   return (
     <DropdownMenu.Root>
@@ -18,7 +17,7 @@ export const EditorUserSettings = () => {
         </IconButton>
       </DropdownMenu.Trigger>
       <DropdownMenu.Content>
-        {!isFetching && <DropdownMenu.Item onClick={() => dispatch(nodeApi.endpoints.toggleColorMode.initiate())}>{'Toggle Color Mode'}</DropdownMenu.Item>}
+        <DropdownMenu.Item onClick={() => dispatch(nodeApi.endpoints.toggleColorMode.initiate())}>{'Toggle Color Mode'}</DropdownMenu.Item>
         {!scrollOverride && <DropdownMenu.Item onClick={() => dispatch(actions.setScrollOverride())}>{'Set Scroll Zoom'}</DropdownMenu.Item>}
         {scrollOverride && <DropdownMenu.Item onClick={() => dispatch(actions.clearScrollOverride())}>{'Clear Scroll Zoom'}</DropdownMenu.Item>}
         {!connectionHelpersVisible && <DropdownMenu.Item onClick={() => dispatch(actions.showConnectionHelpers())}>{'Show Helpers'}</DropdownMenu.Item>}
