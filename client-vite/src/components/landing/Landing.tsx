eb25751804c7604ba4eaaa20b87d3b63f321415d
Krisztian Szucher <krisztian@szucher.com>
2023-11-29 12:33:33 +0100

    change of name

--- client-vite/src/components/landing/Landing.tsx
+++ client-vite/src/components/landing/Landing.tsx
@@ -74,7 +74,7 @@ export const Landing: FC = () => {
                      className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
                      onClick={() => loginWithRedirect()}
                   >
-                    Try Alpha
+                    Login
                     <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                          xmlns="http://www.w3.org/2000/svg">
                       <path
