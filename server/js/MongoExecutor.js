be3e4c5a7e9bde09fde332e803c6830de0236cab
Krisztian Szucher <krisztian@szucher.com>
2023-12-06 13:12:45 +0100

    mongo executor set to CLEAN UP the production env

--- server/js/MongoExecutor.js
+++ server/js/MongoExecutor.js
@@ -1,41 +1,26 @@
 const { baseUri } = require('./MongoSecret')
-const MongoQueries = require('./MongoQueries')
 const MongoMutations = require('./MongoMutations')
-const { genNodeId, saveMap } = require('./MongoMutations')
-const { ObjectId } = require('mongodb')
-const { countNodesBasedOnNodePropValue } = require('./MongoQueries')
 const MongoClient = require('mongodb').MongoClient
 
 async function mongoExecutorCommands (users, maps, shares) {
-  // console.log(await MongoQueries.countNodes(maps))
-  // console.log(await MongoQueries.countNodesBasedOnNodePropExistence(maps, 'svgId'))
-  // console.log(await MongoQueries.countNodesBasedOnNodePropExistence(maps, 'divId'))
-  // console.log(await MongoQueries.countNodesBasedOnNodePropExistence(maps, 'nodeId'))
-  // console.log(JSON.stringify(await MongoQueries.findDeadLinks(maps), null, 4))
-
-  // console.log(await countNodesBasedOnNodePropValue(maps, 'taskStatus', 0))
-  // await MongoMutations.removeNodeProp(maps, 'nodeId')
-  // await MongoMutations.createNodeProp(maps, 'nodeId', genNodeId())
-  // await MongoMutations.updateNodePropValueBasedOnPreviousValue(maps, 'taskStatus', -1, 0)
-  // await MongoMutations.deleteUnusedMaps(users, maps)
-
-  // await maps.updateMany({}, [
-  //   {
-  //     $set: {
-  //       versions: [ {$last: '$versions'} ],
-  //       versionsInfo: [ {$last: '$versionsInfo'} ],
-  //     }
-  //   }
-  // ])
-
-
+  await MongoMutations.deleteUnusedMaps(users, maps)
+  await maps.updateMany({}, [
+    {
+      $set: {
+        versions: [ {$last: '$versions'} ],
+        versionsInfo: [ {$last: '$versionsInfo'} ],
+        frames: [],
+        framesInfo: [],
+      }
+    }
+  ])
 }
 
 async function mongoExecutor() {
   const client = new MongoClient(baseUri, { useNewUrlParser: true, useUnifiedTopology: true })
   try {
     await client.connect()
-    const db = client.db("app_dev")
+    const db = client.db("app_prod")
     const users = db.collection("users")
     const maps = db.collection("maps")
     const shares = db.collection("shares")
