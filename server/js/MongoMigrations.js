3c2c8618f2a0f1fd6421d7e1d6e6d4daf08a304b
Krisztian Szucher <krisztian@szucher.com>
2023-12-06 18:57:47 +0100

    migration script for separating r from s

---
+++ server/js/MongoMigrations.js
@@ -0,0 +1,78 @@
+const fs = require("fs")
+
+const isEqual = (obj1, obj2) =>  JSON.stringify(obj1) === JSON.stringify(obj2)
+
+const sortablePath = (p) => p.map((pi) => isNaN(pi) ? pi: 1000 + pi).join('')
+
+const sortPath = (a, b) => sortablePath(a.path) > sortablePath(b.path) ? 1 : -1
+
+const genHash = () => {
+  const alphanumeric = '0123456789abcdefghijklmnopqrstuvwxyz'
+  const randomAlphanumeric = () => ( alphanumeric[ Math.round( Math.random() * ( alphanumeric.length -  1 )) ] )
+  const randomAlphanumeric8digit = new Array(8).fill('').map(el => randomAlphanumeric())
+  return randomAlphanumeric8digit.join('')
+}
+
+const maps = JSON.parse(fs.readFileSync("../data/maps.json", "utf8"))
+
+const filteredObject = (raw, allowed) => Object.fromEntries(Object.entries(raw).filter(([key, val]) => allowed.includes(key)))
+
+// console.log(maps[0])
+
+maps.forEach(map => Object.assign(
+  map.versions[0],
+  [
+    ...map.versions[0]
+      .filter(node => node.path.length === 1),
+    ...map.versions[0]
+      .filter(node => node.path.length === 2)
+      .map(el => filteredObject(el, [
+        'path',
+        'nodeId',
+        'controlType',
+        'offsetW',
+        'offsetH',
+        'llmDataType',
+        'llmDataId',
+      ])),
+    ...map.versions[0]
+      .filter(node => node.path.length === 2)
+      .map(node => ({...node, path: [...node.path.slice(0, 2), 's', 0], nodeId: 'node' + genHash(8)}))
+      .map(el => filteredObject(el, [
+        'path',
+        'nodeId',
+        'contentType',
+        'content',
+        'linkType',
+        'link',
+        'imageW',
+        'imageH',
+        'dimW',
+        'dimH',
+        'selected',
+        'selection',
+        'lastSelectedChild',
+        'lineWidth',
+        'lineType',
+        'lineColor',
+        'sBorderWidth',
+        'fBorderWidth',
+        'sBorderColor',
+        'fBorderColor',
+        'sFillColor',
+        'fFillColor',
+        'textFontSize',
+        'textColor',
+        'taskStatus',
+        'note',
+        'blur',
+      ])),
+    ...map.versions[0]
+      .filter(node => node.path.length > 2)
+      .map(node => ({...node, path: [...node.path.slice(0, 2), 's', 0, ...node.path.slice(2)]}))
+  ].sort(sortPath)
+))
+
+console.log(maps[0].versions[0])
+
+fs.writeFileSync('../data/mapsUpdated.json', JSON.stringify(maps))
