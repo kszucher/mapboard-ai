9834c5183c201c42309b39fe3223be6779bf6c19
Krisztian Szucher <krisztian@szucher.com>
2023-12-18 16:18:29 +0100

    neutral landing

--- client-vite/src/components/landing/Landing.tsx
+++ client-vite/src/components/landing/Landing.tsx
@@ -47,34 +47,33 @@ export const Landing: FC = () => {
     <>
       {isWaiting
         ? <Spinner/>
-        : <section className="bg-white dark:bg-gray-900">
+        : <section className="w-screen h-screen bg-white dark:bg-gray-900">
           <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
             <section className="bg-white dark:bg-gray-900">
               <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
                 <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
                   <div className="">
-                    <h1 className="flex justify-center">No-Code Automation and Analytics For</h1>
-                    <h1 className="flex justify-center">HR and People Ops:</h1>
-                    <h1 className="rotate-wrap leading-normal flex justify-center ">
-                      <span> &nbsp;</span>
-                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Recruitment & Talent Acquisition</span>
-                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-teal-600 from-green-400">Learning & Development</span>
-                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-orange-600 from-amber-400">Performance Management</span>
-                      <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-sky-600 from-indigo-400">Engagement & Retention</span>
-                    </h1>
+                    <h1 className="flex justify-center">MapBoard</h1>
+                    {/*<h1 className="rotate-wrap leading-normal flex justify-center ">*/}
+                    {/*  <span> &nbsp;</span>*/}
+                    {/*  <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Business Analytics</span>*/}
+                    {/*  <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-teal-600 from-green-400">HR and People Ops</span>*/}
+                    {/*  <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-orange-600 from-amber-400">Product Management</span>*/}
+                    {/*  <span className="rotatingtext text-transparent bg-clip-text bg-gradient-to-r to-sky-600 from-indigo-400">... And More</span>*/}
+                    {/*</h1>*/}
                   </div>
                 </h1>
                 <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-                  MapBoard gives HR and People Ops Professionals Superpowers,
+                  AI-powered Visual Workspaces
                   <br/>
-                  With Expert-level Data Assessment and Analytics based on LLMs
+                  For Research, Analysis and Reporting
                 </p>
                 <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                   <a href="#"
                      className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
                      onClick={() => loginWithRedirect()}
                   >
-                    Login
+                    Try Alpha
                     <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                          xmlns="http://www.w3.org/2000/svg">
                       <path
@@ -98,7 +97,7 @@ export const Landing: FC = () => {
                   className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[16px] rounded-t-xl h-[344px] max-w-[602px] md:h-[588px] md:max-w-[1024px]">
                   <div className="rounded-lg overflow-hidden h-[312px] md:h-[556px] bg-white dark:bg-gray-800">
                     <img
-                      src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaCLN55p8JPEfPPVWcSPSGbCIhLbvZUpPXLKAhaiN9HM80sJXLpPpcILisESdsI066Ht9l46ozSBuOQeKzgVYaWo_-3C=w2560-h1323"
+                      src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaDOq5FfsAQTp416-z5fHt6ty-i7yV-ng2itrxZ-TDXlCy5G3UQlPYY1jKEW4aFvjwO1Wkz3h5wcao-D3SG1b8cRvDZ4HA=w2560-h1323"
                       className="hidden dark:block h-[312px] md:h-[556px] w-full rounded-lg" alt=""/>
                   </div>
                 </div>
