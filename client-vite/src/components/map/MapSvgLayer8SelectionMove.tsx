1b72f994e3ccbfd869665077d07edaa0b427c228
Krisztian Szucher <krisztian@szucher.com>
2023-12-19 17:00:38 +0100

    simplify

--- client-vite/src/components/map/MapSvgLayer8SelectionMove.tsx
+++ client-vite/src/components/map/MapSvgLayer8SelectionMove.tsx
@@ -1,4 +1,4 @@
-import {FC, Fragment,} from "react"
+import {FC} from "react"
 import {useSelector} from "react-redux"
 import colors from "tailwindcss/colors"
 import {useOpenWorkspaceQuery} from "../../apis/NodeApi"
@@ -14,24 +14,27 @@ export const MapSvgLayer8SelectionMove: FC = () => {
   const C = getColors(colorMode)
   return (
     <g>
-      {
-        moveCoords.length &&
-        <Fragment>
-          <path d={getBezierLinePath('M', getBezierLinePoints(moveCoords))} stroke={C.MOVE_LINE_COLOR} strokeWidth={1} fill={'none'}></path>
-          <rect
-            x={moveCoords[2] - 10}
-            y={moveCoords[3] - 10}
-            width={20}
-            height={20}
-            rx={8}
-            ry={8}
-            fill={colorMode === 'dark' ? colors.zinc[800] : colors.zinc[50]}
-            fillOpacity={1}
-            stroke={C.MOVE_RECT_COLOR}
-            strokeWidth={5}
-          >
-          </rect>
-        </Fragment>
+      {moveCoords.length &&
+        <path
+          d={getBezierLinePath('M', getBezierLinePoints(moveCoords))}
+          stroke={C.MOVE_LINE_COLOR}
+          strokeWidth={1}
+          fill={'none'}
+        />
+      }
+      {moveCoords.length &&
+        <rect
+          x={moveCoords[2] - 10}
+          y={moveCoords[3] - 10}
+          width={20}
+          height={20}
+          rx={8}
+          ry={8}
+          fill={colorMode === 'dark' ? colors.zinc[800] : colors.zinc[50]}
+          fillOpacity={1}
+          stroke={C.MOVE_RECT_COLOR}
+          strokeWidth={5}
+        />
       }
     </g>
   )
