5e72babb1b18efc8f9f9fb714c1c453569a82c7b
Krisztian Szucher <krisztian@szucher.com>
2023-12-23 16:05:19 +0100

    indent table fix

--- client-vite/src/reducers/MapPlace.ts
+++ client-vite/src/reducers/MapPlace.ts
@@ -55,15 +55,28 @@ export const mapPlace = (m: M) => {
         const si2 = getTSI2(m, ti) as T
         const i = ti.path.at(-2)
         const j = ti.path.at(-1)
-        if (isRSC(ti.path)) {
-          ti.nodeStartX = MARGIN_X + si2.nodeStartX + si1.sumMaxColWidth[j]
-          ti.nodeStartY = si1.nodeStartY + si1.selfH / 2 - ti.selfH / 2 + si1.sumMaxRowHeight[i] + si1.maxRowHeight[i] / 2 - si1.selfH / 2
-        } else if (isSSC(ti.path)) {
-          ti.nodeStartX = si2.nodeEndX + g.sLineDeltaXDefault + si1.sumMaxColWidth[j]
-          ti.nodeStartY = si1.nodeStartY + si1.selfH / 2 - ti.selfH / 2 + si1.sumMaxRowHeight[i] + si1.maxRowHeight[i] / 2 - si1.selfH / 2
-        } else if (isCSC(ti.path)) {
-          ti.nodeStartX = si2.nodeStartX + 2
-          ti.nodeStartY = si1.nodeStartY + si1.selfH / 2 - ti.selfH / 2 + si1.sumMaxRowHeight[i] + si1.maxRowHeight[i] / 2 - si1.selfH / 2
+        if (g.placeType === PlaceType.EXPLODED) {
+          if (isRSC(ti.path)) {
+            ti.nodeStartX = MARGIN_X + si2.nodeStartX + si1.sumMaxColWidth[j]
+            ti.nodeStartY = si1.nodeStartY + si1.selfH / 2 - ti.selfH / 2 + si1.sumMaxRowHeight[i] + si1.maxRowHeight[i] / 2 - si1.selfH / 2
+          } else if (isSSC(ti.path)) {
+            ti.nodeStartX = si2.nodeEndX + g.sLineDeltaXDefault + si1.sumMaxColWidth[j]
+            ti.nodeStartY = si1.nodeStartY + si1.selfH / 2 - ti.selfH / 2 + si1.sumMaxRowHeight[i] + si1.maxRowHeight[i] / 2 - si1.selfH / 2
+          } else if (isCSC(ti.path)) {
+            ti.nodeStartX = si2.nodeStartX + 2
+            ti.nodeStartY = si1.nodeStartY + si1.selfH / 2 - ti.selfH / 2 + si1.sumMaxRowHeight[i] + si1.maxRowHeight[i] / 2 - si1.selfH / 2
+          }
+        } else if (g.placeType === PlaceType.INDENTED) {
+          if (isRSC(ti.path)) {
+            ti.nodeStartX = MARGIN_X + si2.nodeStartX + si1.sumMaxColWidth[j]
+            ti.nodeStartY = si1.nodeStartY + si1.selfH / 2 - ti.selfH / 2 + si1.sumMaxRowHeight[i] + si1.maxRowHeight[i] / 2 - si1.selfH / 2
+          } else if (isSSC(ti.path)) {
+            ti.nodeStartX = si2.nodeEndX + g.sLineDeltaXDefault - INDENT + 2 + si1.sumMaxColWidth[j]
+            ti.nodeStartY = si1.nodeStartY + si1.selfH / 2 - ti.selfH / 2 + si1.sumMaxRowHeight[i] + si1.maxRowHeight[i] / 2 - si1.selfH / 2
+          } else if (isCSC(ti.path)) {
+            ti.nodeStartX = si2.nodeStartX + 2
+            ti.nodeStartY = si1.nodeStartY + si1.selfH / 2 - ti.selfH / 2 + si1.sumMaxRowHeight[i] + si1.maxRowHeight[i] / 2 - si1.selfH / 2
+          }
         }
         ti.nodeEndX = ti.nodeStartX + ti.selfW
         ti.nodeEndY = ti.nodeStartY + ti.selfH
