89c8e71f4cee69c916b07c7c1181b280c87ac626
Krisztian Szucher <krisztian@szucher.com>
2023-12-28 14:13:23 +0100

    unified boolean notation

--- client-vite/src/reducers/MapMeasure.ts
+++ client-vite/src/reducers/MapMeasure.ts
@@ -22,7 +22,7 @@ export const mapMeasure = (pm: M, m: M) => {
       case isR(ti.path): {
         const countTSO1 = getCountTSO1(m, ti)
         ti.familyW = countTSO1 && Math.max(...getTSO1(m, ti).map(ti => ti.maxW))
-        ti.familyH = countTSO1 && getTSO1(m, ti).reduce((a, b) => a + b.maxH, 0) + ((getCountTSO2(m, ti) || getCountTCO2(m, ti)) ? (countTSO1 - 1) * ti.spacing : 0)
+        ti.familyH = countTSO1 && getTSO1(m, ti).reduce((a, b) => a + b.maxH, 0) + ti.spacing * (countTSO1 - 1) * + Boolean((getCountTSO2(m, ti) || getCountTCO2(m, ti)))
         ti.selfW = ti.familyW + 2 * MARGIN_X + getTaskWidth(getG(m)) * hasTask(m, ti)
         ti.selfH = ti.familyH + 2 * MARGIN_Y
         ti.maxW = ti.selfW
@@ -123,12 +123,12 @@ export const mapMeasure = (pm: M, m: M) => {
         const countTSO1 = getCountTSO1(m, ti)
         if (g.placeType === PlaceType.EXPLODED) {
           ti.familyW = countTSO1 && Math.max(...getTSO1(m, ti).map(ri => ri.maxW)) + g.sLineDeltaXDefault
-          ti.familyH = countTSO1 && getTSO1(m, ti).reduce((a, b) => a + b.maxH, 0) + ((getCountTSO2(m, ti) || getCountTCO2(m, ti)) ? (countTSO1 - 1) * ti.spacing : 0)
+          ti.familyH = countTSO1 && getTSO1(m, ti).reduce((a, b) => a + b.maxH, 0) + ti.spacing * (countTSO1 - 1) * + Boolean((getCountTSO2(m, ti) || getCountTCO2(m, ti)))
           ti.maxW = ti.selfW + ti.familyW
           ti.maxH = Math.max(...[ti.selfH, ti.familyH])
         } else if (g.placeType === PlaceType.INDENTED) {
           ti.familyW = countTSO1 && Math.max(...getTSO1(m, ti).map(ri => ri.maxW)) + INDENT
-          ti.familyH = countTSO1 && getTSO1(m, ti).reduce((a, b) => a + b.maxH, 0) + ((getCountTSO2(m, ti) || getCountTCO2(m, ti)) ? (countTSO1 - 1) * ti.spacing : 0)
+          ti.familyH = countTSO1 && getTSO1(m, ti).reduce((a, b) => a + b.maxH, 0) + ti.spacing * (countTSO1 - 1) * + Boolean((getCountTSO2(m, ti) || getCountTCO2(m, ti)))
           ti.maxW = Math.max(...[ti.selfW, ti.familyW])
           ti.maxH = ti.selfH + ti.familyH
         }
@@ -138,10 +138,10 @@ export const mapMeasure = (pm: M, m: M) => {
         const countTSO1 = getCountTSO1(m, ti)
         if (g.placeType === PlaceType.EXPLODED) {
           ti.familyW = countTSO1 && Math.max(...getTSO1(m, ti).map(ri => ri.maxW)) + g.sLineDeltaXDefault
-          ti.familyH = countTSO1 && getTSO1(m, ti).reduce((a, b) => a + b.maxH, 0) + ((getCountTSO2(m, ti) || getCountTCO2(m, ti)) ? (countTSO1 - 1) * ti.spacing : 0)
+          ti.familyH = countTSO1 && getTSO1(m, ti).reduce((a, b) => a + b.maxH, 0) + ti.spacing * (countTSO1 - 1) * + Boolean((getCountTSO2(m, ti) || getCountTCO2(m, ti)))
         } else if (g.placeType === PlaceType.INDENTED) {
           ti.familyW = countTSO1 && Math.max(...getTSO1(m, ti).map(ri => ri.maxW)) + INDENT
-          ti.familyH = countTSO1 && getTSO1(m, ti).reduce((a, b) => a + b.maxH, 0) + ((getCountTSO2(m, ti) || getCountTCO2(m, ti)) ? (countTSO1 - 1) * ti.spacing : 0)
+          ti.familyH = countTSO1 && getTSO1(m, ti).reduce((a, b) => a + b.maxH, 0) + ti.spacing * (countTSO1 - 1) * + Boolean((getCountTSO2(m, ti) || getCountTCO2(m, ti)))
         }
         ti.maxW = ti.familyW || 60
         ti.maxH = ti.familyH || 30
