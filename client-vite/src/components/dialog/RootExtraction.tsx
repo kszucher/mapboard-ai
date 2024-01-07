07d412e31c709082f6dd441db84c5577bc6013f8
Krisztian Szucher <krisztian@szucher.com>
2024-01-07 22:57:36 +0100

    prevent close on outside click

--- client-vite/src/components/dialog/RootExtraction.tsx
+++ client-vite/src/components/dialog/RootExtraction.tsx
@@ -1,12 +1,17 @@
-import {Dialog} from "@radix-ui/themes"
+import {Button, Dialog} from "@radix-ui/themes"
 
 export const RootExtraction = () => {
   return (
-    <Dialog.Content style={{ maxWidth: 450 }}>
+    <Dialog.Content style={{ maxWidth: 450 }} onInteractOutside={(e) => {e.preventDefault()}}>
       <Dialog.Title>{'EXTRACTION'}</Dialog.Title>
       <Dialog.Description size="2" mb="4">
         {'Extract information in your desired format'}
       </Dialog.Description>
+      <Dialog.Close>
+        <Button variant="soft" color="gray">
+          {'Cancel'}
+        </Button>
+      </Dialog.Close>
     </Dialog.Content>
   )
 }
--- client-vite/src/components/dialog/RootIngestion.tsx
+++ client-vite/src/components/dialog/RootIngestion.tsx
@@ -1,12 +1,17 @@
-import {Dialog} from "@radix-ui/themes"
+import {Button, Dialog} from "@radix-ui/themes"
 
 export const RootIngestion = () => {
   return (
-    <Dialog.Content style={{ maxWidth: 450 }}>
+    <Dialog.Content style={{ maxWidth: 450 }} onInteractOutside={(e) => {e.preventDefault()}}>
       <Dialog.Title>{'INGESTION'}</Dialog.Title>
       <Dialog.Description size="2" mb="4">
         {'Ingest the content of text or audio files'}
       </Dialog.Description>
+      <Dialog.Close>
+        <Button variant="soft" color="gray">
+          {'Cancel'}
+        </Button>
+      </Dialog.Close>
     </Dialog.Content>
   )
 }
