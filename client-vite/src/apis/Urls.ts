fddf08eed62f86cdc87d2e65f067635c44dda16e
krisz <krisztian@szucher.com>
2024-01-25 19:59:26 +0100

    migrated to render free plan

--- client-vite/src/apis/Urls.ts
+++ client-vite/src/apis/Urls.ts
@@ -1,6 +1,6 @@
 export const backendUrl = process.env.NODE_ENV === 'development'
   ? 'http://127.0.0.1:8082'
-  : 'https://mapboard-server.herokuapp.com/'
+  : 'https://mapboard-server.onrender.com'
 
 export const authAudienceUrl = process.env.NODE_ENV === 'development'
   ? 'http://local.mapboard/'
