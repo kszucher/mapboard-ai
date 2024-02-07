58b74b60b2f990e969256d0cc9bcb0ee23ec8fbf
krisz <krisztian@szucher.com>
2024-02-07 15:45:21 +0100

    hash hash hash

--- client-vite/src/reducers/MapCalcTask.ts
+++ client-vite/src/reducers/MapCalcTask.ts
@@ -1,12 +1,13 @@
-import {getNodeByPath, mT} from "../queries/MapQueries.ts"
-import {M} from "../state/MapStateTypes"
+import {mT} from "../queries/MapQueries.ts"
+import {M, T} from "../state/MapStateTypes"
 
 export const mapCalcTask = (m: M) => {
+  const mHashP = new Map<string, T>(m.map(ti => [ti.path.join(''), ti as T]))
   mT(m).toReversed().forEach(ti => {
     if (ti.countTSO1) {
       ti.taskStatus = 4
       for (let i = 0; i < ti.countTSO1; i++) {
-        const cn = getNodeByPath(m, [...ti.path, 's', i])
+        const cn = mHashP.get([...ti.path, 's', i].join(''))!
         ti.taskStatus = cn.taskStatus < ti.taskStatus ? cn.taskStatus : ti.taskStatus
       }
     }
