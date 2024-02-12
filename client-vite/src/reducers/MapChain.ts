989d9db837378eede7dc27ab3b325a53a6bcb560
krisz <krisztian@szucher.com>
2024-02-12 11:02:41 +0100

    no need for pathIndex, expression can be simplified

--- client-vite/src/reducers/MapChain.ts
+++ client-vite/src/reducers/MapChain.ts
@@ -2,9 +2,8 @@ import {M, T} from "../state/MapStateTypes.ts"
 import {mT, isR, isS, isC, isSS, isCS} from "../queries/MapQueries.ts"
 
 export const mapChain = (m: M) => {
-  const mHashN = new Map<string, T>(m.map(ti => [ti.nodeId, ti as T]));
-  const mHashP = new Map<string, T>(m.map(ti => [ti.path.join(''), ti as T]));
-  const pathToIndex = new Map<string, number>(m.map((ti, i) => [ti.path.join(''), i]));
+  const mHashN = new Map<string, T>(m.map(ti => [ti.nodeId, ti as T]))
+  const mHashP = new Map<string, T>(m.map(ti => [ti.path.join(''), ti as T]))
 
   mT(m).forEach(ti => {
     switch (true) {
@@ -12,12 +11,12 @@ export const mapChain = (m: M) => {
         break
       }
       case isS(ti.path): {
-        ti.si1 = mHashP.get(ti.path.slice(0, -2).join(''))!.nodeId;
-        (m.at(pathToIndex.get(ti.path.slice(0, -2).join('')) as number) as T).so1.push(ti.nodeId);
+        ti.si1 = mHashP.get(ti.path.slice(0, -2).join(''))!.nodeId
+        mHashP.get(ti.path.slice(0, -2).join(''))!.so1.push(ti.nodeId)
         if (isSS(ti.path)) {
-          (m.at(pathToIndex.get(ti.path.slice(0, -4).join('')) as number) as T).so2.push(ti.nodeId);
+          mHashP.get(ti.path.slice(0, -4).join(''))!.so2.push(ti.nodeId)
         } else if (isCS(ti.path)) {
-          (m.at(pathToIndex.get(ti.path.slice(0, -5).join('')) as number) as T).so2.push(ti.nodeId);
+          mHashP.get(ti.path.slice(0, -5).join(''))!.so2.push(ti.nodeId)
         }
         for (let i = 0; i < ti.path.at(-1); i++) {
           ti.su.push(mHashP.get([...ti.path.slice(0, -1), i].join(''))!.nodeId)
@@ -25,10 +24,10 @@ export const mapChain = (m: M) => {
         break
       }
       case isC(ti.path): {
-        ti.si1 = mHashP.get(ti.path.slice(0, -3).join(''))!.nodeId;
-        ti.si2 = mHashP.get(ti.path.slice(0, -5).join(''))!.nodeId;
-        (m.at(pathToIndex.get(ti.path.slice(0, -3).join('')) as number) as T).co1.push(ti.nodeId);
-        (m.at(pathToIndex.get(ti.path.slice(0, -5).join('')) as number) as T).co2.push(ti.nodeId);
+        ti.si1 = mHashP.get(ti.path.slice(0, -3).join(''))!.nodeId
+        ti.si2 = mHashP.get(ti.path.slice(0, -5).join(''))!.nodeId
+        mHashP.get(ti.path.slice(0, -3).join(''))!.co1.push(ti.nodeId)
+        mHashP.get(ti.path.slice(0, -5).join(''))!.co2.push(ti.nodeId)
         break
       }
     }
