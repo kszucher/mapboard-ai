a33e06a1ee9cf5682bb572a924d922ab8910d40b
krisz <krisztian@szucher.com>
2024-02-14 18:01:07 +0100

    fix inheritence bug

--- client-vite/src/reducers/MapCalcTask.ts
+++ client-vite/src/reducers/MapCalcTask.ts
@@ -1,9 +1,9 @@
-import {mT} from "../queries/MapQueries.ts"
+import {mTS} from "../queries/MapQueries.ts"
 import {M, T} from "../state/MapStateTypes"
 
 export const mapCalcTask = (m: M) => {
   const mHashP = new Map<string, T>(m.map(ti => [ti.path.join(''), ti as T]))
-  mT(m).toReversed().forEach(ti => {
+  mTS(m).toReversed().forEach(ti => {
     if (ti.so1.length) {
       ti.taskStatus = 4
       for (let i = 0; i < ti.so1.length; i++) {
