282e3c9ffe0a7ec6707c21f9eb2d76dbdd62f4dd
krisz <krisztian@szucher.com>
2024-02-25 17:18:29 +0100

    menu placeholder added

--- client-vite/src/components/map/MapSvgSSelectionPrimary.tsx
+++ client-vite/src/components/map/MapSvgSSelectionPrimary.tsx
@@ -7,6 +7,8 @@ import {defaultUseOpenWorkspaceQueryState} from "../../state/NodeApiState"
 import {mSelector} from "../../state/EditorState"
 import {RootState} from "../../reducers/EditorReducer"
 import {getPolygonPath, pathCommonProps} from "./MapSvgUtils"
+import {DropdownMenu} from "@radix-ui/themes"
+import Dots from "../../assets/dots.svg?react"
 
 export const MapSvgSSelectionPrimary: FC = () => {
   const m = useSelector((state:RootState) => mSelector(state))
@@ -25,7 +27,7 @@ export const MapSvgSSelectionPrimary: FC = () => {
           stroke={C.SELECTION_COLOR}
           strokeWidth={1}
           fill={'none'}{...pathCommonProps}
-          d={getPolygonPath(m, xs, 'sSelf', 4)}
+          d={getPolygonPath(m, xs, 'sSelf', 2)}
         />
       }
       {getXA(m).length === 1 && xs.selection === 's' && !((xs.sBorderColor || xs.sFillColor) || xs.taskStatus > 1 || xs.co1.length) &&
@@ -45,6 +47,27 @@ export const MapSvgSSelectionPrimary: FC = () => {
           fill={'none'}{...pathCommonProps}
           d={getPolygonPath(m, xs, 'sFamily', 4)}
         />
+      }
+      {
+        getXA(m).length === 1 &&
+        <DropdownMenu.Root>
+          <g
+            transform={`translate(${Math.round(xs.nodeStartX + xs.selfW + 12)}, ${Math.round(xs.nodeStartY + xs.selfH / 2 - 12)})`}
+            style={{transition: 'all 0.3s', transitionTimingFunction: 'cubic-bezier(0.0,0.0,0.58,1.0)', transitionProperty: 'all'}}>
+            <DropdownMenu.Trigger>
+              <rect width={24} height={24} rx={20} ry={20} className={"fill-gray-500 hover:fill-teal-700"}/>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+              <DropdownMenu.Item onClick={() => console.log('x')}>
+                {'Menu'}
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+            <g className={"pointer-events-none"}>
+              <Dots/>
+            </g>
+          </g>
+          )
+        </DropdownMenu.Root>
       }
     </Fragment>
   )
