3ffe82f05f7a3cbc4bf9b84f8d61335ff93562ac
krisz <krisztian@szucher.com>
2024-03-09 13:25:46 +0100

    declarative delete

--- client-vite/src/reducers/MapDelete.ts
+++ client-vite/src/reducers/MapDelete.ts
@@ -1,5 +1,5 @@
 import {L, M, PL, PR, PS, PC} from "../state/MapStateTypes"
-import {isRDO, mG, mL, mR, mS, mC, isREO, isCEO, pathToS, idToS, sortPath, getXAS, getXAC, getXAR, getXC, idToC} from "../queries/MapQueries.ts"
+import {isRDO, mG, mL, mR, mS, mC, isREO, pathToS, idToS, sortPath, getXAS, getXAC, getXAR, getXC, idToC} from "../queries/MapQueries.ts"
 
 export const deleteL = (m: M, l: L) => {
   m.splice(0, m.length, ...[
@@ -38,9 +38,8 @@ export const deleteLR = (m: M) => {
 }
 
 export const deleteS = (m: M) => {
-  const xas = getXAS(m)
-  const xaso = xas.flatMap(si => si.so)
-  const xaco = xas.flatMap(si => si.co)
+  const xaso = getXAS(m).flatMap(si => si.so)
+  const xaco = getXAS(m).flatMap(si => si.co)
   m.splice(0, m.length, ...[...mG(m), ...mL(m), ...mR(m), ...mS(m).filter(si => !xaso.includes(si.nodeId)), ...mC(m).filter(si => !xaco.includes(si.nodeId))])
   mS(m).forEach(si => si.path.forEach((pi, i) => si.path.at(i - 1) === 's' && si.path.splice(i, 1, pi - pathToS(m, si.path.slice(0, i + 1) as PS)?.su.map(nid => idToS(m, nid)).filter(si => si.selected).length || 0)))
   mC(m).forEach(ci => ci.path.forEach((pi, i) => ci.path.at(i - 1) === 's' && ci.path.splice(i, 1, pi - pathToS(m, ci.path.slice(0, i + 1) as PS)?.su.map(nid => idToS(m, nid)).filter(si => si.selected).length || 0)))
@@ -49,21 +48,25 @@ export const deleteS = (m: M) => {
 }
 
 export const deleteCR = (m: M) => {
-  const xac = getXAC(m)
-  const cd = getXAC(m).flatMap(ci => ci.cd)
+  const xac = getXAC(m).map(ci => ci.nodeId)
+  const xacso = getXAC(m).flatMap(si => si.so)
+  const xacd = getXAC(m).flatMap(ci => ci.cd)
+  const xacdso = getXAC(m).flatMap(ci => ci.cd).map(nid => idToC(m, nid)).flatMap(ci => ci.so)
   const crIndex = getXC(m).path.indexOf('c') + 1
-  m.splice(0, m.length, ...[...mG(m), ...mL(m), ...mR(m), ...mS(m).filter(si => xac.every(xti => !isCEO(xti.path, si.path))),...mC(m).filter(ci => xac.every(xti => !isCEO(xti.path, ci.path)))])
-  cd.map(nid => idToC(m, nid)).map(ci => ci.nodeId).map(nid => idToC(m, nid)).forEach(ci => ci.path.splice(crIndex, 1, ci.path.at(crIndex) - 1))
-  cd.map(nid => idToC(m, nid)).flatMap(ci => ci.so).map(nid => idToS(m, nid)).forEach(si => si.path.splice(crIndex, 1, si.path.at(crIndex) - 1))
+  m.splice(0, m.length, ...[...mG(m), ...mL(m), ...mR(m), ...mS(m).filter(si => !xacso.includes(si.nodeId)), ...mC(m).filter(ci => !xac.includes(ci.nodeId))])
+  xacd.map(nid => idToC(m, nid)).forEach(ci => ci.path.splice(crIndex, 1, ci.path.at(crIndex) - 1))
+  xacdso.map(nid => idToS(m, nid)).forEach(si => si?.path.splice(crIndex, 1, si.path.at(crIndex) - 1))
   m.sort(sortPath)
 }
 
 export const deleteCC = (m: M) => {
-  const xac = getXAC(m)
-  const cd = getXAC(m).flatMap(ci => ci.cr)
+  const xac = getXAC(m).map(ci => ci.nodeId)
+  const xacso = getXAC(m).flatMap(si => si.so)
+  const xacr = getXAC(m).flatMap(ci => ci.cr)
+  const xacrso = getXAC(m).flatMap(ci => ci.cr).map(nid => idToC(m, nid)).flatMap(ci => ci.so)
   const ccIndex = getXC(m).path.indexOf('c') + 2
-  m.splice(0, m.length, ...[...mG(m), ...mL(m), ...mR(m), ...mS(m).filter(si => xac.every(xti => !isCEO(xti.path, si.path))), ...mC(m).filter(ci => xac.every(xti => !isCEO(xti.path, ci.path)))])
-  cd.map(nid => idToC(m, nid)).map(ci => ci.nodeId).map(nid => idToC(m, nid)).forEach(ci => ci.path.splice(ccIndex, 1, ci.path.at(ccIndex) - 1))
-  cd.map(nid => idToC(m, nid)).flatMap(ci => ci.so).map(nid => idToS(m, nid)).forEach(si => si.path.splice(ccIndex, 1, si.path.at(ccIndex) - 1))
+  m.splice(0, m.length, ...[...mG(m), ...mL(m), ...mR(m), ...mS(m).filter(si => !xacso.includes(si.nodeId)), ...mC(m).filter(ci => !xac.includes(ci.nodeId))])
+  xacr.map(nid => idToC(m, nid)).forEach(ci => ci.path.splice(ccIndex, 1, ci.path.at(ccIndex) - 1))
+  xacrso.map(nid => idToS(m, nid)).forEach(si => si?.path.splice(ccIndex, 1, si.path.at(ccIndex) - 1))
   m.sort(sortPath)
 }
