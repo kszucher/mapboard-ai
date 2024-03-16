c3b2842dd4b959da345836f86d04ae885920fe1a
krisz <krisztian@szucher.com>
2024-03-16 15:17:55 +0100

    mapProcess finally

--- client-vite/src/queries/MapProcess.test.ts
+++ client-vite/src/queries/MapProcess.test.ts
@@ -6,105 +6,105 @@ import {ReadableTree, SubProcess} from "./MapProcessTypes.ts"
 
 describe("MapProcessTests", () => {
   test('getSubProcessList', () => {
-      //      [ta]
+      //      [r0]
       //         \
-      //          [tb]
+      //          [r1]
       //             \
-      //      [td]     [tc]
+      //      [r3]     [r2]
       //         \   /
-      //          [te]
+      //          [r4]
       //         /
-      //      [tg]
+      //      [r6]
       //     /
-      //  [tf]
+      //  [r5]
       const test  = [
-        {nodeId: 'ga', path: ['g']},
-        {nodeId: 'la', path: ['l', 0], fromNodeId: 'ta', fromNodeSide: Side.R, toNodeId: 'tb', toNodeSide: Side.L},
-        {nodeId: 'lb', path: ['l', 1], fromNodeId: 'tb', fromNodeSide: Side.R, toNodeId: 'tc', toNodeSide: Side.L},
-        {nodeId: 'lc', path: ['l', 2], fromNodeId: 'td', fromNodeSide: Side.R, toNodeId: 'te', toNodeSide: Side.L},
-        {nodeId: 'ld', path: ['l', 3], fromNodeId: 'te', fromNodeSide: Side.R, toNodeId: 'tc', toNodeSide: Side.L},
-        {nodeId: 'le', path: ['l', 4], fromNodeId: 'tf', fromNodeSide: Side.R, toNodeId: 'tg', toNodeSide: Side.L},
-        {nodeId: 'lf', path: ['l', 5], fromNodeId: 'tg', fromNodeSide: Side.R, toNodeId: 'te', toNodeSide: Side.L},
-        {nodeId: 'ta', path: ['r', 0], controlType: ControlType.INGESTION, ingestionHash: 'hHash', selected: 1},
-        {nodeId: 'tas', path: ['r', 0, 's', 0], content: 'tas'},
-        {nodeId: 'tb', path: ['r', 1], controlType: ControlType.EXTRACTION},
-        {nodeId: 'tbs', path: ['r', 1, 's', 0], content: 'tbs'},
-        {nodeId: 'tc', path: ['r', 2], controlType: ControlType.EXTRACTION},
-        {nodeId: 'tcs', path: ['r', 2, 's', 0], content: 'tcs'},
-        {nodeId: 'td', path: ['r', 3], controlType: ControlType.INGESTION, ingestionHash: 'kHash'},
-        {nodeId: 'tds', path: ['r', 3, 's', 0], content: 'tds'},
-        {nodeId: 'te', path: ['r', 4], controlType: ControlType.EXTRACTION},
-        {nodeId: 'tes', path: ['r', 4, 's', 0], content: 'tes'},
-        {nodeId: 'tf', path: ['r', 5], controlType: ControlType.INGESTION, ingestionHash: 'mHash'},
-        {nodeId: 'tfs', path: ['r', 5, 's', 0], content: 'tfs'},
-        {nodeId: 'tg', path: ['r', 6], controlType: ControlType.EXTRACTION},
-        {nodeId: 'tgs', path: ['r', 6, 's', 0], content: 'tgs'},
+        {nodeId: 'g', path: ['g']},
+        {nodeId: 'l0', path: ['l', 0], fromNodeId: 'r0', fromNodeSide: Side.R, toNodeId: 'r1', toNodeSide: Side.L},
+        {nodeId: 'l1', path: ['l', 1], fromNodeId: 'r1', fromNodeSide: Side.R, toNodeId: 'r2', toNodeSide: Side.L},
+        {nodeId: 'l2', path: ['l', 2], fromNodeId: 'r3', fromNodeSide: Side.R, toNodeId: 'r4', toNodeSide: Side.L},
+        {nodeId: 'l3', path: ['l', 3], fromNodeId: 'r4', fromNodeSide: Side.R, toNodeId: 'r2', toNodeSide: Side.L},
+        {nodeId: 'l4', path: ['l', 4], fromNodeId: 'r5', fromNodeSide: Side.R, toNodeId: 'r6', toNodeSide: Side.L},
+        {nodeId: 'l5', path: ['l', 5], fromNodeId: 'r6', fromNodeSide: Side.R, toNodeId: 'r4', toNodeSide: Side.L},
+        {nodeId: 'r0', path: ['r', 0], controlType: ControlType.INGESTION, ingestionHash: 'hHash', selected: 1},
+        {nodeId: 'r0s0', path: ['r', 0, 's', 0], content: 'r0s0'},
+        {nodeId: 'r1', path: ['r', 1], controlType: ControlType.EXTRACTION},
+        {nodeId: 'r1s0', path: ['r', 1, 's', 0], content: 'r1s0'},
+        {nodeId: 'r2', path: ['r', 2], controlType: ControlType.EXTRACTION},
+        {nodeId: 'r2s0', path: ['r', 2, 's', 0], content: 'r2s0'},
+        {nodeId: 'r3', path: ['r', 3], controlType: ControlType.INGESTION, ingestionHash: 'kHash'},
+        {nodeId: 'r3s0', path: ['r', 3, 's', 0], content: 'r3s0'},
+        {nodeId: 'r4', path: ['r', 4], controlType: ControlType.EXTRACTION},
+        {nodeId: 'r4s0', path: ['r', 4, 's', 0], content: 'r4s0'},
+        {nodeId: 'r5', path: ['r', 5], controlType: ControlType.INGESTION, ingestionHash: 'mHash'},
+        {nodeId: 'r5s0', path: ['r', 5, 's', 0], content: 'r5s0'},
+        {nodeId: 'r6', path: ['r', 6], controlType: ControlType.EXTRACTION},
+        {nodeId: 'r6s0', path: ['r', 6, 's', 0], content: 'r6s0'},
       ] as MPartial
       const result = [{
-          subProcessId: 'tf',
+          subProcessId: 'r5',
           subProcessType: SubProcessType.INGESTION,
-          subProcessMindMapData: [{nodeId: 'tfs', contentList: ['tfs']}] as ReadableTree,
+          subProcessMindMapData: [{nodeId: 'r5s0', contentList: ['r5s0']}] as ReadableTree,
           inputSubProcesses: [],
           inputSubProcessesAll: [],
           subProcessInputLink: 'mHash',
           shouldQueryAndStoreResultAsMindMapToo: false,
           subProcessPromptOverride: ''
         }, {
-          subProcessId: 'tg',
+          subProcessId: 'r6',
           subProcessType: SubProcessType.EXTRACTION,
-          subProcessMindMapData: [{nodeId: 'tgs', contentList: ['tgs']}] as ReadableTree,
-          inputSubProcesses: ['tf'],
-          inputSubProcessesAll: ['tf'],
+          subProcessMindMapData: [{nodeId: 'r6s0', contentList: ['r6s0']}] as ReadableTree,
+          inputSubProcesses: ['r5'],
+          inputSubProcessesAll: ['r5'],
           subProcessInputLink: '',
           shouldQueryAndStoreResultAsMindMapToo: false,
           subProcessPromptOverride: ''
         }, {
-          subProcessId: 'td',
+          subProcessId: 'r3',
           subProcessType: SubProcessType.INGESTION,
-          subProcessMindMapData: [{nodeId: 'tds', contentList: ['tds']}] as ReadableTree,
+          subProcessMindMapData: [{nodeId: 'r3s0', contentList: ['r3s0']}] as ReadableTree,
           inputSubProcesses: [],
           inputSubProcessesAll: [],
           subProcessInputLink: 'kHash',
           shouldQueryAndStoreResultAsMindMapToo: false,
           subProcessPromptOverride: ''
         }, {
-          subProcessId: 'te',
+          subProcessId: 'r4',
           subProcessType: SubProcessType.EXTRACTION,
-          subProcessMindMapData: [{nodeId: 'tes', contentList: ['tes']}] as ReadableTree,
-          inputSubProcesses: ['td', 'tg'],
-          inputSubProcessesAll: ['td', 'tg', 'tf'],
+          subProcessMindMapData: [{nodeId: 'r4s0', contentList: ['r4s0']}] as ReadableTree,
+          inputSubProcesses: ['r3', 'r6'],
+          inputSubProcessesAll: ['r3', 'r6', 'r5'],
           subProcessInputLink: '',
           shouldQueryAndStoreResultAsMindMapToo: false,
           subProcessPromptOverride: ''
         }, {
-          subProcessId: 'ta',
+          subProcessId: 'r0',
           subProcessType: SubProcessType.INGESTION,
-          subProcessMindMapData: [{nodeId: 'tas', contentList: ['tas']}] as ReadableTree,
+          subProcessMindMapData: [{nodeId: 'r0s0', contentList: ['r0s0']}] as ReadableTree,
           inputSubProcesses: [],
           inputSubProcessesAll: [],
           subProcessInputLink: 'hHash',
           shouldQueryAndStoreResultAsMindMapToo: false,
           subProcessPromptOverride: ''
         }, {
-          subProcessId: 'tb',
+          subProcessId: 'r1',
           subProcessType: SubProcessType.EXTRACTION,
-          subProcessMindMapData: [{nodeId: 'tbs', contentList: ['tbs']}] as ReadableTree,
-          inputSubProcesses: ['ta'],
-          inputSubProcessesAll: ['ta'],
+          subProcessMindMapData: [{nodeId: 'r1s0', contentList: ['r1s0']}] as ReadableTree,
+          inputSubProcesses: ['r0'],
+          inputSubProcessesAll: ['r0'],
           subProcessInputLink: '',
           shouldQueryAndStoreResultAsMindMapToo: false,
           subProcessPromptOverride: ''
         }, {
-          subProcessId: 'tc',
+          subProcessId: 'r2',
           subProcessType: SubProcessType.EXTRACTION,
-          subProcessMindMapData: [{nodeId: 'tcs', contentList: ['tcs']}] as ReadableTree,
-          inputSubProcesses: ['tb', 'te'],
-          inputSubProcessesAll: ['tb', 'te', 'ta', 'td', 'tg', 'tf'],
+          subProcessMindMapData: [{nodeId: 'r2s0', contentList: ['r2s0']}] as ReadableTree,
+          inputSubProcesses: ['r1', 'r4'],
+          inputSubProcessesAll: ['r1', 'r4', 'r0', 'r3', 'r6', 'r5'],
           subProcessInputLink: '',
           shouldQueryAndStoreResultAsMindMapToo: false,
           subProcessPromptOverride: ''
         }] as SubProcess[]
-      expect(getSubProcessList(mapInit(test), 'tc')).toEqual(result)
+      expect(getSubProcessList(mapInit(test), 'r2')).toEqual(result)
     }
   )
 })
