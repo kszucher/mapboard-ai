2425b53c2b7fa23b810dea350075652cba4d0746
krisz <krisztian@szucher.com>
2024-04-21 15:34:15 +0200

    fix css

--- client-vite/src/components/editor/EditorAppBarLeft.tsx
+++ client-vite/src/components/editor/EditorAppBarLeft.tsx
@@ -3,7 +3,7 @@ import TopologyStar from "../../assets/topology-star.svg?react"
 
 export const EditorAppBarLeft: FC = () => {
   return (
-    <div className="fixed top-0 w-[192px] h-[32px] py-1 flex items-center justify-center text-white z-50 gap-2 bg-gradient-to-r from-violet-700 to-violet-700 rounded-r-2xl">
+    <div className="fixed box-border top-0 w-[192px] h-[40px] py-1 flex items-center justify-center text-white z-50 gap-2 bg-gradient-to-r from-violet-700 to-violet-700 rounded-r-2xl">
       <TopologyStar/>
       <div style={{fontFamily: "Comfortaa"}} className="text-xl ">
         {'mapboard'}
