81aa7b803463049fe21ec1e300c656a92b814c47
krisz <krisztian@szucher.com>
2024-07-23 15:18:19 +0200

    simplify unnecessary double maps

--- client-vite/src/reducers/MapInsert.ts
+++ client-vite/src/reducers/MapInsert.ts
@@ -36,8 +36,8 @@ export const insertCRD = (m: M) => {
   const cd = getXAC(m).flatMap(ci => ci.cd).map(nid => idToC(m, nid))
   cd.flatMap(ci => ci.so).map(nid => idToS(m, nid)).forEach(si => si.path.splice(crIndex, 1, si.path.at(crIndex) + 1))
   cd.map(ci => ci.nodeId).map(nid => idToC(m, nid)).forEach(ci => ci.path.splice(crIndex, 1, ci.path.at(crIndex) + 1))
-  m.push(...getXAC(m).map(ci => ci.path.with(crIndex, crValue + 1) as PC).map(el => genNodeC(el)))
-  m.push(...getXAC(m).map(ci => [...ci.path.with(crIndex, crValue + 1), 's', 0] as PS).map(el => genNodeS(el)))
+  m.push(...getXAC(m).map(ci => genNodeC(ci.path.with(crIndex, crValue + 1) as PC)))
+  m.push(...getXAC(m).map(ci => genNodeS([...ci.path.with(crIndex, crValue + 1), 's', 0] as PS)))
   m.sort(sortPath)
 }
 
@@ -47,8 +47,8 @@ export const insertCRU = (m: M) => {
   const ced = getXAC(m).flatMap(ci => [ci.nodeId, ...ci.cd]).map(nid => idToC(m, nid))
   ced.flatMap(ci => ci.so).map(nid => idToS(m, nid)).forEach(si => si.path.splice(crIndex, 1, si.path.at(crIndex) + 1))
   ced.map(ci => ci.nodeId).map(nid => idToC(m, nid)).forEach(ci => ci.path.splice(crIndex, 1, ci.path.at(crIndex) + 1))
-  m.push(...getXAC(m).map(ci => ci.path.with(crIndex, crValue) as PC).map(el => genNodeC(el)))
-  m.push(...getXAC(m).map(ci => [...ci.path.with(crIndex, crValue), 's', 0] as PS).map(el => genNodeS(el)))
+  m.push(...getXAC(m).map(ci => genNodeC(ci.path.with(crIndex, crValue) as PC)))
+  m.push(...getXAC(m).map(ci => genNodeS([...ci.path.with(crIndex, crValue), 's', 0] as PS)))
   m.sort(sortPath)
 }
 
@@ -58,8 +58,8 @@ export const insertCCR = (m: M) => {
   const cr = getXAC(m).flatMap(ci => ci.cr).map(nid => idToC(m, nid))
   cr.flatMap(ci => ci.so).map(nid => idToS(m, nid)).forEach(si => si.path.splice(ccIndex, 1, si.path.at(ccIndex) + 1))
   cr.map(ci => ci.nodeId).map(nid => idToC(m, nid)).forEach(ci => ci.path.splice(ccIndex, 1, ci.path.at(ccIndex) + 1))
-  m.push(...getXAC(m).map(ci => ci.path.with(ccIndex, ccValue + 1) as PC).map(el => genNodeC(el)))
-  m.push(...getXAC(m).map(ci => [...ci.path.with(ccIndex, ccValue + 1), 's', 0] as PS).map(el => genNodeS(el)))
+  m.push(...getXAC(m).map(ci => genNodeC(ci.path.with(ccIndex, ccValue + 1) as PC)))
+  m.push(...getXAC(m).map(ci => genNodeS([...ci.path.with(ccIndex, ccValue + 1), 's', 0] as PS)))
   m.sort(sortPath)
 }
 
@@ -69,15 +69,15 @@ export const insertCCL = (m: M) => {
   const cer = getXAC(m).flatMap(ci => [ci.nodeId, ...ci.cr]).map(nid => idToC(m, nid))
   cer.flatMap(ci => ci.so).map(nid => idToS(m, nid)).forEach(si => si.path.splice(ccIndex, 1, si.path.at(ccIndex) + 1))
   cer.map(ci => ci.nodeId).map(nid => idToC(m, nid)).forEach(ci => ci.path.splice(ccIndex, 1, ci.path.at(ccIndex) + 1))
-  m.push(...getXAC(m).map(ci => ci.path.with(ccIndex, ccValue) as PC).map(el => genNodeC(el)))
-  m.push(...getXAC(m).map(ci => [...ci.path.with(ccIndex, ccValue), 's', 0] as PS).map(el => genNodeS(el)))
+  m.push(...getXAC(m).map(ci => genNodeC(ci.path.with(ccIndex, ccValue) as PC)))
+  m.push(...getXAC(m).map(ci => genNodeS([...ci.path.with(ccIndex, ccValue), 's', 0] as PS)))
   m.sort(sortPath)
 }
 
 export const insertSCRD = (m: M) => {
   const colIndices = Array.from({length: getXS(m).colCount}, (_, i) => i)
-  m.push(...colIndices.map(i => [...getXS(m).path, 'c', getXS(m).rowCount, i] as PC).map(el => genNodeC(el)))
-  m.push(...colIndices.map(i => [...getXS(m).path, 'c', getXS(m).rowCount, i, 's', 0] as PS).map(el => genNodeS(el)))
+  m.push(...colIndices.map(i => genNodeC([...getXS(m).path, 'c', getXS(m).rowCount, i] as PC)))
+  m.push(...colIndices.map(i => genNodeS([...getXS(m).path, 'c', getXS(m).rowCount, i, 's', 0] as PS)))
   m.sort(sortPath)
 }
 
@@ -86,15 +86,15 @@ export const insertSCRU = (m: M) => {
   const colIndices = Array.from({length: getXS(m).colCount}, (_, i) => i)
   getXS(m).co1.map(nid => idToC(m, nid)).forEach(ci => ci.path.splice(crIndex, 1, ci.path.at(crIndex) + 1))
   getXS(m).co1.map(nid => idToC(m, nid)).flatMap(el => el.so).map(nid => idToS(m, nid)).forEach(si => si.path.splice(crIndex, 1, si.path.at(crIndex) + 1))
-  m.push(...colIndices.map(i => [...getXS(m).path, 'c', 0, i] as PC).map(el => genNodeC(el)))
-  m.push(...colIndices.map(i => [...getXS(m).path, 'c', 0, i, 's', 0] as PS).map(el => genNodeS(el)))
+  m.push(...colIndices.map(i => genNodeC([...getXS(m).path, 'c', 0, i] as PC)))
+  m.push(...colIndices.map(i => genNodeS([...getXS(m).path, 'c', 0, i, 's', 0] as PS)))
   m.sort(sortPath)
 }
 
 export const insertSCCR = (m: M) => {
   const rowIndices = Array.from({length: getXS(m).rowCount}, (_, i) => i)
-  m.push(...rowIndices.map(i => [...getXS(m).path, 'c', i, getXS(m).colCount] as PC).map(el => genNodeC(el)))
-  m.push(...rowIndices.map(i => [...getXS(m).path, 'c', i, getXS(m).colCount, 's', 0] as PS).map(el => genNodeS(el)))
+  m.push(...rowIndices.map(i => genNodeC([...getXS(m).path, 'c', i, getXS(m).colCount] as PC)))
+  m.push(...rowIndices.map(i => genNodeS([...getXS(m).path, 'c', i, getXS(m).colCount, 's', 0] as PS)))
   m.sort(sortPath)
 }
 
@@ -103,8 +103,8 @@ export const insertSCCL = (m: M) => {
   const rowIndices = Array.from({length: getXS(m).rowCount}, (_, i) => i)
   getXS(m).co1.map(nid => idToC(m, nid)).forEach(ci => ci.path.splice(ccIndex, 1, ci.path.at(ccIndex) + 1))
   getXS(m).co1.map(nid => idToC(m, nid)).flatMap(el => el.so).map(nid => idToS(m, nid)).forEach(si => si.path.splice(ccIndex, 1, si.path.at(ccIndex) + 1))
-  m.push(...rowIndices.map(i => [...getXS(m).path, 'c', i, 0] as PC).map(el => genNodeC(el)))
-  m.push(...rowIndices.map(i => [...getXS(m).path, 'c', i, 0, 's', 0] as PS).map(el => genNodeS(el)))
+  m.push(...rowIndices.map(i => genNodeC([...getXS(m).path, 'c', i, 0] as PC)))
+  m.push(...rowIndices.map(i => genNodeS([...getXS(m).path, 'c', i, 0, 's', 0] as PS)))
   m.sort(sortPath)
 }
 
@@ -114,7 +114,7 @@ export const insertTable = (m: M, ip: PS, payload: {rowLen: number, colLen: numb
   mC(m).forEach(ci => isCEODO(ip, ci.path) && ci.path.splice(ip.length - 1, 1, ci.path.at(ip.length - 1) + 1))
   unselectNodes(m)
   m.push(genNodeS(ip, {selected: 1}))
-  m.push(...tableIndices.map(el => [...ip, 'c', ...el] as PC).map(el => genNodeC(el)))
-  m.push(...tableIndices.map(el => [...ip, 'c', ...el, 's', 0] as PS).map(el => genNodeS(el)))
+  m.push(...tableIndices.map(el => genNodeC([...ip, 'c', ...el] as PC)))
+  m.push(...tableIndices.map(el => genNodeS([...ip, 'c', ...el, 's', 0] as PS)))
   m.sort(sortPath)
 }
