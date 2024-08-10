3fce9ad04a562332b12d5bbd41cc5c8fe512584a
krisz <krisztian@szucher.com>
2024-08-10 17:23:34 +0200

    symmetry

--- client-vite/src/mapMutations/MapMove.ts
+++ client-vite/src/mapMutations/MapMove.ts
@@ -57,6 +57,7 @@ const clipboardToLRSC = (m: M, cbL: L[], cbRR: R[], cbRS: S[], cbRC: C[]) => {
 }
 
 const clipboardToSC = (m: M, cbSS: S[], cbSC: C[], ip: PS) => {
+  const offset = getXAS(m).length
   cbSS.forEach(si => Object.assign(si, {
     nodeId: genId(),
     path: [...ip.slice(0, -2), 's', ip.at(-1) + si.path.at(1), ...si.path.slice(2)]
@@ -65,7 +66,7 @@ const clipboardToSC = (m: M, cbSS: S[], cbSC: C[], ip: PS) => {
     nodeId: genId(),
     path: [...ip.slice(0, -2), 's', ip.at(-1) + ci.path.at(1), ...ci.path.slice(2)]
   }))
-  offsetSC(m, ip, getXAS(m).length)
+  offsetSC(m, ip, offset)
   unselectNodes(m)
   m.push(...cbSS, ...cbSC)
   m.sort(sortPath)
