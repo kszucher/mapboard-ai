5a074501785b4aca412c8bc704ac3cd5e8f1abd6
krisz <krisztian@szucher.com>
2024-09-02 14:29:10 +0200

    any is needed anyway

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,4 +1,4 @@
-export const isArrayOfEqualValues = (array: []) => new Set(array).size === 1
+export const isArrayOfEqualValues = (array: any[]) => new Set(array).size === 1
 export const isUrl = (string: string) => { try { return Boolean(new URL(string)) } catch(e) { return false } }
 export const genId = () => crypto.randomUUID().slice(-8)
 export const getLatexString = (s: string) => '\\Large ' + s.substring(2, s.length - 2).replace(/\s/g, '')
