73483b8ed137edab3ee5d8d0de234cb62b8b15fc
krisz <krisztian@szucher.com>
2024-09-18 17:17:01 +0200

    select something when a map is deleted

--- src/apiQueries/ApiQueries.ts
+++ src/apiQueries/ApiQueries.ts
@@ -1,14 +1,15 @@
 import {DefaultGetIngestionQueryState, DefaultGetLatestMergedQueryState, DefaultUseOpenWorkspaceQueryState} from "../apiState/ApiStateTypes.ts"
 import {BaseQueryFn, EndpointBuilder} from "@reduxjs/toolkit/query"
+import {api} from "../api/Api.ts"
 
 export const apiQueries = (builder: EndpointBuilder<BaseQueryFn, string, string>) => ({
   openWorkspace: builder.query<DefaultUseOpenWorkspaceQueryState, void>({
     query: () => ({ url: 'open-workspace', method: 'POST' }),
-    async onQueryStarted(_, { queryFulfilled }) {
+    async onQueryStarted(_, { dispatch, queryFulfilled }) {
       try {
         await queryFulfilled
       } catch {
-        console.warn('openWorkspace fail')
+        dispatch(api.endpoints.selectMap.initiate({ mapId: ''}))
       }
     },
     providesTags: ['Workspace']
