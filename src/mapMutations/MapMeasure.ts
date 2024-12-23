05f5c993c80d745032e246a3d915751d6c93a980
krisz <krisztian@szucher.com>
2024-12-23 17:31:20 +0100

    simplify

--- src/mapMutations/MapMeasure.ts
+++ src/mapMutations/MapMeasure.ts
@@ -1,44 +1,34 @@
 import { M_PADDING, R_PADDING } from '../mapConsts/MapConsts.ts';
-import { mR } from '../mapQueries/MapQueries.ts';
-import { isG, isR } from '../mapQueries/PathQueries.ts';
-import { G, M, R } from '../mapState/MapStateTypes.ts';
+import { getG, mR } from '../mapQueries/MapQueries.ts';
+import { M } from '../mapState/MapStateTypes.ts';
 import { ControlType } from '../mapState/MapStateTypesEnums.ts';
 
 export const mapMeasure = (m: M) => {
   const minOffsetW = Math.min(...mR(m).map(ri => ri.offsetW));
   const minOffsetH = Math.min(...mR(m).map(ri => ri.offsetH));
-  mR(m).map(ri =>
+  mR(m).forEach(ri => {
     Object.assign(ri, {
       offsetW: ri.offsetW - minOffsetW,
       offsetH: ri.offsetH - minOffsetH,
-    })
-  );
-  m.slice()
-    .reverse()
-    .forEach(ni => {
-      switch (true) {
-        case isG(ni.path): {
-          const g = <G>ni;
-          g.selfW = Math.max(...mR(m).map(ri => ri.offsetW + ri.selfW)) + 2 * M_PADDING;
-          g.selfH = Math.max(...mR(m).map(ri => ri.offsetH + ri.selfH)) + 2 * M_PADDING;
-          break;
-        }
-        case isR(ni.path): {
-          const ri = <R>ni;
-          if (ri.controlType === ControlType.FILE) {
-            ri.selfW = 160;
-            ri.selfH = 90;
-          } else if (ri.controlType === ControlType.INGESTION) {
-            ri.selfW = 160;
-            ri.selfH = 90;
-          } else if (ri.controlType === ControlType.EXTRACTION) {
-            ri.selfW = 200;
-            ri.selfH = 150;
-          }
-          ri.selfW += 2 * R_PADDING;
-          ri.selfH += 2 * R_PADDING;
-          break;
-        }
-      }
     });
+    switch (ri.controlType) {
+      case ControlType.FILE:
+        ri.selfW = 160;
+        ri.selfH = 90;
+        break;
+      case ControlType.INGESTION:
+        ri.selfW = 160;
+        ri.selfH = 90;
+        break;
+      case ControlType.EXTRACTION:
+        ri.selfW = 200;
+        ri.selfH = 150;
+        break;
+    }
+    ri.selfW += 2 * R_PADDING;
+    ri.selfH += 2 * R_PADDING;
+    const g = getG(m);
+    g.selfW = Math.max(...mR(m).map(ri => ri.offsetW + ri.selfW)) + 2 * M_PADDING;
+    g.selfH = Math.max(...mR(m).map(ri => ri.offsetH + ri.selfH)) + 2 * M_PADDING;
+  });
 };
