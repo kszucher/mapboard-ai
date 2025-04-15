f95951d8d59d0d9519b3c2e3c6433d07c3f03ce9
krisz <krisztian@szucher.com>
2025-04-15 18:55:12 +0200

    more queries added

--- server/src/map-service/map.service.ts
+++ server/src/map-service/map.service.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-import { MapInfoDefaultState } from '../../../shared/types/api-state-types';
+import { MapInfoDefaultState, UserInfoDefaultState } from '../../../shared/types/api-state-types';
 
 export class MapService {
   constructor(private prisma: PrismaClient) {
   }
 
   async getMapInfo({ userId, workspaceId }: { userId: number, workspaceId: number }): Promise<MapInfoDefaultState> {
-    const map = await this.prisma.workspace.findFirst({
+    const workspace = await this.prisma.workspace.findFirst({
       where: { id: workspaceId },
       select: {
         Map: {
@@ -20,9 +20,45 @@ export class MapService {
     });
 
     return {
-      mapId: map.id,
-      mapName: map.name,
-      mapData: map.mapData,
+      mapId: workspace.Map.id,
+      mapName: workspace.Map.name,
+      mapData: workspace.Map.mapData,
+    };
+  }
+
+  async getUserInfo({ workspaceId }: { workspaceId: number }): Promise<UserInfoDefaultState> {
+    const workspace = await this.prisma.workspace.findFirst({
+      where: { id: workspaceId },
+      select: {
+        User: {
+          select: {
+            name: true,
+            colorMode: true,
+            Tab: {
+              select: {
+                mapIds: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const mapIds = workspace.User.Tab.mapIds;
+
+    const tabMaps = await this.prisma.map.findMany({
+      where: { id: { in: mapIds } },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+
+    return {
+      userName: workspace.User.name,
+      colorMode: workspace.User.colorMode,
+      tabMapIdList: tabMaps.map(el => el.id),
+      tabMapNameList: tabMaps.map(el => el.name),
     };
   }
 
