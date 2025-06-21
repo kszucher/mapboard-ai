51d0de104016b8a1f54506e799a48951014ae4f2
krisz <krisztian@szucher.com>
2025-06-21 12:53:02 +0200

    simplify

--- client/src/components/map/LinkNodeBezier.tsx
+++ client/src/components/map/LinkNodeBezier.tsx
@@ -10,45 +10,38 @@ export const LinkNodeBezier: FC = () => {
   const gapLength = 6;
   const dashCycle = dashLength + gapLength;
 
-  return (
-    <>
-      {Object.entries(m.l).map(([nodeId, li]) => {
-        const animated = li.isProcessing;
+  return Object.entries(m.l).map(([nodeId, li]) => {
+    const animated = li.isProcessing;
 
-        const animationStyle = animated
-          ? {
-              strokeDasharray: `${dashLength}, ${gapLength}`,
-              strokeDashoffset: 0,
-              animation: `dashMove ${dashCycle / 12}s linear infinite`,
-            }
-          : {
-              strokeDasharray: 'none',
-              strokeDashoffset: 0,
-              animation: 'none',
-            };
-
-        const mergedStyle = {
-          ...(pathCommonProps.style || {}),
-          ...animationStyle,
+    const animationStyle = animated
+      ? {
+          strokeDasharray: `${dashLength}, ${gapLength}`,
+          strokeDashoffset: 0,
+          animation: `dashMove ${dashCycle / 12}s linear infinite`,
+        }
+      : {
+          strokeDasharray: 'none',
+          strokeDashoffset: 0,
+          animation: 'none',
         };
 
-        return (
-          <>
-            <>
-              <style>{`@keyframes dashMove { to { stroke-dashoffset: -${dashCycle} } } `}</style>
-              <path
-                key={`${nodeId}_l`}
-                d={getBezierLinePath('M', getRootLinePath(m, li))}
-                strokeWidth={1}
-                stroke="#ffffff"
-                fill="none"
-                {...pathCommonProps}
-                style={mergedStyle}
-              />
-            </>
-          </>
-        );
-      })}
-    </>
-  );
+    const mergedStyle = {
+      ...(pathCommonProps.style || {}),
+      ...animationStyle,
+    };
+
+    return (
+      <g key={nodeId}>
+        <style>{`@keyframes dashMove { to { stroke-dashoffset: -${dashCycle} } } `}</style>
+        <path
+          d={getBezierLinePath('M', getRootLinePath(m, li))}
+          strokeWidth={1}
+          stroke="#ffffff"
+          fill="none"
+          {...pathCommonProps}
+          style={mergedStyle}
+        />
+      </g>
+    );
+  });
 };
