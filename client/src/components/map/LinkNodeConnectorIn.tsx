107eb63b0dbc65bef09811f5193cbcacb27588c6
krisz <krisztian@szucher.com>
2025-06-21 12:51:00 +0200

    always show circles

--- client/src/components/map/LinkNodeConnectorIn.tsx
+++ client/src/components/map/LinkNodeConnectorIn.tsx
@@ -8,49 +8,45 @@ import { adjustIcon } from '../../utils/utils.ts';
 
 export const LinkNodeConnectorIn: FC = () => {
   const m = useSelector((state: RootState) => state.slice.commitList[state.slice.commitIndex]);
-  const linkHelpersVisible = useSelector((state: RootState) => state.slice.linkHelpersVisible);
   const connectionStart = useSelector((state: RootState) => state.slice.connectionStart);
   const dispatch = useDispatch<AppDispatch>();
-  return (
-    linkHelpersVisible &&
-    Object.entries(m.r)
-      .filter(([, ri]) =>
-        [ControlType.INGESTION, ControlType.EXTRACTION, ControlType.VECTOR_DATABASE, ControlType.TEXT_OUTPUT].includes(
-          ri.controlType
-        )
+  return Object.entries(m.r)
+    .filter(([, ri]) =>
+      [ControlType.INGESTION, ControlType.EXTRACTION, ControlType.VECTOR_DATABASE, ControlType.TEXT_OUTPUT].includes(
+        ri.controlType
       )
-      .map(([nodeId, ri]) => (
-        <circle
-          key={`${nodeId}_${Side.L}_rc`}
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-          r={8}
-          fill={'#666666'}
-          transform={`translate(${adjustIcon(getNodeStartX(ri))}, ${adjustIcon(getNodeStartY(ri) + ri.selfH / 2)})`}
-          {...{ vectorEffect: 'non-scaling-stroke' }}
-          style={{
-            transition: 'all 0.3s',
-            transitionTimingFunction: 'cubic-bezier(0.0,0.0,0.58,1.0)',
-            transitionProperty: 'all',
-          }}
-          onMouseUp={e => {
-            e.preventDefault();
-            e.stopPropagation();
-            const newLink: Partial<L> = {
-              ...connectionStart,
-              toNodeId: nodeId,
-              toNodeSide: Side.L,
-            };
-            if (
-              connectionStart.fromNodeId !== '' &&
-              connectionStart.fromNodeId !== nodeId &&
-              !isExistingLink(m, newLink)
-            ) {
-              dispatch(actions.insertL({ lPartial: newLink }));
-            }
-          }}
-        />
-      ))
-  );
+    )
+    .map(([nodeId, ri]) => (
+      <circle
+        key={`${nodeId}_${Side.L}_rc`}
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        r={8}
+        fill={'#666666'}
+        transform={`translate(${adjustIcon(getNodeStartX(ri))}, ${adjustIcon(getNodeStartY(ri) + ri.selfH / 2)})`}
+        {...{ vectorEffect: 'non-scaling-stroke' }}
+        style={{
+          transition: 'all 0.3s',
+          transitionTimingFunction: 'cubic-bezier(0.0,0.0,0.58,1.0)',
+          transitionProperty: 'all',
+        }}
+        onMouseUp={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          const newLink: Partial<L> = {
+            ...connectionStart,
+            toNodeId: nodeId,
+            toNodeSide: Side.L,
+          };
+          if (
+            connectionStart.fromNodeId !== '' &&
+            connectionStart.fromNodeId !== nodeId &&
+            !isExistingLink(m, newLink)
+          ) {
+            dispatch(actions.insertL({ lPartial: newLink }));
+          }
+        }}
+      />
+    ));
 };
--- client/src/components/map/LinkNodeConnectorOut.tsx
+++ client/src/components/map/LinkNodeConnectorOut.tsx
@@ -8,46 +8,42 @@ import { adjustIcon } from '../../utils/utils.ts';
 
 export const LinkNodeConnectorOut: FC = () => {
   const m = useSelector((state: RootState) => state.slice.commitList[state.slice.commitIndex]);
-  const linkHelpersVisible = useSelector((state: RootState) => state.slice.linkHelpersVisible);
   const dispatch = useDispatch<AppDispatch>();
-  return (
-    linkHelpersVisible &&
-    Object.entries(m.r)
-      .filter(([, ri]) =>
-        [
-          ControlType.FILE,
-          ControlType.INGESTION,
-          ControlType.TEXT_INPUT,
-          ControlType.EXTRACTION,
-          ControlType.VECTOR_DATABASE,
-        ].includes(ri.controlType)
-      )
-      .map(([nodeId, ri]) => (
-        <circle
-          key={`${nodeId}_${Side.R}_rc`}
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-          r={8}
-          fill={'#666666'}
-          transform={`translate(${adjustIcon(getNodeStartX(ri) + ri.selfW)}, ${adjustIcon(getNodeStartY(ri) + ri.selfH / 2)})`}
-          {...{ vectorEffect: 'non-scaling-stroke' }}
-          style={{
-            transition: 'all 0.3s',
-            transitionTimingFunction: 'cubic-bezier(0.0,0.0,0.58,1.0)',
-            transitionProperty: 'all',
-          }}
-          onMouseDown={e => {
-            e.preventDefault();
-            e.stopPropagation();
-            dispatch(
-              actions.setConnectionStart({
-                fromNodeId: nodeId,
-                fromNodeSide: Side.R,
-              })
-            );
-          }}
-        />
-      ))
-  );
+  return Object.entries(m.r)
+    .filter(([, ri]) =>
+      [
+        ControlType.FILE,
+        ControlType.INGESTION,
+        ControlType.TEXT_INPUT,
+        ControlType.EXTRACTION,
+        ControlType.VECTOR_DATABASE,
+      ].includes(ri.controlType)
+    )
+    .map(([nodeId, ri]) => (
+      <circle
+        key={`${nodeId}_${Side.R}_rc`}
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        r={8}
+        fill={'#666666'}
+        transform={`translate(${adjustIcon(getNodeStartX(ri) + ri.selfW)}, ${adjustIcon(getNodeStartY(ri) + ri.selfH / 2)})`}
+        {...{ vectorEffect: 'non-scaling-stroke' }}
+        style={{
+          transition: 'all 0.3s',
+          transitionTimingFunction: 'cubic-bezier(0.0,0.0,0.58,1.0)',
+          transitionProperty: 'all',
+        }}
+        onMouseDown={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          dispatch(
+            actions.setConnectionStart({
+              fromNodeId: nodeId,
+              fromNodeSide: Side.R,
+            })
+          );
+        }}
+      />
+    ));
 };
