8c0a0f2dbd094444fff9601c99c3659f033c9c2d
krisz <krisztian@szucher.com>
2025-10-19 11:08:11 +0200

    config tabs

--- client/src/components/dialogs/MapConfig.tsx
+++ client/src/components/dialogs/MapConfig.tsx
@@ -1,17 +1,35 @@
-import { Button, Dialog, Flex } from '@radix-ui/themes';
+import { Box, Button, Dialog, Flex, Tabs } from '@radix-ui/themes';
 import { MapConfigEdge } from './MapConfigEdge.tsx';
 import { MapConfigNode } from './MapConfigNode.tsx';
 
 export const MapConfig = () => {
   return (
-    <Dialog.Content style={{ maxWidth: 800 }}>
+    <Dialog.Content
+      style={{
+        animation: 'none',
+        maxWidth: 800,
+        position: 'fixed',
+        top: '10%',
+        left: '50%',
+        transform: 'translateX(-50%)', // center horizontally only
+      }}
+    >
       <Dialog.Title>{'Map Config'}</Dialog.Title>
       <Dialog.Description>Configure map nodes and edges below.</Dialog.Description>
-      <Flex direction="column" gap="6" mt="4" justify="between">
-        <MapConfigNode />
-        <MapConfigEdge />
-      </Flex>
-
+      <Tabs.Root defaultValue="nodes">
+        <Tabs.List>
+          <Tabs.Trigger value="nodes">Nodes</Tabs.Trigger>
+          <Tabs.Trigger value="edges">Edges</Tabs.Trigger>
+        </Tabs.List>
+        <Box pt="3">
+          <Tabs.Content value="nodes">
+            <MapConfigNode />
+          </Tabs.Content>
+          <Tabs.Content value="edges">
+            <MapConfigEdge />
+          </Tabs.Content>
+        </Box>
+      </Tabs.Root>
       <Flex gap="3" mt="4" justify="end">
         <Dialog.Close>
           <Button variant="soft" color="gray">
